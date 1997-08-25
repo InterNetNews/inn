397ee6c8069b2fbc59fe4a957772883d36faf6ac
Clayton O'Neill <coneill@oneill.net>
1997-08-25 21:28:49 +0000

    Added md5.h and md5.c into the tree for the dbz functions.  Were missing earlier

---
+++ include/md5.h
@@ -0,0 +1,30 @@
+/***********************************************************************
+ ** md5.h -- header file for implementation of MD5                    **
+ ** RSA Data Security, Inc. MD5 Message-Digest Algorithm              **
+ ** Created: 2/17/90 RLR                                              **
+ ** Revised: 12/27/90 SRD,AJ,BSK,JT Reference C version               **
+ ** Revised (for MD5): RLR 4/27/91                                    **
+ **********************************************************************/
+
+/* $Id$ */
+
+/* typedef a 32-bit type */
+#ifdef TW_TYPE32
+typedef unsigned TW_TYPE32 UINT4;
+#else
+typedef unsigned long int UINT4;
+#endif
+
+/* Data structure for MD5 (Message-Digest) computation */
+typedef struct {
+  UINT4 i[2];                   /* number of _bits_ handled mod 2^64 */
+  UINT4 buf[4];                                    /* scratch buffer */
+  unsigned char in[64];                              /* input buffer */
+  unsigned char digest[16];     /* actual digest after MD5Final call */
+} MD5_CTX;
+
+void MD5Init();
+void MD5Update();
+void MD5Final();
+
+void sig_md5_get();
