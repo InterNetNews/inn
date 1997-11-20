9a5f34f7758978b5c7a8d5afa699b5977fbfb42f
Clayton O'Neill <coneill@oneill.net>
1997-11-20 22:17:17 +0000

    Added sm.c, a simple storage manager interface from the shell.  Allows printing articles stored with the traditional spool and storage api to stdout and cancelling articles that were stored w/storage api.

---
+++ frontends/sm.c
@@ -0,0 +1,71 @@
+/*  $Revision$
+**
+** Provide a command line interface to the storage manager
+*/
+
+#include <stdio.h>
+#include <errno.h>
+#include "configdata.h"
+#include "clibrary.h"
+#include "libinn.h"
+#include "macros.h"
+#include "qio.h"
+
+void Usage(void) {
+    fprintf(stderr, "Usage sm [-r] [-d] token [token] [token] ...\n");
+    exit(1);
+}
+
+int main(int argc, char **argv) {
+    int                 c;
+    BOOL                Delete = FALSE;
+    int                 i;
+    char                *p;
+    QIOSTATE            *qp;
+    
+    while ((c = getopt(argc, argv, "rd")) != EOF) {
+	switch (c) {
+	case 'r':
+	case 'd':
+	    Delete = TRUE;
+	    break;
+	default:
+	    Usage();
+	}
+    }
+
+    if (!SMinit()) {
+	fprintf(stderr, "Could not initialize the storage manager: %s", SMerrorstr);
+	exit(1);
+    }
+    
+    for (i = optind; i < argc; i++) {
+	if (Delete) {
+	    if (!IsToken(argv[i])) {
+		fprintf(stderr, "%s is not a storage token\n", argv[i]);
+		continue;
+	    }
+	    if (!SMcancel(TextToToken(argv[i])))
+		fprintf(stderr, "Could not remove %s: %s\n", argv[i], SMerrorstr);
+	} else {
+	    if ((qp = QIOopen(argv[i])) == NULL) {
+		fprintf(stderr, "Coult not open %s\n", argv[i]);
+		continue;
+	    } else {
+		while ((p = QIOread(qp)) != NULL) {
+		    if (QIOlength(qp) != 0)
+			if (fwrite(p, QIOlength(qp), 1, stdout) != 1) {
+			    fprintf(stderr, "Output failed: %s\n", strerror(errno));
+			    exit(1);
+			}
+		    printf("\n");
+		}
+		QIOclose(qp);
+		printf("\n");
+	    }
+	}
+	  
+    }
+    SMshutdown();
+    return 0;
+}
