4d0dd519f797ff92d9945acb32af7872c6e29825
Katsuhiro Kondou <kondou@nec.co.jp>
1998-06-11 01:20:20 +0000

    lib/endian.c: 	- From: "Matthias Urlichs" <smurf@noris.de> 	- lib/endian.c: The Standard says main() must return an int.

--- lib/endian.c
+++ lib/endian.c
@@ -48,7 +48,7 @@ SIGHANDLER buserr(int dummy)
 char byte[8] = { (char)0x12, (char)0x36, (char)0x48, (char)0x59,
 		 (char)0x01, (char)0x23, (char)0x45, (char)0x67 };
 
-void main()
+int main()
 {
     /* pointers into the byte order array */
     int *intp = (int *)byte;
