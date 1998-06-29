866915bf53d206c65dc09ec398e8ed34f041ebfa
Katsuhiro Kondou <kondou@nec.co.jp>
1998-06-29 15:05:49 +0000

    storage/cnfs/cnfs.c: 	- From: "Igor Timkin" <ivt@gamma.ru> 	- delete useless line

--- storage/cnfs/cnfs.c
+++ storage/cnfs/cnfs.c
@@ -832,7 +832,6 @@ STATIC int CNFSArtMayBeHere(CYCBUFF *cycbuff, CYCBUFF_OFF_T offset, U_INT32_T cy
     ** avoid a false negative response, *not* a false positive response.
     */
     if (! (cycnum == cycbuff->cyclenum ||
-	(cycbuff->cyclenum == 2 && cycnum + 3 == cycbuff->cyclenum) ||
 	(cycnum == cycbuff->cyclenum - 1 && offset > cycbuff->free) ||
 	(cycnum + 1 == 0 && cycbuff->cyclenum == 2 && offset > cycbuff->free))) {
 	/* We've been overwritten */
