35ffc1c903cd0eb8b74b55f9bf3e46c1cfe4a50a
Katsuhiro Kondou <kondou@nec.co.jp>
1998-09-06 03:57:50 +0000

    innd/innd.h: 	- From: Robert Kiessling <robert@easynet.de> 	- inn does not compile out-of-the box with IRIX 6.2. Here is a patch for 	  the necessary changes.

--- innd/innd.h
+++ innd/innd.h
@@ -516,7 +516,7 @@ extern STRING		CCcancel();
 extern STRING		CCcheckfile();
 
 extern BOOL		HIShavearticle(const HASH MessageID);
-extern BOOL		HISwrite(const ARTDATA *Data, HASH hash, char *paths, TOKEN *token);
+extern BOOL		HISwrite(const ARTDATA *Data, const HASH hash, char *paths, TOKEN *token);
 extern BOOL             HISremember(const HASH MessageID);
 extern char		*HISfilesfor(const HASH MessageID);
 extern void		HISclose(void);
