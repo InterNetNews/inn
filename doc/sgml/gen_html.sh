3b41ef82735e5fd7f005e0528d9dfab3a95357c8
Marc G. Fournier <scrappy@hub.org>
1999-06-11 13:53:04 +0000

    Russ is already redoing things in POD ... any format is going to be hard enough for ppl to learn, let alone trying to deal with more then one ...

--- doc/sgml/gen_html.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-jade -t sgml -d /usr/local/share/sgml/docbook/dsssl/modular/html/docbook.dsl $1.sgml
-mv x1.html $1.html
-jade -t rtf -d /usr/local/share/sgml/docbook/dsssl/modular/print/docbook.dsl $1.sgml
