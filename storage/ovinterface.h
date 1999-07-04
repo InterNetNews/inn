a1c293e3aee2f04d6bcfae50529d95c2c9e8277a
Katsuhiro Kondou <kondou@nec.co.jp>
1999-07-04 02:09:49 +0000

    storage/Make.ovmethods: storage/ovinterface.h: 	- ov3 is now acceced thru OV api

---
+++ storage/ovinterface.h
@@ -0,0 +1,28 @@
+/*  $Id$
+**
+**  Overview interface header
+*/
+
+#ifndef __OVINTERFACE_H__
+#define __OVINTERFACE_H__
+
+#include <configdata.h>
+#include <storage.h>
+
+typedef struct {
+    char	*name;
+    BOOL	(*open)(int mode);
+    BOOL	(*groupstats)(char *group, int *lo, int *hi, int *count, int *flag);
+    BOOL	(*groupadd)(char *group, char *flag);
+    BOOL	(*groupdel)(char *group);
+    BOOL	(*add)(TOKEN token, char *data, int len);
+    BOOL	(*cancel)(TOKEN token);
+    void	*(*opensearch)(char *group, int low, int high);
+    BOOL	(*search)(void *handle, ARTNUM *artnum, char **data, int *len, TOKEN *token);
+    void	(*closesearch)(void *handle);
+    BOOL	(*getartinfo)(char *group, ARTNUM artnum, char **data, int *len, TOKEN *token);
+    BOOL	(*expiregroup)(char *group, int *lo);
+    void	(*close)(void);
+} OV_METHOD;
+
+#endif /* __OVINTERFACE_H__ */
