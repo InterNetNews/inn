591547bc9ce72a10ae9373375aadb46d90aa5cf1
Katsuhiro Kondou <kondou@nec.co.jp>
1999-07-04 01:59:45 +0000

    storage/Makefile: storage/ov.c: storage/ovbuildconfig.in: storage/ov3.c: 	- ov3 is now acceced thru OV api

---
+++ storage/ov.c
@@ -0,0 +1,158 @@
+#include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <syslog.h>
+#include <sys/mman.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+#include "configdata.h"
+#include "macros.h"
+#include "clibrary.h"
+#include "libinn.h"
+#include "paths.h"
+#include "storage.h"
+#include "qio.h"
+#include "ov.h"
+#include "ovmethods.h"
+#include "ovinterface.h"
+
+STATIC OV_METHOD	ov;
+
+BOOL OVopen(int mode) {
+    int			i;
+
+    if (ov.open)
+	/* already opened */
+	return TRUE;
+
+    /* if innconf isn't already read in, do so. */
+    if (innconf == NULL) {
+	if (ReadInnConf() < 0) {
+	    return FALSE;
+	}
+    }
+    for (i=0;i<NUM_OV_METHODS;i++) {
+	if (!strcmp(innconf->ovmethod, ov_methods[i].name))
+	    break;
+    }
+    if (i == NUM_OV_METHODS) {
+	syslog(L_FATAL, "%s is not found for ovmethod", innconf->ovmethod);
+	(void)fprintf(stderr, "%s is not found for ovmethod", innconf->ovmethod);
+	return FALSE;
+    }
+    ov = ov_methods[i];
+    return ((*ov.open)(mode));
+}
+
+BOOL OVgroupstats(char *group, int *lo, int *hi, int *count, int *flag) {
+    if (!ov.open) {
+	/* must be opened */
+	syslog(L_ERROR, "ovopen must be called first");
+	(void)fprintf(stderr, "ovopen must be called first");
+	return FALSE;
+    }
+    return ((*ov.groupstats)(group, lo, hi, count, flag));
+}
+
+BOOL OVgroupadd(char *group, char *flag) {
+    if (!ov.open) {
+	/* must be opened */
+	syslog(L_ERROR, "ovopen must be called first");
+	(void)fprintf(stderr, "ovopen must be called first");
+	return FALSE;
+    }
+    return ((*ov.groupadd)(group, flag));
+}
+
+BOOL OVgroupdel(char *group) {
+    if (!ov.open) {
+	/* must be opened */
+	syslog(L_ERROR, "ovopen must be called first");
+	(void)fprintf(stderr, "ovopen must be called first");
+	return FALSE;
+    }
+    return ((*ov.groupdel)(group));
+}
+
+BOOL OVadd(TOKEN token, char *data, int len) {
+    if (!ov.open) {
+	/* must be opened */
+	syslog(L_ERROR, "ovopen must be called first");
+	(void)fprintf(stderr, "ovopen must be called first");
+	return FALSE;
+    }
+    return ((*ov.add)(token, data, len));
+}
+
+BOOL OVcancel(TOKEN token) {
+    if (!ov.open) {
+	/* must be opened */
+	syslog(L_ERROR, "ovopen must be called first");
+	(void)fprintf(stderr, "ovopen must be called first");
+	return FALSE;
+    }
+    return ((*ov.cancel)(token));
+}
+
+void *OVopensearch(char *group, int low, int high) {
+    if (!ov.open) {
+	/* must be opened */
+	syslog(L_ERROR, "ovopen must be called first");
+	(void)fprintf(stderr, "ovopen must be called first");
+	return FALSE;
+    }
+    return ((*ov.opensearch)(group, low, high));
+}
+
+BOOL OVsearch(void *handle, ARTNUM *artnum, char **data, int *len, TOKEN *token) {
+    if (!ov.open) {
+	/* must be opened */
+	syslog(L_ERROR, "ovopen must be called first");
+	(void)fprintf(stderr, "ovopen must be called first");
+	return FALSE;
+    }
+    return ((*ov.search)(handle, artnum, data, len, token));
+}
+
+void OVclosesearch(void *handle) {
+    if (!ov.open) {
+	/* must be opened */
+	syslog(L_ERROR, "ovopen must be called first");
+	(void)fprintf(stderr, "ovopen must be called first");
+	return;
+    }
+    (*ov.closesearch)(handle);
+    return;
+}
+
+BOOL OVgetartinfo(char *group, ARTNUM artnum, char **data, int *len, TOKEN *token) {
+    if (!ov.open) {
+	/* must be opened */
+	syslog(L_ERROR, "ovopen must be called first");
+	(void)fprintf(stderr, "ovopen must be called first");
+	return FALSE;
+    }
+    return ((*ov.getartinfo)(group, artnum, data, len, token));
+}
+
+BOOL OVexpiregroup(char *group, int *lo) {
+    if (!ov.open) {
+	/* must be opened */
+	syslog(L_ERROR, "ovopen must be called first");
+	(void)fprintf(stderr, "ovopen must be called first");
+	return FALSE;
+    }
+    return ((*ov.expiregroup)(group, lo));
+}
+
+void OVclose(void) {
+    if (!ov.open)
+	return;
+    (*ov.close)();
+    memset(&ov, '\0', sizeof(ov));
+    return;
+}
