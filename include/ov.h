be11034fdb3184d6279c7d4fcc8efe290e1b359b
Katsuhiro Kondou <kondou@nec.co.jp>
1999-08-02 06:58:44 +0000

    include/ov.h 	- add OVprobe(OVSPACE) to see used buffer space (only buffindexed is 	  meaningful)

--- include/ov.h
+++ include/ov.h
@@ -8,6 +8,9 @@ extern "C" {
 #define OV_READ  1
 #define OV_WRITE 2
 
+typedef enum {OVSPACE} OVPROBETYPE;
+#define OV_NOSPACE 100
+
 BOOL OVopen(int mode);
 BOOL OVgroupstats(char *group, int *lo, int *hi, int *count, int *flag);
 BOOL OVgroupadd(char *group, ARTNUM lo, char *flag);
@@ -19,6 +22,7 @@ BOOL OVsearch(void *handle, ARTNUM *artnum, char **data, int *len, TOKEN *token)
 void OVclosesearch(void *handle);
 BOOL OVgetartinfo(char *group, ARTNUM artnum, char **data, int *len, TOKEN *token);
 BOOL OVexpiregroup(char *group, int *lo);
+BOOL OVprobe(OVPROBETYPE type, void *result);
 void OVclose(void);
 
 #ifdef __cplusplus
