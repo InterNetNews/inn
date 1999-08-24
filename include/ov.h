ed34c30cdde7e95984c0366b80d293a0cc165022
Katsuhiro Kondou <kondou@nec.co.jp>
1999-08-24 22:05:43 +0000

    include/ov.h 	- OVprobe is renamed to OVctl 	- add arrival time for newnews

--- include/ov.h
+++ include/ov.h
@@ -8,21 +8,22 @@ extern "C" {
 #define OV_READ  1
 #define OV_WRITE 2
 
-typedef enum {OVSPACE} OVPROBETYPE;
+typedef enum {OVSPACE, OVSORT, OVCUTOFFLOW} OVCTLTYPE;
 #define OV_NOSPACE 100
+typedef enum {OVNEWSGROUP, OVARRIVED} OVSORTTYPE;
 
 BOOL OVopen(int mode);
 BOOL OVgroupstats(char *group, int *lo, int *hi, int *count, int *flag);
 BOOL OVgroupadd(char *group, ARTNUM lo, ARTNUM hi, char *flag);
 BOOL OVgroupdel(char *group);
-BOOL OVadd(TOKEN token, char *data, int len);
+BOOL OVadd(TOKEN token, char *data, int len, time_t arrived);
 BOOL OVcancel(TOKEN token);
 void *OVopensearch(char *group, int low, int high);
-BOOL OVsearch(void *handle, ARTNUM *artnum, char **data, int *len, TOKEN *token);
+BOOL OVsearch(void *handle, ARTNUM *artnum, char **data, int *len, TOKEN *token, time_t *arrived);
 void OVclosesearch(void *handle);
 BOOL OVgetartinfo(char *group, ARTNUM artnum, char **data, int *len, TOKEN *token);
 BOOL OVexpiregroup(char *group, int *lo);
-BOOL OVprobe(OVPROBETYPE type, void *result);
+BOOL OVctl(OVCTLTYPE type, void *val);
 void OVclose(void);
 
 #ifdef __cplusplus
