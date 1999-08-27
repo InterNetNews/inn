e971a6b1e65756aec896b653f18492d059252973
Katsuhiro Kondou <kondou@nec.co.jp>
1999-08-27 05:35:38 +0000

    include/patchlevel.h: 	- date is updated

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -14,7 +14,7 @@
 */
 #define RELEASE "2"
 #define PATCHLEVEL "3experimental"
-#define DATE "08-Jun-1999"
+#define DATE "27-Aug-1999"
 #define RELEASE_C 2
 #define PATCHLEVEL_C 3
 #define SUB_PATCHLEVEL_C 0
