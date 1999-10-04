4babbfae477737dddb0bb361bf40b97b5909e045
Katsuhiro Kondou <kondou@nec.co.jp>
1999-10-04 02:43:50 +0000

    innd/nc.c: 	- From: Heiko Schlichting <inn-bugs@fu-berlin.de> 	- When streaming is enabled and INN gets a NNTP "check" command WITHOUT 	  a following space/tab and message-id, it prints some memory dump to 	  the requestor

--- innd/nc.c
+++ innd/nc.c
@@ -1291,7 +1291,7 @@ NCcheck(CHANNEL *cp)
 
     cp->Check++;
     /* Snip off the Message-ID. */
-    for (p = cp->In.Data; !ISWHITE(*p); p++)
+    for (p = cp->In.Data; *p && !ISWHITE(*p); p++)
 	continue;
     for ( ; ISWHITE(*p); p++)
 	continue;
