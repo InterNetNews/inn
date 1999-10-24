623557678112269b5931158db943551347c07992
Russ Allbery <eagle@eyrie.org>
1999-10-24 22:56:58 +0000

    Reworked to use the autoconf defines directly rather than requiring defines saying what style of close-on-exec to use.  Improved the fcntl() method to preserve the other flag bits, not that any system uses them (but it's technically more correct).  Cut down on needless includes.

--- lib/closeonexec.c
+++ lib/closeonexec.c
@@ -1,47 +1,69 @@
-/*  $Revision$
+/*  $Id$
 **
+**  Portably set or clear close-on-exec status of a file descriptor,
+**  ignoring errors.
 */
-#include <stdio.h>
-#include <sys/types.h>
-#include "configdata.h"
-#include "clibrary.h"
+#include "config.h"
+#include "libinn.h"
+
 #include <errno.h>
-
 
-#if	defined(CLX_IOCTL)
-#include <sgtty.h>
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
 
+#ifdef HAVE_FCNTL_H
+# include <sys/types.h>
+# include <fcntl.h>
+# ifndef FD_CLOEXEC
+#  define FD_CLOEXEC 1
+# endif
+#else
+# ifdef HAVE_SGTTY_H
+#  include <sgtty.h>
+# endif
+# ifdef HAVE_SYS_FILIO_H
+#  include <sys/filio.h>
+# endif
+#endif
 
-/*
-**  Mark a file close-on-exec so that it doesn't get shared with our
-**  children.  Ignore any error codes.
-*/
-void CloseOnExec(int fd, int flag)
-{
-    int		oerrno;
-
-    oerrno = errno;
-    (void)ioctl(fd, (unsigned long) (flag ? FIOCLEX : FIONCLEX), (char *)NULL);
-    errno = oerrno;
-}
-#endif	/* defined(CLX_IOCTL) */
-
-
-
-#if	defined(CLX_FCNTL)
-#include <fcntl.h>
-
+#ifdef HAVE_FCNTL_H
 
 /*
-**  Mark a file close-on-exec so that it doesn't get shared with our
-**  children.  Ignore any error codes.
+**  Technically, one is supposed to retrieve the flags, add FD_CLOEXEC, and
+**  then set them, although I've never seen a system with any flags other
+**  than close-on-exec.  Do it right anyway; it's not that expensive.
 */
-void CloseOnExec(int fd, int flag)
+void
+CloseOnExec(int fd, int flag)
 {
-    int		oerrno;
+    int oerrno;
+    int oflag;
 
     oerrno = errno;
-    (void)fcntl(fd, F_SETFD, flag ? 1 : 0);
+    oflag = fcntl(fd, F_GETFD, 0);
+    if (oflag < 0) {
+        errno = oerrno;
+        return;
+    }
+    fcntl(fd, F_SETFD, flag ? (oflag | FD_CLOEXEC) : (oflag & ~FD_CLOEXEC));
     errno = oerrno;
 }
-#endif	/* defined(CLX_FCNTL) */
+
+#else /* !HAVE_FCNTL_H */
+
+/*
+**  If we don't have fcntl.h, assume we'll be able to find a definition of
+**  FIOCLEX somewhere.
+*/
+void
+CloseOnExec(int fd, int flag)
+{
+    int oerrno;
+
+    oerrno = errno;
+    ioctl(fd, (flag ? FIOCLEX : FIONCLEX), (char *) 0);
+    errno = oerrno;
+}
+
+#endif /* !HAVE_FCNTL_H */
