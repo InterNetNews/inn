4b79a691910f222bfb2c7b3469938dfe56af21c6
Russ Allbery <eagle@eyrie.org>
1999-10-27 10:12:45 +0000

    Use HAVE_SIGACTION instead of USE_SIGACTION, remove needless #includes, use a typedef for signal handler function pointers for more readable declarations.

--- lib/xsignal.c
+++ lib/xsignal.c
@@ -1,38 +1,46 @@
-/*
-**  Provides a 'reliable' implementation of signal() for SYSV-derived systems
+/*  $Id$
+**
+**  Provides a 'reliable' implementation of signal() for SYSV-derived
+**  systems.
+**
+**  Be aware that there's weird declaration stuff going on here; a signal
+**  handler is a pointer to a function taking an int and returning void.
+**  We typedef this as SIG_HANDLER_T for clearer code.
 */
-#include <stdio.h>
-#include <sys/types.h>
+#include "config.h"
+#include "libinn.h"
 #include <signal.h>
-#include "configdata.h"
-#include "clibrary.h"
 
-#ifdef USE_SIGACTION
+typedef void (*SIG_HANDLER_T)(int);
 
-void (*xsignal(int signum, void (*sigfunc)(int )))(int )
+#ifdef HAVE_SIGACTION
+
+SIG_HANDLER_T
+xsignal(int signum, SIG_HANDLER_T sigfunc)
 {
-	struct sigaction act, oact;
+    struct sigaction act, oact;
 
-	act.sa_handler = sigfunc;
-	sigemptyset(&act.sa_mask);
+    act.sa_handler = sigfunc;
+    sigemptyset(&act.sa_mask);
 
+    /* Try to restart system calls if possible. */
 #ifdef SA_RESTART
-	act.sa_flags = SA_RESTART;
+    act.sa_flags = SA_RESTART;
 #else
-	act.sa_flags = 0;
+    act.sa_flags = 0;
 #endif
 
-	if (sigaction(signum, &act, &oact) < 0) {
-		return SIG_ERR;
-	}
-	return oact.sa_handler;
+    if (sigaction(signum, &act, &oact) < 0)
+        return SIG_ERR;
+    return oact.sa_handler;
 }
 
-#else
+#else /* !HAVE_SIGACTION */
 
-void (*xsignal(int signum, void (*sigfunc)(int )))(int )
+SIG_HANDLER_T
+xsignal(int signum, SIG_HANDLER_T sigfunc)
 {
-	return signal(signum, sigfunc);
+    return signal(signum, sigfunc);
 }
 
-#endif
+#endif /* !HAVE_SIGACTION */
