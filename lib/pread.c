b5f7b91d4df97c377499dc47aa7bb3926828ad64
Russ Allbery <eagle@eyrie.org>
1999-10-31 05:28:02 +0000

    Remove needless includes, use correct POSIX types, reset the file pointer back to its original value after the read or write as is required by the definition of pread() and pwrite().  Remove the #ifndef HAVE_PREAD, etc. defines, since the Makefile now takes care of that the same was as with other replacement functions.

--- lib/pread.c
+++ lib/pread.c
@@ -1,22 +1,42 @@
-/*
-** $Revision 1.0 $
+/*  $Id$
+**
+**  Reads at an offset.
+**
+**  Replacement for system pread(2) call, if missing.  pread(2) is defined
+**  as not changing the file pointer.  Note that this is not atomic;
+**  threaded programs should require the system pread(2).
 */
-
-#include <unistd.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <stdio.h>
 #include "config.h"
-#include "clibrary.h"
+#include <sys/types.h>
 
-#ifndef HAVE_PREAD
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
 
-OFFSET_T pread(int fd, void *buf, OFFSET_T nbyte, OFFSET_T offset ) {
-    
-    if (lseek(fd, offset, SEEK_SET) < 0)
-	return -1;
+#ifndef SEEK_SET
+# define SEEK_SET 0
+#endif
+#ifndef SEEK_CUR
+# define SEEK_CUR 1
+#endif
+#ifndef SEEK_END
+# define SEEK_END 2
+#endif
 
-    return read(fd, buf, nbyte);
+ssize_t
+pread(int fd, void *buf, size_t nbyte, OFFSET_T offset)
+{
+    OFFSET_T    current;
+    ssize_t     nread;
+
+    current = lseek(fd, 0, SEEK_CUR);
+    if (current < 0 || lseek(fd, offset, SEEK_SET) < 0)
+        return -1;
+
+    nread = read(fd, buf, nbyte);
+
+    /* Ignore errors in restoring the file position; this isn't ideal, but
+       reporting a failed read when the read succeeded is worse. */
+    lseek(fd, current, SEEK_SET);
+    return nread;
 }
-
-#endif /* HAVE_PREAD */
--- lib/pwrite.c
+++ lib/pwrite.c
@@ -1,22 +1,42 @@
-/*
-** $Revision 1.0 $
+/*  $Id$
+**
+**  Writes at an offset.
+**
+**  Replacement for system pwrite(2) call, if missing.  pwrite(2) is defined
+**  as not changing the file pointer.  Note that this is not atomic;
+**  threaded programs should require the system pwrite(2).
 */
-
-#include <unistd.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <stdio.h>
 #include "config.h"
-#include "clibrary.h"
+#include <sys/types.h>
 
-#ifndef HAVE_PWRITE
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
 
-OFFSET_T pwrite(int fd, void *buf, OFFSET_T nbyte, OFFSET_T offset ) {
-    
-    if (lseek(fd, offset, SEEK_SET) < 0)
-	return -1;
+#ifndef SEEK_SET
+# define SEEK_SET 0
+#endif
+#ifndef SEEK_CUR
+# define SEEK_CUR 1
+#endif
+#ifndef SEEK_END
+# define SEEK_END 2
+#endif
 
-    return write(fd, buf, nbyte);
+ssize_t
+pwrite(int fd, void *buf, size_t nbyte, OFFSET_T offset)
+{
+    OFFSET_T    current;
+    ssize_t     nwritten;
+
+    current = lseek(fd, 0, SEEK_CUR);
+    if (current < 0 || lseek(fd, offset, SEEK_SET) < 0)
+        return -1;
+
+    nwritten = write(fd, buf, nbyte);
+
+    /* Ignore errors in restoring the file position; this isn't ideal, but
+       reporting a failed write when the write succeeded is worse. */
+    lseek(fd, current, SEEK_SET);
+    return nwritten;
 }
-
-#endif /* HAVE_PWRITE */
