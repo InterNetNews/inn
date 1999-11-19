f003be2ff89be47c9a10aa4585525a969f891d39
Katsuhiro Kondou <kondou@nec.co.jp>
1999-11-19 01:30:32 +0000

    innd/art.c: 	- previous patch still does not fix the problem which leads innd 	  dumping core

--- innd/art.c
+++ innd/art.c
@@ -2127,7 +2127,7 @@ STRING ARTpost(CHANNEL *cp)
     p = HDR(_distribution);
     distributions = *p ? CommaSplit(p) : NULL;
     if (distributions) {
-      if (distributions[0] == '\0') {
+      if (*distributions[0] == '\0') {
 	(void)sprintf(buff, "%d bogus distribution \"%s\"",
 		NNTP_REJECTIT_VAL,
 		MaxLength(p, p));
