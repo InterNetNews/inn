119a939bcf4540d5b8afbd4642c95b5e5e01ac1a
Russ Allbery <eagle@eyrie.org>
1999-11-19 09:31:03 +0000

    Clean up and reformat, remove the C++ protection that should be unnecessary, remove lots of needless casts that interfere with good compiler type checking, and remove the obsolete CAST macro as well as a few other lint-silencing hacks.

--- include/macros.h
+++ include/macros.h
@@ -1,108 +1,66 @@
-/*  $Revision$
+/*  $Id$
 **
 **  Here be some useful macros.
 */
 
-#ifdef __cplusplus
-extern "C" {
+#ifndef __MACROS_H__
+#define __MACROS_H__
+
+/* We've probably already included this; only include it if we need it. */
+#ifndef __CONFIG_H__
+# include "config.h"
 #endif
 
-/*
-**  Memory allocation.  Wrappers around wrapper functions.
-**  Don't replace any existing definitions, for use with malloc-debug
-**  packages, e.g.
-*/
-#if	defined(_DEBUG_MALLOC_INC)
-#undef _DEBUG_MALLOC_INC
-#include "malloc.h"
+/* Memory allocation.  Wrappers around wrapper functions.  Don't replace any
+   existing definitions, for use with malloc-debug packages, e.g. */
+#ifdef _DEBUG_MALLOC_INC
+# undef _DEBUG_MALLOC_INC
+# include "malloc.h"
 #else
-#define malloc_enter(func)
-#define malloc_leave(func)
-#define malloc_chain_check()
-#define malloc_dump(fd)
-#define malloc_list(a,b,c)
-#define malloc_size(hist)	(*(hist) = 0, 0)
-#endif	/* defined(_DEBUG_MALLOC_INC) */
+# define malloc_enter(func)
+# define malloc_leave(func)
+# define malloc_chain_check()
+# define malloc_dump(fd)
+# define malloc_list(a,b,c)
+# define malloc_size(hist)	(*(hist) = 0, 0)
+#endif
 
-#if	!defined(NEW)
+/* Memory allocation macros. */
+#define NEW(T, c)       (xmalloc((sizeof(T) * (c)), __FILE__, __LINE__))
+#define COPY(p)         strcpy(NEW(char, strlen(p) + 1), p)
+#define DISPOSE(p)      free(p)
+#define RENEW(p, T, c)	\
+    (p = xrealloc((p), (sizeof(T) * (c)), __FILE__, __LINE__))
 
-#define NEW(T, c)			\
-	((T *)xmalloc((unsigned int)(sizeof (T) * (c)), __FILE__, __LINE__))
+#define ONALLOCFAIL(func)       (xmemfailure = (func))
 
-#define RENEW(p, T, c)			\
-        (p = (T *)xrealloc((char *)(p), (unsigned int)(sizeof (T) * (c)), \
-                           __FILE__, __LINE__))
-
-#define DISPOSE(p)		free(p)
-
-#define ONALLOCFAIL(func)		(xmemfailure = (func))
-
-#endif	/* !defined(NEW) */
-
-
-/*
-**  Copy a string to allocated memory.
-*/
-#define COPY(p)				\
-	strcpy(NEW(char, strlen(p) + 1), p)
-
-
-/*
-**  Wrappers around str[n]cmp.  Don't add the ((a) == (b)) test here; it's
-**  already been done in places where it's time-critical.
-*/
+/* Wrappers around str[n]cmp.  Don't add the ((a) == (b)) test here; it's
+   already been done in places where it's time-critical. */
 #define EQ(a, b)		(strcmp((a), (b)) == 0)
-#define EQn(a, b, n)		(strncmp((a), (b), (SIZE_T)(n)) == 0)
+#define EQn(a, b, n)		(strncmp((a), (b), (n)) == 0)
 #define caseEQ(a, b)		(strcasecmp((a), (b)) == 0)
-#define caseEQn(a, b, n)	(strncasecmp((a), (b), (SIZE_T)(n)) == 0)
+#define caseEQn(a, b, n)	(strncasecmp((a), (b), (n)) == 0)
 
+/* <ctype.h> usually includes \n, which is not what we want. */
+#define ISWHITE(c)      ((c) == ' ' || (c) == '\t')
 
-/*
-**  Cast a pointer into another point, but keep lint quiet.
-*/
-#if	!defined(lint)
-#define CAST(t, p)	((t)(p))
-#else
-#define CAST(t, p)	((p) ? (t)NULL : (t)NULL)
-#endif /* !defined(lint) */
-
-
-/*
-**  <ctype.h> usually includes \n, which is not what we want.
-*/
-#define ISWHITE(c)			((c) == ' ' || (c) == '\t')
-
-
-/*
-**  Get the number of elements in a fixed-size array, or a pointer just
-**  past the end of it.
-*/
-#define SIZEOF(array)	((int)(sizeof array / sizeof array[0]))
+/* Get the number of elements in a fixed-size array, or a pointer just past
+   the end of it. */
+#define SIZEOF(array)	(sizeof array / sizeof array[0])
 #define ENDOF(array)	(&array[SIZEOF(array)])
 
+/* Get the length of a string constant. */
+#define STRLEN(string)  (sizeof string - 1)
 
-/*
-**  Get the length of a string constant.
-*/
-#define STRLEN(string)	((int)(sizeof string - 1))
-
-
-/*
-**  Turn a TIMEINFO into a floating point number.
-*/
+/* Turn a TIMEINFO into a floating point number. */
 #define TIMEINFOasDOUBLE(t)	\
     ((double)(t).time + ((double)(t).usec) / 1000000.0)
 
-
-/*
-**  Use a read or recv call to read a descriptor.
-*/
-#if	defined(HAVE_UNIX_DOMAIN_SOCKETS)
-#define RECVorREAD(fd, p, s)	recv((fd), (p), (s), 0)
+/* Use a read or recv call to read a descriptor. */
+#ifdef HAVE_UNIX_DOMAIN_SOCKETS
+# define RECVorREAD(fd, p, s)   recv((fd), (p), (s), 0)
 #else
-#define RECVorREAD(fd, p, s)	read((fd), (p), (s))
-#endif	/* defined(HAVE_UNIX_DOMAIN_SOCKETS) */
+# define RECVorREAD(fd, p, s)   read((fd), (p), (s))
+#endif
 
-#ifdef __cplusplus
-}
-#endif /* __cplusplus */
+#endif /* __MACROS_H__ */
