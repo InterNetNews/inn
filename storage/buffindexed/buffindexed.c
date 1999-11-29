bcb2f85927df4e624d0cf5d1c2ec9b5bc20add43
Katsuhiro Kondou <kondou@nec.co.jp>
1999-11-29 21:57:01 +0000

    storage/buffindexed/buffindexed.c: 	- group.index was opened always as O_RDWR | O_CREAT

--- storage/buffindexed/buffindexed.c
+++ storage/buffindexed/buffindexed.c
@@ -894,7 +894,7 @@ BOOL buffindexed_open(int mode) {
   if (Needunlink && unlink(groupfn) == 0) {
     syslog(L_NOTICE, "%s: all buffers are brandnew, unlink '%s'", LocalLogName, groupfn);
   }
-  GROUPfd = open(groupfn, O_RDWR | O_CREAT, ARTFILE_MODE);
+  GROUPfd = open(groupfn, ovbuffmode & OV_WRITE ? O_RDWR | O_CREAT : O_RDONLY);
   if (GROUPfd < 0) {
     syslog(L_FATAL, "%s: Could not create %s: %m", LocalLogName, groupfn);
     DISPOSE(groupfn);
