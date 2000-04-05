ad8e76e61b19c34a04ddf6fa04dedfd035f37d25
Katsuhiro Kondou <kondou@nec.co.jp>
2000-04-05 13:51:15 +0000

    storage/cnfs/cnfs.c: 	- skip checking bit maps to see if they are not unset for article size 	  when retrieving

--- storage/cnfs/cnfs.c
+++ storage/cnfs/cnfs.c
@@ -1162,27 +1162,7 @@ ARTHANDLE *cnfs_retrieve(const TOKEN token, const RETRTYPE amount) {
 	    return NULL;
 	}
     }
-    /* check the bitmap to ensure cah.size is not broken */
-    /* cannot believe cycbuff->free, since it may not be updated by writer */
-    blockfudge = (sizeof(cah) + plusoffset + ntohl(cah.size)) % CNFS_BLOCKSIZE;
-    limit = offset + sizeof(cah) + plusoffset + ntohl(cah.size) - blockfudge + CNFS_BLOCKSIZE;
-    for (middle = offset + CNFS_BLOCKSIZE; middle < limit;
-	middle += CNFS_BLOCKSIZE) {
-	if (CNFSUsedBlock(cycbuff, middle, FALSE, FALSE) != 0)
-	/* Bitmap set.  This article assumes to be broken */
-	break;
-    }
-    if (middle != limit) {
-	char buf1[24], buf2[24], buf3[24];
-	strcpy(buf1, CNFSofft2hex(cycbuff->free, FALSE));
-	strcpy(buf2, CNFSofft2hex(middle, FALSE));
-	strcpy(buf3, CNFSofft2hex(limit, FALSE));
-	SMseterror(SMERR_UNDEFINED, "size overflows bitmaps");
-	syslog(L_ERROR, "%s: size overflows bitmaps %s %s:0x%s:0x%s:0x%s: %ld",
-	LocalLogName, TokenToText(token), cycbuffname, buf1, buf2, buf3, ntohl(cah.size));
-	DISPOSE(art);
-	return NULL;
-    }
+    /* checking the bitmap to ensure cah.size is not broken was dropped */
     private = NEW(PRIV_CNFS, 1);
     art->private = (void *)private;
     art->arrived = ntohl(cah.arrived);
