687a6e775576756a3d259808321c6e3d63e9e409
Russ Allbery <eagle@eyrie.org>
2000-06-08 08:20:34 +0000

    Reorganized somewhat.  If no dates are given on the command line, convdate now uses the current time.  Exit properly after printing the usage message if -h is given.

--- expire/convdate.c
+++ expire/convdate.c
@@ -11,18 +11,24 @@
 #include "libinn.h"
 
 static const char usage[] = "\
-Usage: convdate -n date [date ...]\n\
-       convdate [-dl] -c time [time ...]\n\
-       convdate [-dl] [-s] date [date ...]\n\
+Usage: convdate -n [date ...]\n\
+       convdate [-dl] -c [time ...]\n\
+       convdate [-dl] [-s] [date ...]\n\
 \n\
 convdate -n converts a date (in any format parseable by parsedate) to the\n\
 number of seconds since epoch.  convdate -s does the same, but converts\n\
 to a date string.  convdate -c converts seconds since epoch to a date\n\
-string.  The default date string is the output of ctime (normally the\n\
-same format as returned by the date command).  If -d is given, the output\n\
-is formatted as a valid Usenet article Date header.  If -l is given with\n\
--d, format the time in local time rather than UTC.  If no options are\n\
-given, the -s behavior is the default.";
+string.  The default output is the output of ctime (normally the same format\n\
+as returned by the date command).  If -d is given, the output is formatted\n\
+as a valid Usenet article Date header.  If -l is given with -d, format the\n\
+time in local time rather than UTC.  If no options are given, the -s\n\
+behavior is the default; if no dates are given, the current time is used.";
+
+/* Whether to format the output as a Date header. */
+static BOOL date_format = FALSE;
+
+/* Whether to use local time instead of UTC. */
+static BOOL date_local = FALSE;
 
 
 /*
@@ -40,19 +46,18 @@ isdigits(const char *p)
 
 /*
 **  Print date corresponding to the provided time_t.  By default, the output
-**  of ctime is printed, but if the second argument is true, makedate is
-**  used instead.  If the third argument is true, format in local time;
-**  otherwise, use UTC (only meaningful if the second argument is true).
-**  Returns success.
+**  of ctime is printed, but if date_format is true, makedate is used
+**  instead.  If date_local is true, format in local time; otherwise, use
+**  UTC.  Returns success.
 */
 static BOOL
-print_date(time_t date, BOOL format, BOOL local)
+print_date(time_t date)
 {
     char date_buffer[128];
     char *result;
 
-    if (format) {
-        if (!makedate(date, local, date_buffer, 128)) {
+    if (date_format) {
+        if (!makedate(date, date_local, date_buffer, 128)) {
             warn("can't format %ld", (long) date);
             return FALSE;
         } else {
@@ -70,15 +75,52 @@ print_date(time_t date, BOOL format, BOOL local)
 }
 
 
+/*
+**  The core function.  Given a string representing a date (in some format
+**  given by the mode) and a mode ('s', 'n', or 'c', corresponding to the
+**  basic three options to the program), convert the date and print the
+**  output.  date may be NULL, in which case the current date is used.
+**  Returns true if conversion was successful, false otherwise.
+*/
+static BOOL
+convdate(const char *date, char mode)
+{
+    time_t seconds;
+
+    /* Convert the given date to seconds or obtain the current time. */
+    if (date == NULL) {
+        seconds = time(NULL);
+    } else if (mode == 'c') {
+        if (!isdigits(date)) {
+            warn("\"%s\" doesn't look like a number", date);
+            return FALSE;
+        } else {
+            seconds = (time_t) atol(date);
+        }
+    } else {
+        seconds = parsedate((char *) date, NULL);
+        if (seconds == (time_t) -1) {
+            warn("can't convert \"%s\"", date);
+            return FALSE;
+        }
+    }
+
+    /* Output the resulting date. */
+    if (mode == 'n') {
+        printf("%ld\n", seconds);
+        return TRUE;
+    } else {
+        return print_date(seconds);
+    }
+}
+
+
 int
 main(int argc, char *argv[])
 {
     int         option, status;
-    int         mode = 0;
-    BOOL        date_format = FALSE;
-    BOOL        date_local = FALSE;
+    char        mode = 0;
     char *      date;
-    time_t      seconds;
 
     error_program_name = "convdate";
 
@@ -87,6 +129,7 @@ main(int argc, char *argv[])
         switch (option) {
         case 'h':
             printf("%s\n", usage);
+            exit(0);
             break;
         case 'd':
             date_format = TRUE;
@@ -108,42 +151,13 @@ main(int argc, char *argv[])
     if (mode == 0) mode = 's';
     argc -= optind;
     argv += optind;
-    if (argc == 0) die("no date to convert given");
 
     /* Perform the desired action for each provided argument. */
-    for (date = *argv, status = 0; date != NULL; date = *++argv) {
-        switch (mode) {
-        default:
-        case 's':
-            seconds = parsedate(date, NULL);
-            if (seconds == -1) {
-                warn("can't convert \"%s\"", date);
-                status++;
-            } else {
-                if (!print_date(seconds, date_format, date_local))
-                    status++;
-            }
-            break;
-        case 'n':
-            seconds = parsedate(date, NULL);
-            if (seconds == -1) {
-                warn("can't convert \"%s\"", date);
-                status++;
-            } else {
-                printf("%ld\n", seconds);
-            }
-            break;
-        case 'c':
-            if (!isdigits(date)) {
-                warn("\"%s\" doesn't look like a number", date);
-                status++;
-            } else {
-                seconds = (time_t) atol(date);
-                if (!print_date(seconds, date_format, date_local))
-                    status++;
-            }
-            break;
-        }
+    if (argc == 0) {
+        exit(convdate(NULL, mode) ? 0 : 1);
+    } else {
+        for (date = *argv, status = 0; date != NULL; date = *++argv)
+            status += (convdate(date, mode) ? 0 : 1);
+        exit(status);
     }
-    exit(status);
 }
