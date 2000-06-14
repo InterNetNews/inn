c385757c4d462843f68a40b3918f1f2355bc90b5
Russ Allbery <eagle@eyrie.org>
2000-06-14 07:12:34 +0000

    Rewritten, cleaning up the headers and being more paranoid about overflows of the static buffer in truly bizarre situations.

--- lib/strerror.c
+++ lib/strerror.c
@@ -1,28 +1,41 @@
-/*  $Revision$
+/*  $Id$
 **
-**  Only <errno.h> is needed; the others are just to get the right sprintf()
-**  declaration, sigh.
+**  Replacement for a missing strerror.
+**
+**  Written by Russ Allbery <rra@stanford.edu>
+**  This work is hereby placed in the public domain by its author.
+**
+**  Provides the same functionality as the standard library routine strerror
+**  for those platforms that don't have it (e.g. Ultrix).  Assume that we
+**  have sys_nerr and sys_errlist available to use instead.  Calling
+**  strerror should be thread-safe unless it is called for an unknown errno.
 */
-#include <stdio.h>
-#include <sys/types.h>
-#include "configdata.h"
-#include "clibrary.h"
+
 #include <errno.h>
+#include <stdio.h>
 
+/* If we're running the test suite, rename strerror to avoid conflicts with
+   the system version. */
+#if TESTING
+# define strerror test_strerror
+#endif
 
-/*
-**  Return a string representation of errno.
-*/
-char *
-strerror(e)
-    int		e;
+const char *
+strerror(int error)
 {
-    extern int	sys_nerr;
-    extern char	*sys_errlist[];
-    static char	buff[30];
+    extern const int sys_nerr;
+    extern const char *sys_errlist[];
+    static char buff[32];
+    int oerrno;
 
-    if (e >= 0 && e < sys_nerr)
-	return sys_errlist[e];
-    (void)sprintf(buff, "Error code %d\n", e);
+    if (error >= 0 && error < sys_nerr) return sys_errlist[error];
+
+    /* Paranoia.  If an int is very large (like 128 bytes) one could
+       overflow the buffer here, so refuse to process particularly large
+       values of error. */
+    if (error > 999999 || error < -99999) return "";
+    oerrno = errno;
+    sprintf(buff, "Error code %d", error);
+    errno = oerrno;
     return buff;
 }
