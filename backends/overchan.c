ec0e13b589006c94839037b04fda3272639201b6
Katsuhiro Kondou <kondou@nec.co.jp>
2000-08-09 11:03:53 +0000

    - notification message is modified for convenience

--- backends/overchan.c
+++ backends/overchan.c
@@ -106,7 +106,7 @@ int main(int ac, char *av[])
     if (ReadInnConf() < 0) exit(1);
     (void)umask(NEWSUMASK);
     if (innconf->enableoverview && !innconf->useoverchan)
-	syslog(L_ERROR, "overchan runs while innd creates overview data");
+	syslog(L_ERROR, "overchan runs while innd creates overview data(you can ignore this message if you run makehistory '-F')");
 
     ac -= 1;
     av += 1;
