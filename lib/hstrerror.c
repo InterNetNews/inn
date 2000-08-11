79562d1d9a4e78a84323a5dd9f419f49f10f49b2
Russ Allbery <eagle@eyrie.org>
2000-08-11 05:21:32 +0000

    Largely rewritten, programming style changed to match most of the rest of INN rather than innfeed.  Removed the declaration of h_errno since the function doesn't need it.  Added an error string for an h_errno of -1.

--- lib/hstrerror.c
+++ lib/hstrerror.c
@@ -1,36 +1,46 @@
-#include <sys/types.h>
-#include <netdb.h>
-#include "configdata.h"
+/*  $Id$
+**
+**  Replacement for a missing hstrerror.
+**
+**  Written by Russ Allbery <rra@stanford.edu>
+**  This work is hereby placed in the public domain by its author.
+**
+**  Provides hstrerror (strerror, but for h_errno from the resolver
+**  libraries) on those platforms that don't have it (most non-BSD).  This
+**  function is thread-safe unless called with an unknown h_errno.
+*/
+
+#include "config.h"
 #include "clibrary.h"
+#include <netdb.h>
 
-static const char * const pvt_h_errlist[] = {
-  "Resolver Error 0 (no error)",
-  "Unknown host",                         /* 1 HOST_NOT_FOUND */
-  "Host name lookup failure",             /* 2 TRY_AGAIN */
-  "Unknown server error",                 /* 3 NO_RECOVERY */
-  "No address associated with name",      /* 4 NO_ADDRESS */
+static const char * const errors[] = {
+    "No resolver error",                /* 0 NETDB_SUCCESS */
+    "Unknown host",                     /* 1 HOST_NOT_FOUND */
+    "Host name lookup failure",         /* 2 TRY_AGAIN */
+    "Unknown server error",             /* 3 NO_RECOVERY */
+    "No address associated with name",  /* 4 NO_ADDRESS / NO_DATA */
 };
+static int nerrors = (sizeof errors / sizeof errors[0]);
 
-static int pvt_h_nerr = (sizeof pvt_h_errlist / sizeof pvt_h_errlist[0]);
-
-#if defined(hpux) || defined(__hpux) || defined(_SCO_DS)
-extern int h_errno;
+/* If we're running the test suite, rename hstrerror to avoid conflicts with
+   the system version. */
+#if TESTING
+# define hstrerror test_hstrerror
 #endif
 
-/* return a friendly string for the current value of h_errno. Pinched from
-   Stevens */
-const char *hstrerror(int err)
+const char *
+hstrerror(int error)
 {
-  static char buf[SMBUF];
-  if (err != 0)
-    {
-      if (err > 0 && err < pvt_h_nerr)
-        return(pvt_h_errlist[err]) ;
-      else {
-        sprintf (buf,"(herrno = %d)", h_errno) ;
-	return(buf);
-      }
-    }
-  else
-    return("Resolver Error 0 (no error)");
+    static char buf[32];
+
+    if (error == -1) return "Internal resolver error";
+    if (error >= 0 && error < nerrors) return errors[error];
+
+    /* Paranoia.  If an int is very large (like 128 bytes) one could
+       overflow the buffer here, so refuse to process particularly large
+       values of error. */
+    if (error > 999999 || error < -99999) return "";
+    sprintf(buf, "Resolver error %d", error);
+    return buf;
 }
