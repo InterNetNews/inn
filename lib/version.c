58a4a505c06e440f3c4878cec8c8a12ad5defaee
Russ Allbery <eagle@eyrie.org>
2000-10-01 01:59:45 +0000

    Remove INNVersion(), minor formatting changes.

--- lib/version.c
+++ lib/version.c
@@ -1,20 +1,13 @@
-/*
- * $Id$
- */
+/*  $Id$
+**
+**  INN compile-time version information.
+*/
+
+#include "config.h"
 #include "inn/version.h"
 
-const int inn_version[3] = { INN_VERSION_MAJOR,
-                             INN_VERSION_MINOR,
-                             INN_VERSION_PATCH  };
+const int inn_version[3] = {
+    INN_VERSION_MAJOR, INN_VERSION_MINOR, INN_VERSION_PATCH
+};
 const char inn_version_extra[]  = INN_VERSION_EXTRA;
 const char inn_version_string[] = INN_VERSION_STRING;
-
-/* This function is deprecated, and is provided solely for backwards
-   compatibility for non-INN users of libinn.  Callers should reference
-   inn_version_string directly instead. */
-const char *
-INNVersion(void)
-{
-    return inn_version_string;
-}
-
