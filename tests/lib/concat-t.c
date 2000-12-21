71bd571e27e73c9b63c4b3a0068068c180bdc090
Russ Allbery <eagle@eyrie.org>
2000-12-21 03:47:31 +0000

    Add tests for concatpath.

--- tests/lib/concat-t.c
+++ tests/lib/concat-t.c
@@ -17,13 +17,17 @@ ok(int n, int success)
 int
 main(void)
 {
-    printf("7\n");
-    ok(1, !strcmp("a",     concat("a",                   END)));
-    ok(2, !strcmp("ab",    concat("a", "b",              END)));
-    ok(3, !strcmp("ab",    concat("ab", "",              END)));
-    ok(4, !strcmp("ab",    concat("", "ab",              END)));
-    ok(5, !strcmp("",      concat("",                    END)));
-    ok(6, !strcmp("abcde", concat("ab", "c", "", "de",   END)));
-    ok(7, !strcmp("abcde", concat("abc", "de", END, "f", END)));
+    printf("11\n");
+    ok( 1, !strcmp("a",     concat("a",                   END)));
+    ok( 2, !strcmp("ab",    concat("a", "b",              END)));
+    ok( 3, !strcmp("ab",    concat("ab", "",              END)));
+    ok( 4, !strcmp("ab",    concat("", "ab",              END)));
+    ok( 5, !strcmp("",      concat("",                    END)));
+    ok( 6, !strcmp("abcde", concat("ab", "c", "", "de",   END)));
+    ok( 7, !strcmp("abcde", concat("abc", "de", END, "f", END)));
+    ok( 8, !strcmp("/foo",             concatpath("/bar", "/foo")));
+    ok( 9, !strcmp("/foo/bar",         concatpath("/foo", "bar")));
+    ok(10, !strcmp("./bar",            concatpath("/foo", "./bar")));
+    ok(11, !strcmp("/bar/baz/foo/bar", concatpath("/bar/baz", "foo/bar")));
     return 0;
 }
