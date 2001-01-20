a4b0e15e1695e201d60cc190713079375b8cb79e
Katsuhiro Kondou <kondou@nec.co.jp>
2001-01-20 05:52:38 +0000

    - From: Kenichi Okada <okada@opaopa.org> - sys/time.h and time.h needs to be included if timeval is used and   TIME_WITH_SYS_TIME is defined

--- frontends/ovdb_server.c
+++ frontends/ovdb_server.c
@@ -42,6 +42,17 @@
                                  && (((unsigned)(status)) & 0xFF00) == 0)
 #endif
 
+#ifdef TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# ifdef HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
 #include "libinn.h"
 #include "macros.h"
 #include "paths.h"
