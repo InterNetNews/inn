6ddd4c5ee3c80f69fd580deb9471280b9dd43c45
Russ Allbery <eagle@eyrie.org>
2001-02-03 07:29:26 +0000

    Add an implementation of printfiles (that does nothing, since nothing is actually stored in trash).

--- storage/trash/trash.h
+++ storage/trash/trash.h
@@ -17,6 +17,7 @@ void trash_freearticle(ARTHANDLE *article);
 BOOL trash_cancel(TOKEN token);
 BOOL trash_ctl(PROBETYPE type, TOKEN *token, void *value);
 BOOL trash_flushcacheddata(FLUSHTYPE type);
+void trash_printfiles(FILE *file, TOKEN token, char **xref, int ngroups);
 void trash_shutdown(void);
 
 #endif
