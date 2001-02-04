a5118b512b7110b986a102f24eaf809b28d090e5
Katsuhiro Kondou <kondou@nec.co.jp>
2001-02-04 14:53:22 +0000

    - innconf does not need to be read at dbz

--- lib/dbz.c
+++ lib/dbz.c
@@ -785,10 +785,6 @@ int dbzinit(const char *name) {
     tagboth = tagbits | taghere;
     canttag_warned = 0;
 #else
-    if ((innconf == NULL) && (ReadInnConf() < 0)) {
-	Fclose(dirf);
-	return FALSE;
-    }
     if (!openhashtable(name, idx, &idxtab, sizeof(of_t), options.pag_incore)) {
 	Fclose(dirf);
 	return FALSE;
