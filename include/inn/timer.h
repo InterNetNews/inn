b38fcd47716bde9d8f8fc6e1e84e004cdef8a75a
Russ Allbery <eagle@eyrie.org>
2001-02-07 08:40:05 +0000

    Interface for timer library, from Alex Kiernan <alexk@demon.net>.

---
+++ include/inn/timer.h
@@ -0,0 +1,34 @@
+/*  $Id$
+**
+**  Timer library interface.
+**
+**  An interface to a simple profiling library.  An application can declare
+**  its intent to use n timers by calling TMRinit(n), and then start and
+**  stop numbered timers with TMRstart and TMRstop.  TMRsummary logs the
+**  results to syslog given labels for each numbered timer.
+*/
+
+#ifndef INN_TIMER_H
+#define INN_TIMER_H
+
+#include <inn/defines.h>
+
+BEGIN_DECLS
+
+enum {
+    TMR_HISHAVE,                /* Looking up ID in history (yes/no). */
+    TMR_HISGREP,                /* Looking up ID in history (data). */
+    TMR_HISWRITE,               /* Writing to history. */
+    TMR_HISSYNC,                /* Syncing history to disk. */
+    TMR_APPLICATION             /* Application numbering starts here. */
+};
+
+void            TMRinit(unsigned int);
+void            TMRstart(unsigned int);
+void            TMRstop(unsigned int);
+void            TMRsummary(const char *const *labels);
+unsigned long   TMRnow(void);
+
+END_DECLS
+
+#endif /* INN_TIMER_H */
