5c425010db5b2bb28e03c766c82b2131e2787600
Russ Allbery <eagle@eyrie.org>
2001-06-29 23:20:27 +0000

    Declare private functions as static.  Pass allocated strings to the hash table so that they can be freed on hash_delete and to avoid type system violations.

--- tests/lib/hashtab-t.c
+++ tests/lib/hashtab-t.c
@@ -19,13 +19,13 @@ ok(int n, int success)
     printf("%sok %d\n", success ? "" : "not ", n);
 }
 
-const void *
+static const void *
 string_key(const void *entry)
 {
     return entry;
 }
 
-bool
+static bool
 string_equal(const void *key, const void *entry)
 {
     const char *p, *q;
@@ -35,13 +35,13 @@ string_equal(const void *key, const void *entry)
     return !strcmp(p, q);
 }
 
-void
+static void
 string_delete(void *entry)
 {
     free(entry);
 }
 
-void
+static void
 string_traverse(void *entry, void *data)
 {
     int i;
@@ -63,10 +63,16 @@ main(void)
     int reported, i;
     char buffer[1024];
     char *word;
+    char *test, *testing, *strange, *change;
 
     struct wordref wordrefs[4] = {
         { "test", 0 }, { "testing", 0 }, { "change", 0 }, { NULL, 0 }
     };
+
+    test = xstrdup("test");
+    testing = xstrdup("testing");
+    strange = xstrdup("strange");
+    change = xstrdup("change");
 
     puts("26");
     hash = hash_create(4, hash_string, string_key, string_equal,
@@ -75,21 +81,22 @@ main(void)
     if (hash == NULL)
         abort();
 
-    ok(2, hash_insert(hash, "test", "test"));
+    ok(2, hash_insert(hash, "test", test));
     ok(3, hash_collisions(hash) == 0);
     ok(4, hash_expansions(hash) == 0);
     ok(5, hash_searches(hash) == 1);
     word = hash_lookup(hash, "test");
     ok(6, word != NULL && !strcmp("test", word));
     ok(7, hash_delete(hash, "test"));
+    test = xstrdup("test");
     ok(8, hash_lookup(hash, "test") == NULL);
     ok(9, !hash_delete(hash, "test"));
-    ok(10, !hash_replace(hash, "test", "testing"));
-    ok(11, hash_insert(hash, "test", "test"));
-    ok(12, hash_insert(hash, "testing", "testing"));
-    ok(13, hash_insert(hash, "strange", "strange"));
+    ok(10, !hash_replace(hash, "test", testing));
+    ok(11, hash_insert(hash, "test", test));
+    ok(12, hash_insert(hash, "testing", testing));
+    ok(13, hash_insert(hash, "strange", strange));
     ok(14, hash_expansions(hash) == 0);
-    ok(15, hash_insert(hash, "change", "change"));
+    ok(15, hash_insert(hash, "change", change));
     ok(16, hash_expansions(hash) == 1);
     word = hash_lookup(hash, "testing");
     ok(17, word != NULL && !strcmp("testing", word));
@@ -98,6 +105,7 @@ main(void)
     ok(19, hash_lookup(hash, "thingie") == NULL);
     ok(20, !hash_delete(hash, "thingie"));
     ok(21, hash_delete(hash, "strange"));
+    strange = xstrdup("strange");
     ok(22, hash_lookup(hash, "strange") == NULL);
 
     hash_traverse(hash, string_traverse, &wordrefs[0]);
