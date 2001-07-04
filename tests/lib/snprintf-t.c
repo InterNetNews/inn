6079dede72b61f026d1b712ade7c2ccd435a217b
Russ Allbery <eagle@eyrie.org>
2001-07-04 07:05:01 +0000

    Add Hrvoje Niksic's tests for long long support, which also required massaging buffer sizes since some of the results were too long for the buffer size that was being used.

--- tests/lib/snprintf-t.c
+++ tests/lib/snprintf-t.c
@@ -10,19 +10,29 @@ int test_vsnprintf(char *str, size_t count, const char *fmt, va_list args);
 
 static const char string[] = "abcdefghijklmnopqrstuvwxyz0123456789";
 
-static const char * const fp_formats[] = {
+static const char *const fp_formats[] = {
     "%-1.5f",   "%1.5f",    "%31.9f",   "%10.5f",   "% 10.5f",  "%+22.9f",
     "%+4.9f",   "%01.3f",   "%3.1f",    "%3.2f",    "%.0f",     "%.1f",
     "%f",       NULL
 };
-static const char * const int_formats[] = {
+static const char *const int_formats[] = {
     "%-1.5d",   "%1.5d",    "%31.9d",   "%5.5d",    "%10.5d",   "% 10.5d",
     "%+22.30d", "%01.3d",   "%4d",      "%d",       "%ld",      NULL
 };
-static const char * const uint_formats[] = {
+static const char *const uint_formats[] = {
     "%-1.5lu",  "%1.5lu",   "%31.9lu",  "%5.5lu",   "%10.5lu",  "% 10.5lu",
     "%+6.30lu", "%01.3lu",  "%4lu",     "%lu",      "%4lx",     "%4lX",
     "%01.3lx",  "%1lo",     NULL
+};
+static const char *const llong_formats[] = {
+    "%lld",     "%-1.5lld",  "%1.5lld",    "%123.9lld",  "%5.5lld",
+    "%10.5lld", "% 10.5lld", "%+22.33lld", "%01.3lld",   "%4lld",
+    NULL
+};
+static const char *const ullong_formats[] = {
+    "%llu",     "%-1.5llu",  "%1.5llu",    "%123.9llu",  "%5.5llu",
+    "%10.5llu", "% 10.5llu", "%+22.33llu", "%01.3llu",   "%4llu",
+    "%llx",     "%llo",      NULL
 };
 
 static const double fp_nums[] = {
@@ -35,18 +45,31 @@ static long int_nums[] = {
 static unsigned long uint_nums[] = {
     (unsigned long) -1, 134, 91340, 341, 0203, 0
 };
+static long long llong_nums[] = {
+    ~(long long) 0,                     /* All-1 bit pattern. */
+    (~(unsigned long long) 0) >> 1,     /* Largest signed long long. */
+    -150, 134, 91340, 341,
+    0
+};
+static unsigned long long ullong_nums[] = {
+    ~(long long) 0,                     /* All-1 bit pattern. */
+    (~(unsigned long long) 0) >> 1,     /* Largest signed long long. */
+    134, 91340, 341,
+    0
+};
 
 #define ARRAY_SIZE(array)       sizeof(array) / sizeof(array[0])
 
 static void
-ok_format(int n, const char *expected, int count, const char *format, ...)
+ok_format(int n, bool truncate, const char *expected, int count,
+          const char *format, ...)
 {
-    char buf[32];
+    char buf[128];
     int result;
     va_list args;
 
     va_start(args, format);
-    result = test_vsnprintf(buf, sizeof(buf), format, args);
+    result = test_vsnprintf(buf, truncate ? 32 : sizeof(buf), format, args);
     va_end(args);
     if (!strcmp(buf, expected) && result == count) {
         printf("ok %d\n", n);
@@ -72,11 +95,14 @@ main(void)
     unsigned int j;
     long lcount;
     char buf[32];
+    char lgbuf[128];
 
     printf("%d\n",
            (25 + (ARRAY_SIZE(fp_formats) - 1) * ARRAY_SIZE(fp_nums)
             + (ARRAY_SIZE(int_formats) - 1) * ARRAY_SIZE(int_nums)
-            + (ARRAY_SIZE(uint_formats) - 1) * ARRAY_SIZE(uint_nums)));
+            + (ARRAY_SIZE(uint_formats) - 1) * ARRAY_SIZE(uint_nums)
+            + (ARRAY_SIZE(llong_formats) - 1) * ARRAY_SIZE(llong_nums)
+            + (ARRAY_SIZE(ullong_formats) - 1) * ARRAY_SIZE(ullong_nums)));
 
     ok(1, test_snprintf(NULL, 0, "%s", "abcd") == 4);
     ok(2, test_snprintf(NULL, 0, "%d", 20) == 2);
@@ -84,46 +110,62 @@ main(void)
     ok(4, test_snprintf(NULL, 0, "%c", 'a') == 1);
     ok(5, test_snprintf(NULL, 0, "") == 0);
 
-    ok_format(6, "abcd", 4, "%s", "abcd");
-    ok_format(7, "20", 2, "%d", 20);
-    ok_format(8, "Test ab", 7, "Test %.2s", "abcd");
-    ok_format(9, "a", 1, "%c", 'a');
-    ok_format(10, "", 0, "");
-    ok_format(11, "abcdefghijklmnopqrstuvwxyz01234", 36, "%s", string);
-    ok_format(12, "abcdefghij", 10, "%.10s", string);
-    ok_format(13, "  abcdefghij", 12, "%12.10s", string);
-    ok_format(14, "    abcdefghijklmnopqrstuvwxyz0", 40, "%40s", string);
-    ok_format(15, "abcdefghij    ", 14, "%-14.10s", string);
-    ok_format(16, "              abcdefghijklmnopq", 50, "%50s", string);
-    ok_format(17, "%abcd%", 6, "%%%0s%%", "abcd");
-    ok_format(18, "", 0, "%.0s", string);
-    ok_format(19, "abcdefghijklmnopqrstuvwxyz  444", 32, "%.26s  %d",
+    ok_format(6, true, "abcd", 4, "%s", "abcd");
+    ok_format(7, true, "20", 2, "%d", 20);
+    ok_format(8, true, "Test ab", 7, "Test %.2s", "abcd");
+    ok_format(9, true, "a", 1, "%c", 'a');
+    ok_format(10, true, "", 0, "");
+    ok_format(11, true, "abcdefghijklmnopqrstuvwxyz01234", 36, "%s", string);
+    ok_format(12, true, "abcdefghij", 10, "%.10s", string);
+    ok_format(13, true, "  abcdefghij", 12, "%12.10s", string);
+    ok_format(14, true, "    abcdefghijklmnopqrstuvwxyz0", 40, "%40s",
+              string);
+    ok_format(15, true, "abcdefghij    ", 14, "%-14.10s", string);
+    ok_format(16, true, "              abcdefghijklmnopq", 50, "%50s",
+              string);
+    ok_format(17, true, "%abcd%", 6, "%%%0s%%", "abcd");
+    ok_format(18, true, "", 0, "%.0s", string);
+    ok_format(19, true, "abcdefghijklmnopqrstuvwxyz  444", 32, "%.26s  %d",
               string, 4444);
-    ok_format(20, "abcdefghijklmnopqrstuvwxyz  -2.", 32, "%.26s  %.1f",
+    ok_format(20, true, "abcdefghijklmnopqrstuvwxyz  -2.", 32, "%.26s  %.1f",
               string, -2.5);
 
-    ok_format(21, "abcdefghij4444", 14, "%.10s%n%d", string, &count, 4444);
+    ok_format(21, true, "abcdefghij4444", 14, "%.10s%n%d", string, &count,
+              4444);
     ok(22, count == 10);
-    ok_format(23, "abcdefghijklmnopqrstuvwxyz01234", 36, "%ln%s%n", &count,
-              string, &lcount);
+    ok_format(23, true, "abcdefghijklmnopqrstuvwxyz01234", 36, "%ln%s%n",
+              &count, string, &lcount);
     ok(24, count == 0);
     ok(25, lcount == 31);
 
     n = 25;
     for (i = 0; fp_formats[i] != NULL; i++)
         for (j = 0; j < ARRAY_SIZE(fp_nums); j++) {
-            count = sprintf(buf, fp_formats[i], fp_nums[j]);
-            ok_format(++n, buf, count, fp_formats[i], fp_nums[j]);
+            count = sprintf(lgbuf, fp_formats[i], fp_nums[j]);
+            ok_format(++n, false, lgbuf, count, fp_formats[i], fp_nums[j]);
         }
     for (i = 0; int_formats[i] != NULL; i++)
         for (j = 0; j < ARRAY_SIZE(int_nums); j++) {
-            count = sprintf(buf, int_formats[i], int_nums[j]);
-            ok_format(++n, buf, count, int_formats[i], int_nums[j]);
+            count = sprintf(lgbuf, int_formats[i], int_nums[j]);
+            ok_format(++n, false, lgbuf, count, int_formats[i], int_nums[j]);
         }
     for (i = 0; uint_formats[i] != NULL; i++)
         for (j = 0; j < ARRAY_SIZE(uint_nums); j++) {
-            count = sprintf(buf, uint_formats[i], uint_nums[j]);
-            ok_format(++n, buf, count, uint_formats[i], uint_nums[j]);
+            count = sprintf(lgbuf, uint_formats[i], uint_nums[j]);
+            ok_format(++n, false, lgbuf, count, uint_formats[i],
+                      uint_nums[j]);
+        }
+    for (i = 0; llong_formats[i] != NULL; i++)
+        for (j = 0; j < ARRAY_SIZE(llong_nums); j++) {
+            count = sprintf(lgbuf, llong_formats[i], llong_nums[j]);
+            ok_format(++n, false, lgbuf, count, llong_formats[i],
+                      llong_nums[j]);
+        }
+    for (i = 0; ullong_formats[i] != NULL; i++)
+        for (j = 0; j < ARRAY_SIZE(ullong_nums); j++) {
+            count = sprintf(lgbuf, ullong_formats[i], ullong_nums[j]);
+            ok_format(++n, false, lgbuf, count, ullong_formats[i],
+                      ullong_nums[j]);
         }
 
     return 0;
