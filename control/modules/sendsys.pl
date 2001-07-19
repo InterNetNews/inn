28e04ea8b5845dbdc86a34eada444d7693791504
Russ Allbery <eagle@eyrie.org>
2001-07-19 00:32:56 +0000

    Add a standard header to all files using the comment convention used elsewhere in INN for Perl code, and state the license (only the first two clauses of the general INN license).

--- control/modules/sendsys.pl
+++ control/modules/sendsys.pl
@@ -1,6 +1,22 @@
-# $Id$
-# Copyright 2001 by Marco d'Itri <md@linux.it>
+##  $Id$
+##
+##  sendsys control message handler.
+##
+##  Copyright 2001 by Marco d'Itri <md@linux.it>
+##
+##  Redistribution and use in source and binary forms, with or without
+##  modification, are permitted provided that the following conditions
+##  are met:
+##
+##   1. Redistributions of source code must retain the above copyright
+##      notice, this list of conditions and the following disclaimer.
+##
+##   2. Redistributions in binary form must reproduce the above copyright
+##      notice, this list of conditions and the following disclaimer in the
+##      documentation and/or other materials provided with the distribution.
+
 use strict;
+
 sub control_sendsys {
     my ($par, $sender, $replyto, $site, $action, $log, $approved,
         $headers, $body) = @_;
