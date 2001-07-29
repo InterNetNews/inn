e0734c388eb14226eac18e4c9a07a50f8c692308
Katsuhiro Kondou <kondou@nec.co.jp>
2001-07-29 14:36:39 +0000

    - From: christian mock <cm@tahina.priv.at> - fixes compilation error with gcc3.0

--- nnrpd/nnrpd.h
+++ nnrpd/nnrpd.h
@@ -191,7 +191,7 @@ extern char		*Glom();
 extern int		Argify();
 extern NORETURN		ExitWithStats(int x, BOOL readconf);
 extern BOOL		GetGroupList();
-extern char		*GetHeader();
+extern char		*GetHeader(char *header, bool IsLineS);
 extern void		GRPreport();
 extern void		HIScheck();
 extern char		*HISgetent();
