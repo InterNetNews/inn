48abdb336d2fd5e773ad2f9e43f8873272752a94
Katsuhiro Kondou <kondou@nec.co.jp>
2001-09-21 15:11:09 +0000

    - show correct error message, if stat()'ing cycbuffer fails

--- storage/cnfs/cnfs.c
+++ storage/cnfs/cnfs.c
@@ -342,7 +342,7 @@ static bool CNFSparse_part_line(char *l) {
   memset(cycbuff->path, '\0', CNFSPASIZ);
   strcpy(cycbuff->path, l);
   if (stat(cycbuff->path, &sb) < 0) {
-    syslog(L_ERROR, "%s: file '%s' does not exist, ignoring '%s' cycbuff",
+    syslog(L_ERROR, "%s: file '%s' : %m, ignoring '%s' cycbuff",
 	   LocalLogName, cycbuff->path, cycbuff->name);
     DISPOSE(cycbuff);
     return FALSE;
