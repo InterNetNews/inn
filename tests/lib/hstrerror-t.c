dc2cce52a14ba23c05052d97f0fd25ac1710e48b
Russ Allbery <eagle@eyrie.org>
2001-12-12 09:20:10 +0000

    Use ok_string() and rename the test function to test_error() from ok().

--- tests/lib/hstrerror-t.c
+++ tests/lib/hstrerror-t.c
@@ -5,25 +5,28 @@
 #include <netdb.h>
 #include <stdio.h>
 
+#include "libtest.h"
+
 const char *test_hstrerror(int);
 
 static void
-ok(int n, int success)
+test_error(int n, const char *expected, int error)
 {
-    printf("%sok %d\n", success ? "" : "not ", n);
+    ok_string(n, expected, test_hstrerror(error));
 }
 
 int
 main(void)
 {
     puts("7");
-    ok(1, !strcmp(test_hstrerror(-1), "Internal resolver error"));
-    ok(2, !strcmp(test_hstrerror(0), "No resolver error"));
-    ok(3, !strcmp(test_hstrerror(NO_ADDRESS),
-                  "No address associated with name"));
-    ok(4, !strcmp(test_hstrerror(77777), "Resolver error 77777"));
-    ok(5, !strcmp(test_hstrerror(-99999), "Resolver error -99999"));
-    ok(6, !strcmp(test_hstrerror(1000000), ""));
-    ok(7, !strcmp(test_hstrerror(-100000), ""));
+
+    test_error(1, "Internal resolver error", -1);
+    test_error(2, "No resolver error", 0);
+    test_error(3, "No address associated with name", NO_ADDRESS);
+    test_error(4, "Resolver error 777777", 777777);
+    test_error(5, "Resolver error -99999", -99999);
+    test_error(6, "", 1000000);
+    test_error(7, "", -100000);
+
     return 0;
 }
