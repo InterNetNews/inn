f964b68ddb67be84c85aade995a4d4a3b6a57a37
Russ Allbery <eagle@eyrie.org>
2001-12-12 09:16:18 +0000

    Use the libtest.c ok() and ok_string() functions and break tests that were testing multiple things at the same time into separate tests.

--- tests/lib/date-t.c
+++ tests/lib/date-t.c
@@ -6,6 +6,7 @@
 #include <time.h>
 
 #include "libinn.h"
+#include "libtest.h"
 
 #define ARRAY_SIZE(array)       sizeof(array) / sizeof(array[0])
 
@@ -30,26 +31,6 @@ static const time_t test_times[] = {
 };
 
 static void
-ok_string(int n, bool status, const char *wanted, const char *seen)
-{
-    if (strcmp(wanted, seen) != 0)
-        printf("not ok %d\n  wanted: %s\n    seen: %s\n", n, wanted, seen);
-    else if (!status)
-        printf("not ok %d\n  makedate returned false\n", n);
-    else
-        printf("ok %d\n", n);
-}
-
-static void
-ok_bool(int n, bool wanted, bool seen)
-{
-    if (wanted == seen)
-        printf("ok %d\n", n);
-    else
-        printf("not ok %d\n  wanted %d seen %d\n", n, wanted, seen);
-}
-
-static void
 ok_time(int n, time_t right, const char *date, const char *hour, bool local)
 {
     time_t seen;
@@ -61,12 +42,6 @@ ok_time(int n, time_t right, const char *date, const char *hour, bool local)
         printf("not ok %d\n  wanted %lu seen %lu\n  %s %s %d\n", n,
                (unsigned long) right, (unsigned long) seen, date, hour,
                local);
-}
-
-static void
-ok(int n, int success)
-{
-    printf("%sok %d\n", success ? "" : "not ", n);
 }
 
 static void
@@ -102,7 +77,7 @@ main(void)
     int n;
     unsigned int i;
 
-    printf("%d\n", 33 + ARRAY_SIZE(test_times) * 3 + 3);
+    printf("%d\n", 40 + ARRAY_SIZE(test_times) * 3 + 3);
 
     now = time(NULL);
     status = makedate(-1, false, buff, sizeof(buff));
@@ -119,65 +94,77 @@ main(void)
     }
     ok(2, status && diff >= 0 && diff < 10);
 
+    /* This next line may produce warnings because under SUSv2 putenv is
+       prototyped to take a char * instead of a const char *.  This is
+       obviously stupid; the lack of a cast is intentional. */
     putenv("TZ=PST8PDT");
     tzset();
 
     status = makedate(100000000UL, false, buff, sizeof(buff));
-    ok_string(3, status, "Sat, 3 Mar 1973 09:46:40 +0000 (UTC)", buff);
+    ok(3, status);
+    ok_string(4, "Sat, 3 Mar 1973 09:46:40 +0000 (UTC)", buff);
     status = makedate(100000000UL, true, buff, sizeof(buff));
-    ok_string(4, status, "Sat, 3 Mar 1973 01:46:40 -0800 (PST)", buff);
+    ok(5, status);
+    ok_string(6, "Sat, 3 Mar 1973 01:46:40 -0800 (PST)", buff);
     status = makedate(300000000UL, false, buff, sizeof(buff));
-    ok_string(5, status, "Thu, 5 Jul 1979 05:20:00 +0000 (UTC)", buff);
+    ok(7, status);
+    ok_string(8, "Thu, 5 Jul 1979 05:20:00 +0000 (UTC)", buff);
     status = makedate(300000000UL, true, buff, sizeof(buff));
-    ok_string(6, status, "Wed, 4 Jul 1979 22:20:00 -0700 (PDT)", buff);
+    ok(9, status);
+    ok_string(10, "Wed, 4 Jul 1979 22:20:00 -0700 (PDT)", buff);
 
     status = makedate(300000000UL, false, buff, 32);
-    ok_bool(7, false, status);
+    ok(11, !status);
     status = makedate(300000000UL, false, buff, 33);
-    ok_string(8, status, "Thu, 5 Jul 1979 05:20:00 +0000", buff);
+    ok(12, status);
+    ok_string(13, "Thu, 5 Jul 1979 05:20:00 +0000", buff);
     status = makedate(300000000UL, true, buff, 33);
-    ok_string(9, status, "Wed, 4 Jul 1979 22:20:00 -0700", buff);
+    ok(14, status);
+    ok_string(15, "Wed, 4 Jul 1979 22:20:00 -0700", buff);
 
+    /* May produce warnings; see above. */
     putenv("TZ=Canada/Newfoundland");
     tzset();
 
     status = makedate(900000045UL, true, buff, sizeof(buff));
-    ok_string(10, status, "Thu, 9 Jul 1998 13:30:45 -0230 (NDT)", buff);
+    ok(16, status);
+    ok_string(17, "Thu, 9 Jul 1998 13:30:45 -0230 (NDT)", buff);
 
+    /* May produce warnings; see above. */
     putenv("TZ=PST8PDT");
     tzset();
 
-    ok_time(11, (time_t) -1, "20000132", "000000", false);
-    ok_time(12, (time_t) -1, "20000132", "000000", true);
-    ok_time(13, (time_t) -1, "20000230", "000000", false);
-    ok_time(14, (time_t) -1, "20000230", "000000", true);
-    ok_time(15, (time_t) -1, "19990229", "000000", false);
-    ok_time(16, (time_t) -1, "19990229", "000000", true);
-    ok_time(17, (time_t) -1, "19990020", "000000", false);
-    ok_time(18, (time_t) -1, "19990120", "240000", false);
-    ok_time(19, (time_t) -1, "19990120", "146000", false);
-    ok_time(20, (time_t) -1, "19990120", "145961", false);
-    ok_time(21, (time_t) -1,   "691231", "235959", false);
-    ok_time(22, (time_t) -1, "19691231", "235959", false);
-    ok_time(23, (time_t) -1, "19700100", "000000", false);
-    ok_time(24,           0, "19700101", "000000", false);
-    ok_time(25,           0,   "700101", "000000", false);
-    ok_time(26, (time_t) -1, "2000010101", "000000", false);
-    ok_time(27, (time_t) -1,    "00101", "000000", false);
-    ok_time(28, (time_t) -1, "20000101",  "11111", false);
-    ok_time(29, (time_t) -1, "20000101", "1111111", false);
-    ok_time(30, (time_t) -1, "200001a1", "000000", false);
-    ok_time(31, (time_t) -1, "20000101", "00a000", false);
+    ok_time(18, (time_t) -1, "20000132", "000000", false);
+    ok_time(19, (time_t) -1, "20000132", "000000", true);
+    ok_time(20, (time_t) -1, "20000230", "000000", false);
+    ok_time(21, (time_t) -1, "20000230", "000000", true);
+    ok_time(22, (time_t) -1, "19990229", "000000", false);
+    ok_time(23, (time_t) -1, "19990229", "000000", true);
+    ok_time(24, (time_t) -1, "19990020", "000000", false);
+    ok_time(25, (time_t) -1, "19990120", "240000", false);
+    ok_time(26, (time_t) -1, "19990120", "146000", false);
+    ok_time(27, (time_t) -1, "19990120", "145961", false);
+    ok_time(28, (time_t) -1,   "691231", "235959", false);
+    ok_time(29, (time_t) -1, "19691231", "235959", false);
+    ok_time(30, (time_t) -1, "19700100", "000000", false);
+    ok_time(31,           0, "19700101", "000000", false);
+    ok_time(32,           0,   "700101", "000000", false);
+    ok_time(33, (time_t) -1, "2000010101", "000000", false);
+    ok_time(34, (time_t) -1,    "00101", "000000", false);
+    ok_time(35, (time_t) -1, "20000101",  "11111", false);
+    ok_time(36, (time_t) -1, "20000101", "1111111", false);
+    ok_time(37, (time_t) -1, "200001a1", "000000", false);
+    ok_time(38, (time_t) -1, "20000101", "00a000", false);
 
     /* Times around the fall daylight savings change are ambiguous; accept
        either of the possible interpretations, but make sure we get one or
        the other. */
     result = parsedate_nntp("20001029", "010000", true);
-    ok(32, result == 972806400UL || result == 972810000UL);
+    ok(39, result == 972806400UL || result == 972810000UL);
     result = parsedate_nntp("001029", "013000", true);
-    ok(33, result == 972808200UL || result == 972811800UL);
+    ok(40, result == 972808200UL || result == 972811800UL);
 
-    n = 34;
+    n = 41;
     for (i = 0; i < ARRAY_SIZE(test_times); i++)
         check_nntp(&n, test_times[i]);
     check_nntp(&n, time(NULL));
