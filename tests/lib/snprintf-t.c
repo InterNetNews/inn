8c848b9026ee2b13878503bc65febd1ef9445ba2
Russ Allbery <eagle@eyrie.org>
2001-12-12 09:26:30 +0000

    Rename ok_format to test_format, remove an unused variable, and use ok() from libtest.c.

--- tests/lib/snprintf-t.c
+++ tests/lib/snprintf-t.c
@@ -5,6 +5,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#include "libtest.h"
+
 int test_snprintf(char *str, size_t count, const char *fmt, ...);
 int test_vsnprintf(char *str, size_t count, const char *fmt, va_list args);
 
@@ -61,8 +63,8 @@ static unsigned long long ullong_nums[] = {
 #define ARRAY_SIZE(array)       sizeof(array) / sizeof(array[0])
 
 static void
-ok_format(int n, bool truncate, const char *expected, int count,
-          const char *format, ...)
+test_format(int n, bool truncate, const char *expected, int count,
+            const char *format, ...)
 {
     char buf[128];
     int result;
@@ -82,19 +84,12 @@ ok_format(int n, bool truncate, const char *expected, int count,
     }
 }
 
-static void
-ok(int n, int success)
-{
-    printf("%sok %d\n", success ? "" : "not ", n);
-}
-
 int
 main(void)
 {
     int n, i, count;
     unsigned int j;
     long lcount;
-    char buf[32];
     char lgbuf[128];
 
     printf("%d\n",
@@ -110,31 +105,31 @@ main(void)
     ok(4, test_snprintf(NULL, 0, "%c", 'a') == 1);
     ok(5, test_snprintf(NULL, 0, "") == 0);
 
-    ok_format(6, true, "abcd", 4, "%s", "abcd");
-    ok_format(7, true, "20", 2, "%d", 20);
-    ok_format(8, true, "Test ab", 7, "Test %.2s", "abcd");
-    ok_format(9, true, "a", 1, "%c", 'a');
-    ok_format(10, true, "", 0, "");
-    ok_format(11, true, "abcdefghijklmnopqrstuvwxyz01234", 36, "%s", string);
-    ok_format(12, true, "abcdefghij", 10, "%.10s", string);
-    ok_format(13, true, "  abcdefghij", 12, "%12.10s", string);
-    ok_format(14, true, "    abcdefghijklmnopqrstuvwxyz0", 40, "%40s",
-              string);
-    ok_format(15, true, "abcdefghij    ", 14, "%-14.10s", string);
-    ok_format(16, true, "              abcdefghijklmnopq", 50, "%50s",
-              string);
-    ok_format(17, true, "%abcd%", 6, "%%%0s%%", "abcd");
-    ok_format(18, true, "", 0, "%.0s", string);
-    ok_format(19, true, "abcdefghijklmnopqrstuvwxyz  444", 32, "%.26s  %d",
-              string, 4444);
-    ok_format(20, true, "abcdefghijklmnopqrstuvwxyz  -2.", 32, "%.26s  %.1f",
-              string, -2.5);
-
-    ok_format(21, true, "abcdefghij4444", 14, "%.10s%n%d", string, &count,
-              4444);
+    test_format(6, true, "abcd", 4, "%s", "abcd");
+    test_format(7, true, "20", 2, "%d", 20);
+    test_format(8, true, "Test ab", 7, "Test %.2s", "abcd");
+    test_format(9, true, "a", 1, "%c", 'a');
+    test_format(10, true, "", 0, "");
+    test_format(11, true, "abcdefghijklmnopqrstuvwxyz01234", 36, "%s",
+                string);
+    test_format(12, true, "abcdefghij", 10, "%.10s", string);
+    test_format(13, true, "  abcdefghij", 12, "%12.10s", string);
+    test_format(14, true, "    abcdefghijklmnopqrstuvwxyz0", 40, "%40s",
+                string);
+    test_format(15, true, "abcdefghij    ", 14, "%-14.10s", string);
+    test_format(16, true, "              abcdefghijklmnopq", 50, "%50s",
+                string);
+    test_format(17, true, "%abcd%", 6, "%%%0s%%", "abcd");
+    test_format(18, true, "", 0, "%.0s", string);
+    test_format(19, true, "abcdefghijklmnopqrstuvwxyz  444", 32, "%.26s  %d",
+                string, 4444);
+    test_format(20, true, "abcdefghijklmnopqrstuvwxyz  -2.", 32,
+                "%.26s  %.1f", string, -2.5);
+    test_format(21, true, "abcdefghij4444", 14, "%.10s%n%d", string, &count,
+                4444);
     ok(22, count == 10);
-    ok_format(23, true, "abcdefghijklmnopqrstuvwxyz01234", 36, "%ln%s%n",
-              &count, string, &lcount);
+    test_format(23, true, "abcdefghijklmnopqrstuvwxyz01234", 36, "%ln%s%n",
+                &count, string, &lcount);
     ok(24, count == 0);
     ok(25, lcount == 31);
 
@@ -142,30 +137,31 @@ main(void)
     for (i = 0; fp_formats[i] != NULL; i++)
         for (j = 0; j < ARRAY_SIZE(fp_nums); j++) {
             count = sprintf(lgbuf, fp_formats[i], fp_nums[j]);
-            ok_format(++n, false, lgbuf, count, fp_formats[i], fp_nums[j]);
+            test_format(++n, false, lgbuf, count, fp_formats[i], fp_nums[j]);
         }
     for (i = 0; int_formats[i] != NULL; i++)
         for (j = 0; j < ARRAY_SIZE(int_nums); j++) {
             count = sprintf(lgbuf, int_formats[i], int_nums[j]);
-            ok_format(++n, false, lgbuf, count, int_formats[i], int_nums[j]);
+            test_format(++n, false, lgbuf, count, int_formats[i],
+                        int_nums[j]);
         }
     for (i = 0; uint_formats[i] != NULL; i++)
         for (j = 0; j < ARRAY_SIZE(uint_nums); j++) {
             count = sprintf(lgbuf, uint_formats[i], uint_nums[j]);
-            ok_format(++n, false, lgbuf, count, uint_formats[i],
-                      uint_nums[j]);
+            test_format(++n, false, lgbuf, count, uint_formats[i],
+                        uint_nums[j]);
         }
     for (i = 0; llong_formats[i] != NULL; i++)
         for (j = 0; j < ARRAY_SIZE(llong_nums); j++) {
             count = sprintf(lgbuf, llong_formats[i], llong_nums[j]);
-            ok_format(++n, false, lgbuf, count, llong_formats[i],
-                      llong_nums[j]);
+            test_format(++n, false, lgbuf, count, llong_formats[i],
+                        llong_nums[j]);
         }
     for (i = 0; ullong_formats[i] != NULL; i++)
         for (j = 0; j < ARRAY_SIZE(ullong_nums); j++) {
             count = sprintf(lgbuf, ullong_formats[i], ullong_nums[j]);
-            ok_format(++n, false, lgbuf, count, ullong_formats[i],
-                      ullong_nums[j]);
+            test_format(++n, false, lgbuf, count, ullong_formats[i],
+                        ullong_nums[j]);
         }
 
     return 0;
