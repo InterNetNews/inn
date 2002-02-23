fc7a90214daf330cfa7912d7f4156efb2357cc96
Katsuhiro Kondou <kondou@nec.co.jp>
2002-02-23 05:58:24 +0000

    - LISTGROUP with no arg didn't work properly

--- nnrpd/group.c
+++ nnrpd/group.c
@@ -31,8 +31,12 @@ FUNCTYPE CMDgroup(int ac, char *av[])
 
     /* Parse arguments. */
     if (ac == 1) {
+	if (GRPcur == NULL) {
 	    Printf("%d No group specified\r\n", NNTP_XGTITLE_BAD);
 	    return;
+	} else {
+	    group = GRPcur;
+	}
     } else {
 	group = av[1];
     }
