0c61fba1e9fff607ebf6f97e7aa46ce9b5e20b14
Russ Allbery <eagle@eyrie.org>
2002-03-09 01:04:31 +0000

    Add options to dump the group index data and overview data for a group. Move the option to specify the path to the overview files to -p.

--- storage/tradindexed/tdx-util.c
+++ storage/tradindexed/tdx-util.c
@@ -14,6 +14,7 @@
 #include "libinn.h"
 #include "ov.h"
 #include "tdx-private.h"
+#include "tdx-structure.h"
 
 static void
 dump_index(void)
@@ -31,15 +32,73 @@ static void
 dump_group(const char *group)
 {
     struct group_index *index;
-    unsigned long high, low, count;
-    char flag;
+    struct group_entry *entry;
 
     index = tdx_group_index_open(OV_READ);
     if (index == NULL)
         return;
-    if (!tdx_group_index_info(index, group, &high, &low, &count, &flag))
+    entry = tdx_group_index_entry(index, group);
+    if (entry == NULL)
         return;
-    printf("%s %lu %lu %lu %c\n", group, high, low, count, flag);
+    tdx_group_index_print(group, entry);
+    tdx_group_index_close(index);
+}
+
+static void
+dump_group_index(const char *group)
+{
+    struct group_index *index;
+    struct group_data *data;
+
+    index = tdx_group_index_open(OV_READ);
+    if (index == NULL)
+        return;
+    data = tdx_data_open(index, group);
+    if (data == NULL)
+        return;
+    tdx_data_index_dump(data);
+    tdx_data_close(data);
+    tdx_group_index_close(index);
+}
+
+static void
+dump_overview(const char *group)
+{
+    struct group_index *index;
+    struct group_data *data;
+    struct group_entry *entry;
+    struct article article;
+    struct search *search;
+    char datestring[256];
+
+    index = tdx_group_index_open(OV_READ);
+    if (index == NULL)
+        return;
+    data = tdx_data_open(index, group);
+    if (data == NULL)
+        return;
+
+    entry = tdx_group_index_entry(index, group);
+    if (entry == NULL)
+        return;
+    search = tdx_search_open(data, entry->low, entry->high);
+    if (search == NULL)
+        return;
+    while (tdx_search(search, &article)) {
+        fwrite(article.overview, article.overlen - 2, 1, stdout);
+        printf("\tArticle: %lu\tToken: %s", article.number,
+               TokenToText(article.token));
+        makedate(article.arrived, true, datestring, sizeof(datestring));
+        printf("\tArrived: %s", datestring);
+        if (article.expires != 0) {
+            makedate(article.expires, true, datestring, sizeof(datestring));
+            printf("\tExpires: %s", datestring);
+        }
+        printf("\n");
+    }
+    tdx_search_close(search);
+    tdx_data_close(data);
+    tdx_group_index_close(index);
 }
 
 int
@@ -55,24 +114,37 @@ main(int argc, char *argv[])
         exit(1);
 
     /* Parse options. */
-    while ((option = getopt(argc, argv, "di:o:")) != EOF) {
+    opterr = 0;
+    while ((option = getopt(argc, argv, "dg:i:o:")) != EOF) {
         switch (option) {
         case 'd':
             if (mode != '\0')
-                die("only one of -d and -i allowed");
+                die("only one mode option allowed");
             mode = 'd';
+            break;
+        case 'g':
+            if (mode != '\0')
+                die("only one mode option allowed");
+            mode = 'g';
+            newsgroup = optarg;
             break;
         case 'i':
             if (mode != '\0')
-                die("only one of -d and -i allowed");
+                die("only one mode option allowed");
             mode = 'i';
             newsgroup = optarg;
             break;
         case 'o':
+            if (mode != '\0')
+                die("only one mode option allowed");
+            mode = 'o';
+            newsgroup = optarg;
+            break;
+        case 'p':
             innconf->pathoverview = xstrdup(optarg);
             break;
         default:
-            die("invalid option %c", option);
+            die("invalid option %c", optopt);
             break;
         }
     }
@@ -82,11 +154,17 @@ main(int argc, char *argv[])
     case 'd':
         dump_index();
         break;
+    case 'g':
+        dump_group_index(newsgroup);
+        break;
     case 'i':
         dump_group(newsgroup);
         break;
+    case 'o':
+        dump_overview(newsgroup);
+        break;
     default:
-        die("one of -d or -i must be specified");
+        die("a mode option must be specified");
         break;
     }
     exit(0);
