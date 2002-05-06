908948a04392b6481ab7377ce640ab61926bf1b5
Russ Allbery <eagle@eyrie.org>
2002-05-06 05:44:13 +0000

    Add code to read the overview.fmt configuration and to generate overview data from an article.  (As yet untested.)

--- storage/overdata.c
+++ storage/overdata.c
@@ -10,9 +10,165 @@
 #include "clibrary.h"
 #include <ctype.h>
 
+#include "inn/buffer.h"
+#include "inn/messages.h"
+#include "inn/qio.h"
 #include "inn/vector.h"
 #include "libinn.h"
+#include "macros.h"
 #include "ovinterface.h"
+#include "paths.h"
+
+
+/* The standard overview fields. */
+static const char * const fields[] = {
+    "Subject", "From", "Date", "Message-ID", "References", "Bytes", "Lines"
+};
+
+
+/*
+**  Parse the overview schema and return a vector of the additional fields
+**  over the standard ones.  Caller is responsible for freeing the vector.
+*/
+struct vector *
+overview_extra_fields(void)
+{
+    struct vector *list;
+    char *schema = NULL;
+    char *line, *p;
+    QIOSTATE *qp = NULL;
+    unsigned int field;
+    bool full = false;
+
+    schema = concatpath(innconf->pathetc, _PATH_SCHEMA);
+    qp = QIOopen(schema);
+    if (qp == NULL) {
+        syswarn("cannot open %s", schema);
+        goto fail;
+    }
+    list = vector_new();
+    for (field = 0, line = QIOread(qp); line != NULL; line = QIOread(qp)) {
+        while (ISWHITE(*line))
+            line++;
+        p = strchr(line, '#');
+        if (p != NULL)
+            *p = '\0';
+        p = strchr(line, '\n');
+        if (p != NULL)
+            *p = '\0';
+        if (*line == '\0')
+            continue;
+        p = strchr(line, ':');
+        if (p != NULL) {
+            *p++ = '\0';
+            full = (strcmp(p, "full") == 0);
+        }
+        if (field >= SIZEOF(fields)) {
+            if (!full)
+                warn("additional field %s not marked with :full", line);
+            vector_add(list, line);
+        } else {
+            if (strcasecmp(line, fields[field]) != 0)
+                warn("field %d is %s, should be %s", field, line,
+                     fields[field]);
+        }
+    }
+    if (QIOerror(qp)) {
+        if (QIOtoolong(qp)) {
+            warn("line too long in %s", schema);
+        } else {
+            syswarn("error while reading %s", schema);
+        }
+    }
+    return list;
+
+ fail:
+    if (schema != NULL)
+        free(schema);
+    if (qp != NULL)
+        QIOclose(qp);
+    return NULL;
+}
+
+
+/*
+**  Given an article, its length, the name of a header, and a buffer to append
+**  the data to, append header data for that header to the overview data
+**  that's being constructed.  Doesn't append any data if the header isn't
+**  found.
+*/
+static void
+build_header(const char *article, size_t length, const char *header,
+             struct buffer *overview)
+{
+    ptrdiff_t size;
+    size_t offset;
+    const char *data, *end, *p;
+
+    data = HeaderFindMem(article, length, header, strlen(header));
+    if (data == NULL)
+        return;
+    data += strlen(header) + 1;
+    if (ISWHITE(*header))
+        header++;
+    end = strchr(data, '\n');
+    while (end != NULL && ISWHITE(end[1]))
+        end = strchr(end + 1, '\n');
+    if (end == NULL)
+        return;
+
+    size = end - data + 1;
+    offset = overview->used + overview->left;
+    buffer_resize(overview, offset + size);
+
+    for (p = data; p <= end; p++) {
+        if (*p == '\r' && p[1] == '\n') {
+            p++;
+            continue;
+        }
+        if (*p == '\0' || *p == '\t' || *p == '\n' || *p == '\r')
+            overview->data[offset++] = ' ';
+        else
+            overview->data[offset++] = *p;
+        overview->left++;
+    }
+}
+
+
+/*
+**  Given an article and a vector of additional headers, generate overview
+**  data into the provided buffer.  If the buffer parameter is NULL, a new
+**  buffer is allocated.  This data doesn't include the article number.  The
+**  article should be in wire format.  Returns the buffer containing the
+**  overview data.
+*/
+struct buffer *
+overview_build(ARTNUM number, const char *article, size_t length,
+               struct vector *extra, struct buffer *overview)
+{
+    unsigned int field;
+    char buffer[32];
+
+    snprintf(buffer, sizeof(buffer), "%lu", number);
+    if (overview == NULL)
+        overview = buffer_new();
+    buffer_set(overview, buffer, strlen(buffer));
+    for (field = 0; field < SIZEOF(fields); field++) {
+        buffer_append(overview, "\t", 1);
+        if (field == 5) {
+            snprintf(buffer, sizeof(buffer), "%lu", length);
+            buffer_append(overview, buffer, strlen(buffer));
+        } else
+            build_header(article, length, fields[field], overview);
+    }
+    for (field = 0; field < extra->count; field++) {
+        buffer_append(overview, extra->strings[field],
+                      strlen(extra->strings[field]));
+        buffer_append(overview, ": ", 2);
+        build_header(article, length, extra->strings[field], overview);
+    }
+    return overview;
+}
 
 
 /*
@@ -77,39 +233,39 @@ bool
 overview_check(const char *data, size_t length, ARTNUM article)
 {
     char *copy;
-    struct cvector *fields;
+    struct cvector *overview;
     ARTNUM overnum;
     size_t i;
 
     copy = xstrndup(data, length);
-    fields = cvector_split(copy, '\t', NULL);
+    overview = cvector_split(copy, '\t', NULL);
 
     /* The actual checks.  We don't verify all of the data, since that data
        may be malformed in the article, but we do check to be sure that the
        fields that should be numbers are numbers.  That should catch most
        positional errors.  We can't check Lines yet since right now INN is
        still accepting the value from the post verbatim. */
-    if (fields->count < 8)
+    if (overview->count < 8)
         goto fail;
-    if (!valid_number(fields->strings[0]))
+    if (!valid_number(overview->strings[0]))
         goto fail;
-    overnum = strtoul(fields->strings[0], NULL, 10);
+    overnum = strtoul(overview->strings[0], NULL, 10);
     if (overnum != article)
         goto fail;
-    if (!valid_number(fields->strings[6]))
+    if (!valid_number(overview->strings[6]))
         goto fail;
     for (i = 1; i < 6; i++)
-        if (!valid_overview_string(fields->strings[i], false))
+        if (!valid_overview_string(overview->strings[i], false))
             goto fail;
-    for (i = 8; i < fields->count; i++)
-        if (!valid_overview_string(fields->strings[i], true))
+    for (i = 8; i < overview->count; i++)
+        if (!valid_overview_string(overview->strings[i], true))
             goto fail;
-    cvector_free(fields);
+    cvector_free(overview);
     free(copy);
     return true;
 
  fail:
-    cvector_free(fields);
+    cvector_free(overview);
     free(copy);
     return false;
 }
--- storage/ovinterface.h
+++ storage/ovinterface.h
@@ -11,6 +11,9 @@
 #include "storage.h"
 #include "inn/history.h"
 
+struct buffer;
+struct vector;
+
 typedef struct {
     const char	*name;
     bool	(*open)(int mode);
@@ -36,6 +39,11 @@ bool OVhisthasmsgid(struct history *, const char *data);
 void OVEXPremove(TOKEN token, bool deletedgroups, char **xref, int ngroups);
 void OVEXPcleanup(void);
 
+/* Overview data manipulation functions. */
+struct vector *overview_extra_fields(void);
+struct buffer *overview_build(ARTNUM number, const char *article,
+                              size_t length, struct vector *extra,
+                              struct buffer *);
 bool overview_check(const char *data, size_t length, ARTNUM article);
 
 #define DEFAULT_MAX_XREF_LEN 8192
