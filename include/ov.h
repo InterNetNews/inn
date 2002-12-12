ae6e9579a09b9a441f24f606bb2775eabca99477
Alex Kiernan <alexk@alexk.eng.demon.net>
2002-12-12 09:53:11 +0000

    Add a missing prototype for overview_getheader

--- include/ov.h
+++ include/ov.h
@@ -57,6 +57,7 @@ bool overview_check(const char *data, size_t length, ARTNUM article);
 int overview_index(const char *field, const struct vector *extra);
 struct cvector *overview_split(const char *line, size_t length,
 			       ARTNUM *number, struct cvector *vector);
+char *overview_getheader(struct cvector *vector, int element);
 
 /* offsets into vectors for standard overview headers */
 enum {
