0f23e9504f2d846298f7bae386e6a021bf5a8caf
Jeffrey M. Vinocur <jeff@litech.org>
2002-12-12 05:01:42 +0000

    Add example showing how to check included text ratio (to replace the hardcoded checkincludedtext: in inn.conf).

--- samples/filter_nnrpd.pl
+++ samples/filter_nnrpd.pl
@@ -9,28 +9,66 @@
 # `filter_post', then that function will be called during processing of a
 # posting. It has access to the headers of the article via the associative
 # array `%hdr'. If it returns a null string then the article is accepted
-# for posting. A non-null stringrejects it, and the value returned is used
+# for posting. A non-null string rejects it, and the value returned is used
 # in the rejection message.
 #
+
+#
+# Do any initialization steps.
+#
+my %config = (checkincludedtext => 0,
+              includedcutoff => 40,
+              includedratio => 0.6,
+              quotere => '^[>:]',
+              antiquotere => '^[<]',  # so as not to reject dict(1) output
+             );
+
 
 #
 # Sample filter
 #
 sub filter_post {
-    my $rval = "" ;		# assume we'll accept.
+    my $rval = "" ;             # assume we'll accept.
 
 ### Uncomment this next block to reject articles that have 'make money'
 ### in their subject, or which have a "Re: " subject, but no References:
 ### header, or which have an invalid From.
 
 ##    if ($hdr{"Subject"} =~ /make.*money/i) {
-##	$rval = "Spam is not acceptable here..." ;
+##        $rval = "Spam is not acceptable here..." ;
 ##    } elsif ($hdr{'Subject'} =~ /^Re: /o and $hdr{'References'} eq "") {
-##	$rval = "Followup without References:";
+##        $rval = "Followup without References:";
 ##    } elsif ($hdr{'From'} =~ /^\w*$/o or
-##	     $hdr{'From'} !~ /^(.+?)\@([-\w\d]+\.)*([-\w\d]+)\.([-\w\d]{2,})$/o) {
-##	$rval = "From: is invalid, must be user\@[host.]domain.tld";
+##             $hdr{'From'} !~ /^(.+?)\@([-\w\d]+\.)*([-\w\d]+)\.([-\w\d]{2,})$/o) {
+##        $rval = "From: is invalid, must be user\@[host.]domain.tld";
 ##    }
 
-	$rval ;
+
+### The next block rejects articles with too much quoted text, if the
+### config hash directs it to.
+
+    if ($config{checkincludedtext}) {
+        my ($lines, $quoted, $antiquoted) = analyze($body);
+        if ($lines > $config{includedcutoff}
+                && $quoted - $antiquoted > $lines * $config{includedratio}) {
+            $rval = "Article contains too much quoted text";
+        }
+    }
+
+    return $rval;
+}
+
+sub analyze {
+    my ($lines, $quoted, $antiquoted) = (0, 0, 0);
+    local $_ = shift;
+
+    do {
+        if ( /\G$config{quotere}/mgc ) {
+            $quoted++;
+        } elsif ( /\G$config{antiquotere}/mgc ) {
+            $antiquoted++;
+        }
+    } while ( /\G(.*)\n/gc && ++$lines );
+
+    return ($lines, $quoted, $antiquoted);
 }
