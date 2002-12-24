a03e9926c9cdd9b66b7dff31ee7e54468006ce3d
Russ Allbery <eagle@eyrie.org>
2002-12-24 23:02:25 +0000

    Convert the build method of libstorage and libinnhist to use non-recursive make.
    Add a configure probe for whether the compiler can handle -o and -c at the
    same time, and use the results to choose either those flags or mv to allow
    object files to be built into a directory other than the current one.
    Make buildconfig a bit smarter so that all the makefiles of the storage
    methods can be eliminated, and add support for inclusion of makefile
    fragments to handle special cases for buffindexed and tradindexed.  Fix all
    the rules to not recurse.
    
    This change should fix both dependency handling and parallel make for all
    of the storage and history methods.
    
    Also add a fallback to -ldb with no special compilation flags for linking
    with BerkeleyDB, to handle the current layout in Debian.

---
+++ storage/buffindexed/ovmethod.mk
@@ -0,0 +1,10 @@
+##  $Id$
+
+# This rule requires a compiler that supports -o with -c.  Since it's normally
+# used by developers, that should be acceptable.
+buffindexed/buffindexed_d.o: buffindexed/buffindexed.c
+	$(CC) $(CFLAGS) -DDEBUG -c -o $@ buffindexed/buffindexed.c
+
+buffindexed/debug: buffindexed/buffindexed_d.o libstorage.$(EXTLIB) $(LIBHIST)
+	$(LIBLD) $(LDFLAGS) -o $@ buffindexed/buffindexed_d.o \
+	    $(LIBSTORAGE) $(LIBHIST) $(LIBINN) $(EXTSTORAGELIBS) $(LIBS)
---
+++ storage/tradindexed/ovmethod.mk
@@ -0,0 +1,8 @@
+##  $Id$
+
+tradindexed/tdx-util: tradindexed/tdx-util.o libstorage.$(EXTLIB) $(LIBHIST)
+	$(LIBLD) $(LDFLAGS) -o $@ tradindexed/tdx-util.o \
+	    $(LIBSTORAGE) $(LIBHIST) $(LIBINN) $(EXTSTORAGELIBS) $(LIBS)
+
+$(D)$(PATHBIN)/tdx-util: tradindexed/tdx-util
+	$(LI_XPRI) $? $@
