f391f7ddfa3556ba40693fe760924f42a22f61db
Russ Allbery <eagle@eyrie.org>
2002-12-25 04:54:37 +0000

    Fix error in previous commit:  HAVE_SSL, not HAVE_OPENSSL.

--- nnrpd/nnrpd.c
+++ nnrpd/nnrpd.c
@@ -216,7 +216,7 @@ ExitWithStats(int x, bool readconf)
         syslog(L_NOTICE, "%s overstats count %ld hit %ld miss %ld time %ld size %ld dbz %ld seek %ld get %ld artcheck %ld", ClientHost,
             OVERcount, OVERhit, OVERmiss, OVERtime, OVERsize, OVERdbz, OVERseek, OVERget, OVERartcheck);
 
-#ifdef HAVE_OPENSSL
+#ifdef HAVE_SSL
      if (tls_conn) {
         SSL_shutdown(tls_conn);
         tls_conn = NULL;
