61a972acf255c35e07558f62f66187091161ef50
Russ Allbery <eagle@eyrie.org>
2003-01-19 00:42:35 +0000

    TIMEINFOasDOUBLE is no longer used.

--- include/macros.h
+++ include/macros.h
@@ -11,10 +11,6 @@
 /* <ctype.h> usually includes \n, which is not what we want. */
 #define ISWHITE(c)              ((c) == ' ' || (c) == '\t')
 
-/* Turn a TIMEINFO into a floating point number. */
-#define TIMEINFOasDOUBLE(t)	\
-    ((double)(t).time + ((double)(t).usec) / 1000000.0)
-
 /* Use a read or recv call to read a descriptor. */
 #ifdef HAVE_UNIX_DOMAIN_SOCKETS
 # define RECVorREAD(fd, p, s)   recv((fd), (p), (s), 0)
