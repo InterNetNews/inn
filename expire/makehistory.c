765ab301f468aef79d15b5636467967aa781d962
Russ Allbery <eagle@eyrie.org>
2003-03-28 01:16:40 +0000

    Can't use strlcpy when the source argument is not a nul-terminated string, even if the length argument is less than the valid length of the string. This should fix the core dumps in makehistory when using CNFS.

--- expire/makehistory.c
+++ expire/makehistory.c
@@ -360,8 +360,9 @@ WriteOverLine(TOKEN *token, const char *xrefs, int xrefslen,
 	    return;
 	}
 	/* q points to start of ng name, r points to its end. */
-        assert(sizeof(temp) > r - q);
-	strlcpy(temp, q, r - q + 1);
+        assert(sizeof(temp) > r - q + 1);
+	memcpy(temp, q, r - q + 1);
+        temp[r - q + 1] = '\0';
 	fprintf(OverTmpFile, "%s\t%10lu\t%lu\t%s\t", temp,
                 (unsigned long) arrived, (unsigned long) expires,
                 TokenToText(*token));
