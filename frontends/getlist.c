75074fa989c6d7df816744a5579cea4ee6614f09
Russ Allbery <eagle@eyrie.org>
2003-07-12 19:53:19 +0000

    First pass at using the new NNTP utility library (still needs some work).

--- frontends/getlist.c
+++ frontends/getlist.c
@@ -1,194 +1,299 @@
 /*  $Id$
 **
-**  Get a file list from an NNTP server.
+**  Send a LIST command to an NNTP server and print the results.
 */
+
 #include "config.h"
 #include "clibrary.h"
 #include <errno.h>
-#include <syslog.h>
 
 #include "inn/innconf.h"
 #include "inn/messages.h"
+#include "inn/nntp.h"
 #include "inn/qio.h"
+#include "inn/vector.h"
 #include "libinn.h"
 #include "paths.h"
 
+static const char usage[] = "\
+Usage: getlist [-A] [-h host] [-p port] [list [pattern [types]]]\n\
+\n\
+getlist obtains a list from an NNTP server and prints it out.  By default,\n\
+the active file is retrieved, but any list that the NNTP server supports\n\
+can be requested.  The pattern is passed to the remote server as a filter\n\
+on the returned values.  If the list parameter is \"active\", a third\n\
+parameter can be given, listing the acceptable group types.  Only groups of\n\
+that type (y and m being the most common) are returned.\n";
+
 
 /*
-**  Print usage message and exit.
+**  Print out an appropriate error message for a bad NNTP response code and
+**  exit.
 */
 static void
-Usage(void)
+die_nntp_code(enum nntp_code code, const char *line)
 {
-    fprintf(stderr, "Usage: getlist [-p port] [-h host] [-A] [type [pat [groups]]\n");
-    exit(1);
+    if (code == 0)
+        die("unexpected server response: %s", line);
+    else
+        die("unexpected server response: %03d %s", code, line);
+}
+
+
+/*
+**  Print out an appropriate error message for a bad NNTP status and exit.
+*/
+static void
+die_nntp_status(enum nntp_status status)
+{
+    switch (status) {
+    case NNTP_READ_LONG:
+        die("line from server too long for 128KB buffer");
+    case NNTP_READ_EOF:
+        die("server closed connection unexpectedly");
+    case NNTP_READ_TIMEOUT:
+        die("read from server timed out");
+    default:
+        sysdie("cannot read response from server");
+    }
+}
+
+
+/*
+**  Send a command and pattern to the remote server and make sure we get an
+**  appropriate response.
+*/
+static void
+send_list(struct nntp *nntp, const char *command, const char *pattern)
+{
+    bool okay;
+    enum nntp_status status;
+    enum nntp_code code;
+    char *line;
+
+    if (pattern != NULL)
+        okay = nntp_send_line(nntp, "LIST %s %s", command, pattern);
+    else
+        okay = nntp_send_line(nntp, "LIST %s", command);
+    if (!okay)
+        sysdie("cannot send LIST command to server");
+    status = nntp_read_response(nntp, 10 * 60, &code, &line);
+    if (status != NNTP_READ_OK)
+        die_nntp_status(status);
+    if (code != NNTP_OK_LIST)
+        die_nntp_code(code, line);
+}
+
+
+/*
+**  Print out the results of a LIST command.  Used for generic list commands
+**  or active commands without a type parameter.
+*/
+static void
+print_list(struct nntp *nntp)
+{
+    enum nntp_status status;
+    char *line;
+
+    status = nntp_read_line(nntp, 10 * 60, &line);
+    while (status == NNTP_READ_OK) {
+        if (strcmp(line, ".") == 0)
+            break;
+        printf("%s\n", line);
+        status = nntp_read_line(nntp, 10 * 60, &line);
+    }
+    if (status != NNTP_READ_OK)
+        die_nntp_status(status);
+}
+
+
+/*
+**  Print out the results of a LIST ACTIVE command, limited to particular
+**  group types.
+*/
+static void
+print_active(struct nntp *nntp, const char *types)
+{
+    enum nntp_status status;
+    char *line;
+    struct cvector *group = NULL;
+
+    status = nntp_read_line(nntp, 10 * 60, &line);
+    while (status == NNTP_READ_OK) {
+        if (strcmp(line, ".") == 0)
+            break;
+        group = cvector_split_space(line, group);
+        if (group->count != 4) {
+            warn("malformed line from server: %s", line);
+            continue;
+        }
+        if (strchr(types, group->strings[3][0]) != NULL)
+            printf("%s %s %s %s\n", group->strings[0], group->strings[1],
+                   group->strings[2], group->strings[3]);
+        status = nntp_read_line(nntp, 10 * 60, &line);
+    }
+    if (status != NNTP_READ_OK)
+        die_nntp_status(status);
+}
+
+
+/*
+**  Get the username and password for a remote site from the local password
+**  file.  username and password will be set to newly allocated strings on
+**  success.
+*/
+static bool
+get_authinfo(const char *server, char **username, char **password)
+{
+    char *path, *line;
+    QIOSTATE *passwords;
+    struct cvector *info = NULL;
+
+    path = concatpath(innconf->pathetc, _PATH_NNTPPASS);
+    passwords = QIOopen(path);
+    if (passwords == NULL) {
+        if (errno != ENOENT)
+            warn("cannot open %s", path);
+        return false;
+    }
+    free(path);
+    line = QIOread(passwords);
+    while (line != NULL) {
+        if (line[0] == '\0' || line[0] == '#')
+            continue;
+        info = cvector_split(line, ':', info);
+        if (info->count != 4)
+            continue;
+        if (strcmp(info->strings[2], "authinfo") != 0)
+            continue;
+        if (strcasecmp(info->strings[3], server) != 0)
+            continue;
+        *username = xstrdup(info->strings[0]);
+        *password = xstrdup(info->strings[1]);
+        return true;
+    }
+    return false;
+}
+
+
+/*
+**  Send AUTHINFO information to a remote site.  Returns true if successful,
+**  false on failure.  Problems sending to the remote site (as opposed to just
+**  having the remote site reject the authentication) are fatal.
+*/
+static bool
+send_authinfo(struct nntp *nntp, const char *username, const char *password)
+{
+    enum nntp_status status;
+    enum nntp_code code;
+    char *line;
+
+    if (!nntp_send_line(nntp, "AUTHINFO USER %s", username))
+        sysdie("cannot send AUTHINFO USER to remote server");
+    status = nntp_read_response(nntp, 10 * 60, &code, &line);
+    if (status != NNTP_READ_OK)
+        die_nntp_status(status);
+    if (code == NNTP_OK_AUTHINFO)
+        return true;
+    if (code != NNTP_CONT_AUTHINFO)
+        return false;
+    if (!nntp_send_line(nntp, "AUTHINFO PASS %s", password))
+        sysdie("cannot send AUTHINFO PASS to remote server");
+    status = nntp_read_response(nntp, 10 * 60, &code, &line);
+    if (status != NNTP_READ_OK)
+        die_nntp_status(status);
+    return (code == NNTP_OK_AUTHINFO);
 }
 
 
 int
-main(int ac, char *av[])
+main(int argc, char *argv[])
 {
-    FILE	*active;
-    FILE	*FromServer;
-    FILE	*ToServer;
-    QIOSTATE	*qp;
-    char	*field4;
-    char	*types;
-    char	*host;
-    char	*line;
-    const char	*list;
-    char	*p;
-    char	*pattern;
-    char	buff[512 + 1];
-    int		port;
-    int		authinfo;
-    int		i;
+    struct nntp *nntp;
+    const char *host = NULL;
+    const char *list = "active";
+    const char *pattern = NULL;
+    const char *types = NULL;
+    enum nntp_status status;
+    enum nntp_code response;
+    char *line;
+    unsigned short port = NNTP_PORT;
+    bool authinfo = false;
+    int option;
 
-    /* First thing, set up our identity. */
     message_program_name = "getlist";
-
     if (!innconf_read(NULL))
         exit(1);
+    host = innconf->server;
 
-    /* Set defaults. */
-    host = NULL;
-    pattern = NULL;
-    types = NULL;
-    port = NNTP_PORT;
-    authinfo = 0;
-
-    /* Parse JCL. */
-    while ((i = getopt(ac, av, "Ah:p:")) != EOF)
-	switch (i) {
-	default:
-	    Usage();
-	    /* NOTREACHED */
-	case 'A':
-	    authinfo = 1;
-	    break;
-	case 'h':
-	    host = optarg;
-	    break;
-	case 'p':
-	    port = atoi(optarg);
-	    if (port <= 0)
-                die("illegal value for -p option");
-	    break;
-	}
-    ac -= optind;
-    av += optind;
-
-    /* Parse parameters. */
-    switch (ac) {
-    default:
-	Usage();
-	/* NOTREACHED */
-    case 0:
-    case 1:
-	break;
-    case 2:
-	pattern = av[1];
-	break;
-    case 3:
-	pattern = av[1];
-	types = av[2];
-	break;
+    /* Parse options. */
+    while ((option = getopt(argc, argv, "Ah:p:")) != EOF) {
+        switch (option) {
+        case 'A':
+            authinfo = true;
+            break;
+        case 'h':
+            host = optarg;
+            break;
+        case 'p':
+            port = atoi(optarg);
+            if (port <= 0)
+                die("%s is not a valid port number", optarg);
+            break;
+        default:
+            exit(1);
+        }
     }
-    if (av[0] == NULL)
-	list = "active";
-    else {
-	list = av[0];
-        if (strcmp(list, "active") != 0 && types != NULL)
-            Usage();
-	if (strcmp(list, "active") != 0 && strcmp(list, "newsgroups") != 0
-         && pattern != NULL)
-	    Usage();
-    }
+    argc -= optind;
+    argv += optind;
 
-    /* Open a connection to the server. */
-    if (host == NULL && (host = innconf->server) == NULL)
+    /* Read optional arguments. */
+    if (argc > 3)
+        die("too many arguments");
+    if (argc >= 1)
+        list = argv[0];
+    if (argc >= 2)
+        pattern = argv[1];
+    if (argc == 3)
+        types = argv[2];
+    if (strcmp(list, "active") != 0 && types != NULL)
+        die("group types can only be specified with a list type of active");
+
+    /* Connect to the server. */
+    if (host == NULL)
         sysdie("cannot get server name");
-    buff[0] = '\0';
-    if (NNTPconnect(host, port, &FromServer, &ToServer, buff) < 0)
-        die("cannot connect to server: %s", buff[0] ? buff : strerror(errno));
-    if (authinfo && NNTPsendpassword(host, FromServer, ToServer) < 0)
-        die("cannot authenticate to server");
+    nntp = nntp_connect(host, port, 128 * 1024);
+    if (nntp == NULL)
+        sysdie("cannot connect to server %s:%hu", host, port);
+    status = nntp_read_response(nntp, 10 * 60, &response, &line);
+    if (status != NNTP_READ_OK)
+        die_nntp_status(status);
+    if (response < 200 || response > 201)
+        die_nntp_code(response, line);
 
-    /* Get the data from the server. */
-    active = CAlistopen(FromServer, ToServer,
-                        (strcmp(list, "active") == 0) ? NULL : list);
-    if (active == NULL)
-        sysdie("cannot retrieve data");
+    /* Authenticate if desired. */
+    if (authinfo) {
+        char *username, *password;
 
-    /* Set up to read it quickly. */
-    if ((qp = QIOfdopen((int)fileno(active))) == NULL)
-        sysdie("cannot read temporary file");
-
-    /* Scan server's output, displaying appropriate lines. */
-    i = 1;
-    while ((line = QIOread(qp)) != NULL) {
-	i++;
-
-	/* No pattern means print all. */
-	if (pattern == NULL) {
-	    printf("%s\n", line);
-	    continue;
-	}
-
-	/* Get the group name, see if it's one we want. */
-	if ((p = strchr(line, ' ')) == NULL) {
-            warn("line %d is malformed", i);
-	    continue;
-	}
-	*p = '\0';
-	if (!uwildmat(line, pattern))
-	    continue;
-	*p = ' ';
-
-	/* If no group types, we want them all. */
-	if (types == NULL) {
-	    printf("%s\n", line);
-	    continue;
-	}
-
-	/* Find the fourth field. */
-	if ((p = strchr(p + 1, ' ')) == NULL) {
-            warn("line %d (field 2) is malformed", i);
-	    continue;
-	}
-	if ((p = strchr(p + 1, ' ')) == NULL) {
-            warn("line %d (field 3) is malformed", i);
-	    continue;
-	}
-	field4 = p + 1;
-	if ((p = strchr(field4, ' ')) != NULL) {
-            warn("line %d has more than 4 fields", i);
-	    continue;
-	}
-
-	/* Is this the type of line we want? */
-	if (strchr(types, field4[0]) != NULL)
-	    printf("%s\n", line);
+        if (get_authinfo(host, &username, &password)) {
+            if (!send_authinfo(nntp, username, password))
+                warn("server %s did not accept authentication", host);
+            free(username);
+            free(password);
+        } else
+            warn("no authentication information found for %s", host);
     }
 
-    /* Determine why we stopped */
-    if (QIOerror(qp)) {
-        syswarn("cannot read temporary file at line %d", i);
-	i = 1;
-    }
-    else if (QIOtoolong(qp)) {
-        warn("line %d is too long", i);
-	i = i;
-    }
+    /* Get and display the data. */
+    send_list(nntp, list, pattern);
+    if (types != NULL)
+        print_active(nntp, types);
     else
-	i = 0;
+        print_list(nntp);
 
-    /* All done. */
-    CAclose();
-    fprintf(ToServer, "quit\r\n");
-    fclose(ToServer);
-    fgets(buff, sizeof buff, FromServer);
-    fclose(FromServer);
-    exit(i);
-    /* NOTREACHED */
+    /* Be polite and say goodbye; it gives the server a chance to shut the
+       connection down cleanly. */
+    if (nntp_send_line(nntp, "QUIT"))
+        status = nntp_read_response(nntp, 10 * 60, &response, &line);
+    exit(0);
 }
