284dbafa539d27b8cb5fee4d9770ade6fd962ebe
Russ Allbery <eagle@eyrie.org>
2003-07-12 19:14:34 +0000

    Make the third argument of mapcntl go away if we're on a platform that doesn't take three arguments to msync.

--- include/inn/mmap.h
+++ include/inn/mmap.h
@@ -18,6 +18,14 @@ BEGIN_DECLS
  */
 void mapcntl(void *, size_t, int);
 
+/* Some platforms only support two arguments to msync.  On those platforms,
+   make the third argument to mapcntl always be zero, getting rid of whatever
+   the caller tried to pass.  This avoids undefined symbols for MS_ASYNC and
+   friends on platforms with two-argument msync functions. */
+#ifndef INN_HAVE_MSYNC_3_ARG
+# define mapcntl(p, l, f) mapcntl((p), (l), 0)
+#endif
+
 END_DECLS
 
 #endif /* INN_MMAP_H */
