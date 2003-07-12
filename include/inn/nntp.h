b623456e9432bdc2510e27d2e798c00195fde8c3
Russ Allbery <eagle@eyrie.org>
2003-07-12 19:52:22 +0000

    Add the first pass at a new, general-purpose NNTP utility library.  Update Makefile dependencies for lib.

---
+++ include/inn/nntp.h
@@ -0,0 +1,212 @@
+/*  $Id$
+**
+**  NNTP codes and utility functions for speaking the NNTP protocol.
+**
+**  The nntp_code enum defines NNTP codes for every response supported by INN.
+**  The functions speak the NNTP protocol to a remote server or client.  So
+**  far, most of the library support is for servers rather than clients.
+**  Buffering is handled internally when reading from the network to allow for
+**  easier parsing.
+*/
+
+#ifndef INN_NNTP_H
+#define INN_NNTP_H 1
+
+#include <stdio.h>
+
+/*
+**  NNTP response codes as defined in RFC 977 and elsewhere.
+**
+**  All NNTP response codes are three digits.  The first digit is one of:
+**
+**      1xx     Informative message.
+**      2xx     Command completed OK.
+**      3xx     Command OK so far; send the rest of it.
+**      4xx     Command was correct, but couldn't be performed for some
+**              reason.
+**      5xx     Command unimplemented, or incorrect, or a serious program
+**              error occurred.
+**
+**  The second digit defines the type of command:
+**
+**      x0x     Connection, setup, and miscellaneous messages
+**      x1x     Newsgroup selection
+**      x2x     Article selection
+**      x3x     Distribution functions
+**      x4x     Posting
+**      x8x     Reserved for authentication and authorization extensions
+**      x9x     Reserved for private use (non-standard extensions)
+**
+**  Each response code is assigned a symbolic name starting with NNTP_.  The
+**  second component of the code is INFO, OK, CONT, FAIL, or ERR,
+**  corresponding to the first digit of the response code.  An enum is
+**  available for holding NNTP codes specifically.
+*/
+
+enum nntp_code {
+    NNTP_INFO_HELP              = 100,
+    NNTP_INFO_DATE              = 111,
+    NNTP_OK_BANNER_POST         = 200,
+    NNTP_OK_BANNER_NOPOST       = 201,
+    NNTP_OK_EXTENSIONS          = 202,
+    NNTP_OK_QUIT                = 205,
+    NNTP_OK_GROUP               = 211,
+    NNTP_OK_LIST                = 215,
+    NNTP_OK_ARTICLE             = 220,
+    NNTP_OK_HEAD                = 221,
+    NNTP_OK_BODY                = 222,
+    NNTP_OK_STAT                = 223,
+    NNTP_OK_OVER                = 224,
+    NNTP_OK_NEWNEWS             = 230,
+    NNTP_OK_NEWGROUPS           = 231,
+    NNTP_OK_IHAVE               = 235,
+    NNTP_OK_POST                = 240,
+    NNTP_CONT_IHAVE             = 335,
+    NNTP_CONT_POST              = 340,
+    NNTP_FAIL_TERMINATING       = 400,
+    NNTP_FAIL_BAD_GROUP         = 411, /* Group unknown */
+    NNTP_FAIL_NO_GROUP          = 412, /* Not in a newsgroup */
+    NNTP_FAIL_NO_ARTICLE        = 420, /* No current article */
+    NNTP_FAIL_NEXT              = 421,
+    NNTP_FAIL_PREV              = 422,
+    NNTP_FAIL_BAD_ARTICLE       = 423, /* Bad article number */
+    NNTP_FAIL_NOTFOUND          = 430, /* Article not found */
+    NNTP_FAIL_IHAVE_REFUSE      = 435, /* IHAVE article not wanted */
+    NNTP_FAIL_IHAVE_DEFER       = 436, /* IHAVE article deferred */
+    NNTP_FAIL_IHAVE_REJECT      = 437, /* IHAVE article rejected */
+    NNTP_FAIL_POST_AUTH         = 440, /* Posting not allowed */
+    NNTP_FAIL_POST_REJECT       = 441, /* POST article rejected */
+    NNTP_ERR_COMMAND            = 500,
+    NNTP_ERR_SYNTAX             = 501,
+    NNTP_ERR_ACCESS             = 502,
+    NNTP_ERR_UNAVAILABLE        = 503,
+
+    /* Streaming extension. */
+    NNTP_OK_STREAM              = 203,
+    NNTP_OK_CHECK               = 238,
+    NNTP_OK_TAKETHIS            = 239,
+    NNTP_FAIL_CHECK_DEFER       = 431,
+    NNTP_FAIL_CHECK_REFUSE      = 438,
+    NNTP_FAIL_TAKETHIS_REJECT   = 439,
+
+    /* Authentication extension. */
+    NNTP_OK_AUTHINFO            = 281,
+    NNTP_CONT_AUTHINFO          = 381,
+    NNTP_FAIL_AUTH_NEEDED       = 480,
+    NNTP_FAIL_AUTHINFO_BAD      = 481,
+    NNTP_FAIL_AUTHINFO_REJECT   = 482,
+
+    /* TLS extension.  (These codes will change.) */
+    NNTP_CONT_STARTTLS          = 382,
+    NNTP_FAIL_STARTTLS          = 483,
+    NNTP_ERR_STARTTLS           = 580,
+
+    /* XGTITLE extension (deprecated, use LIST NEWSGROUPS). */
+    NNTP_OK_XGTITLE             = 282,
+    NNTP_FAIL_XGTITLE           = 481, /* Duplicates NNTP_FAIL_AUTHINFO_BAD */
+
+    /* MODE CANCEL extension.  (These codes should change.) */
+    NNTP_OK_MODE_CANCEL         = 284,
+    NNTP_OK_CANCEL              = 289,
+    NNTP_FAIL_CANCEL            = 484,
+
+    /* XBATCH extension. */
+    NNTP_OK_XBATCH              = 239,
+    NNTP_CONT_XBATCH            = 339,
+    NNTP_FAIL_XBATCH            = 436  /* Duplicates NNTP_FAIL_IHAVE_DEFER */
+};
+
+
+/* Per draft-ietf-nntpext-base-17.txt:
+
+       Command lines MUST NOT exceed 512 octets, which includes the
+       terminating US-ASCII CRLF pair.  The arguments MUST NOT exceed 497
+       octets.
+
+   Also see below for an additional restriction on message IDs. */
+
+#define NNTP_MAXLEN_COMMAND     512
+
+/* Consensus on the USEFOR mailing list in June of 2000 indicates that the
+   next revision of the Usenet article standard will limit the length of the
+   message ID to 250 characters.  This is also the limit recommended by
+   son-of-1036.
+
+   You can increase this limit if you want, but don't increase it above 497.
+   RFC 977 limits each line of the NNTP protocol to 512 octets, including
+   the terminating CRLF.  For a message ID to be passed using the TAKETHIS
+   command, it can therefore be a maximum of 501 octets.  The November 1999
+   draft of the replacement RFC limits it to 497 octets.
+
+   Both Cyclone and DNews are known to reject message IDs longer than 500
+   octets as of June of 2000.  DNews has been reported to have problems with
+   message IDs of 494 octets. */
+
+#define NNTP_MAXLEN_MSGID       250
+
+/* Forward declaration. */
+struct cvector;
+
+/* Opaque struct that holds NNTP connection state. */
+struct nntp;
+
+/* Return codes for NNTP reader functions. */
+enum nntp_status {
+    NNTP_READ_OK,
+    NNTP_READ_EOF,
+    NNTP_READ_ERROR,
+    NNTP_READ_TIMEOUT,
+    NNTP_READ_LONG
+};
+
+/* Allocate a new nntp for a pair of file descriptors.  Also takes the maximum
+   size for the read buffer; messages longer than this will not be read. */
+struct nntp *nntp_new(int in, int out, size_t maxsize);
+
+/* Free an nntp struct and close the connection. */
+void nntp_free(struct nntp *);
+
+/* Connect to a remote host and return an nntp struct for that connection. */
+struct nntp *nntp_connect(const char *host, unsigned short port,
+                          size_t maxsize);
+
+/* Read a single line from an NNTP connection with the given timeout, placing
+   the nul-terminated line (without the \r\n line ending) in the provided
+   pointer.  The string will be invalidated by the next read from that
+   connection. */
+enum nntp_status nntp_read_line(struct nntp *, time_t timeout, char **);
+
+/* Read a response to an NNTP command with the given timeout, placing the
+   response code in the third argument and the rest of the line in the fourth
+   argument.  If no response code could be found, the code will be set to 0.
+   The string will be invalidated by the next read from that connection. */
+enum nntp_status nntp_read_response(struct nntp *nntp, time_t timeout,
+                                    enum nntp_code *code, char **rest);
+
+/* Read a command from an NNTP connection with the given timeout, placing the
+   command and its arguments into the provided cvector.  The cvector will be
+   invalidated by the next read from that connection. */
+enum nntp_status nntp_read_command(struct nntp *, time_t timeout,
+                                   struct cvector *);
+
+/* Read multiline data from an NNTP connection with the given timeout.  Set
+   the third argument to a pointer to the data (still in wire format) and the
+   fourth argument to its length. */
+enum nntp_status nntp_read_multiline(struct nntp *, time_t timeout, char **,
+                                     size_t *);
+
+/* Send a line to the remote connection.  The output is flushed after sending
+   the line. */
+bool nntp_send_line(struct nntp *, const char *format, ...);
+
+/* Send a response to an NNTP command or an opening banner.  The string may be
+   NULL to indicate nothing should follow the response code; otherwise, it is
+   a printf-style format.  The noflush variant doesn't flush the output after
+   sending it, used for introducing multiline responses. */
+bool nntp_respond(struct nntp *, enum nntp_code, const char *, ...);
+void nntp_respond_noflush(struct nntp *, enum nntp_code, const char *, ...);
+
+/* Flush NNTP output, returning true on success and false on any error. */
+bool nntp_flush(struct nntp *);
+
+#endif /* INN_NNTP_H */
---
+++ lib/nntp.c
@@ -0,0 +1,374 @@
+/*  $Id$
+**
+**  Utility functions for speaking the NNTP protocol.
+**
+**  These functions speak the NNTP protocol over stdio FILEs.  So far, only
+**  the server functions are implemented; there is no client support as yet.
+*/
+
+#include "config.h"
+#include "clibrary.h"
+#include "portable/socket.h"
+#include <ctype.h>
+#include <errno.h>
+
+#include "inn/buffer.h"
+#include "inn/innconf.h"
+#include "inn/network.h"
+#include "inn/nntp.h"
+#include "inn/vector.h"
+#include "libinn.h"
+
+/* State for an NNTP connection. */
+struct nntp {
+    int in_fd;
+    int out_fd;
+    struct buffer in;
+    struct buffer out;
+    size_t maxsize;
+};
+
+
+/*
+**  Allocate a new nntp struct and return it.  Takes the file descriptor to
+**  read from, the file descriptor to write to, and the maximum multiline size
+**  allowed.
+*/
+struct nntp *
+nntp_new(int in, int out, size_t maxsize)
+{
+    struct nntp *nntp;
+
+    nntp = xmalloc(sizeof(struct nntp));
+    nntp->in_fd = in;
+    nntp->out_fd = out;
+    nntp->maxsize = maxsize;
+    nntp->in.data = NULL;
+    nntp->in.size = 0;
+    nntp->in.used = 0;
+    nntp->in.left = 0;
+    nntp->out.data = NULL;
+    nntp->out.size = 0;
+    nntp->out.used = 0;
+    nntp->out.left = 0;
+    return nntp;
+}
+
+
+/*
+**  Free an nntp struct and close the associated file descriptors.
+*/
+void
+nntp_free(struct nntp *nntp)
+{
+    if (nntp == NULL)
+        return;
+    if (nntp->in.data != NULL)
+        free(nntp->in.data);
+    if (nntp->out.data != NULL)
+        free(nntp->out.data);
+    if (nntp->in_fd >= 0)
+        close(nntp->in_fd);
+    if (nntp->out_fd >= 0 && nntp->out_fd != nntp->in_fd)
+        close(nntp->out_fd);
+    free(nntp);
+}
+
+
+/*
+**  Connect to a remote NNTP server.  Allocates and returns a new nntp struct.
+**  Takes the server name, the port to connect to, and the maximum buffer size
+**  to use.  On any failure to connect to the remote host, returns NULL.
+*/
+struct nntp *
+nntp_connect(const char *host, unsigned short port, size_t maxsize)
+{
+    struct addrinfo hints, *ai;
+    char portbuf[16];
+    int fd, oerrno;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = NETWORK_AF_HINT;
+    hints.ai_socktype = SOCK_STREAM;
+    snprintf(portbuf, sizeof(portbuf), "%d", port);
+    if (getaddrinfo(host, portbuf, &hints, &ai) != 0)
+        return NULL;
+    fd = network_connect(ai);
+    oerrno = errno;
+    freeaddrinfo(ai);
+    errno = oerrno;
+    if (fd < 0)
+        return NULL;
+    return nntp_new(fd, fd, maxsize);
+}
+
+
+/*
+**  Read data from an NNTP connection, as much as is available, putting it
+**  into the reader buffer.  Return an nntp_status code.
+*/
+static enum nntp_status
+nntp_read_data(struct nntp *nntp, time_t timeout)
+{
+    ssize_t count;
+    int status;
+
+    /* Resize the buffer if we're out of space, but don't allow the buffer to
+       grow longer than maxsize. */
+    if (nntp->in.size == 0)
+        buffer_resize(&nntp->in, 1024);
+    if (nntp->in.used + nntp->in.left == nntp->in.size) {
+        size_t size;
+
+        if (nntp->in.size >= nntp->maxsize)
+            return NNTP_READ_LONG;
+        size = nntp->in.size * 2;
+        if (size > nntp->maxsize)
+            size = nntp->maxsize;
+        buffer_resize(&nntp->in, size);
+    }
+
+    /* Wait for activity. */
+    do {
+        fd_set mask;
+        struct timeval tv;
+
+        FD_ZERO(&mask);
+        FD_SET(nntp->in_fd, &mask);
+        tv.tv_sec = timeout;
+        tv.tv_usec = 0;
+        status = select(nntp->in_fd + 1, &mask, NULL, NULL, &tv);
+        if (status == -1 && errno != EINTR)
+            return NNTP_READ_ERROR;
+    } while (status == -1);
+
+    /* Check for timeout. */
+    if (status == 0)
+        return NNTP_READ_TIMEOUT;
+
+    /* Do the actual read. */
+    count = buffer_read(&nntp->in, nntp->in_fd);
+    if (count < 0)
+        return NNTP_READ_ERROR;
+    else if (count == 0)
+        return NNTP_READ_EOF;
+    else
+        return NNTP_READ_OK;
+}
+
+/*
+**  Read a single line of data from an NNTP connection with the given
+**  timeout.  Puts the nul-terminated line in the third argument; it points
+**  into the nntp input buffer and will therefore be invalidated on the next
+**  read or on nntp_free, but not until then.  Returns an nntp_status.
+*/
+enum nntp_status
+nntp_read_line(struct nntp *nntp, time_t timeout, char **line)
+{
+    struct buffer *in = &nntp->in;
+    enum nntp_status status = NNTP_READ_OK;
+    size_t offset;
+    size_t start = 0;
+
+    if (in->used + in->left == in->size)
+        buffer_compact(in);
+    while (status == NNTP_READ_OK) {
+        if (buffer_find_string(in, "\r\n", start, &offset)) {
+            in->data[in->used + offset] = '\0';
+            in->left -= offset + 2;
+            *line = in->data + in->used;
+            in->used += offset + 2;
+            return NNTP_READ_OK;
+        }
+
+        /* Back up one character in case \r\n was split across read
+           boundaries. */
+        start = (in->left > 0) ? in->left - 1 : 0;
+        status = nntp_read_data(nntp, timeout);
+    }
+    return status;
+}
+
+
+/*
+**  Read a response to an NNTP command.  Puts the status code in the third
+**  argument and the rest of the line in the fourth argument, using a status
+**  code of 000 if no status code could be found at the beginning of the
+**  line.  The line will be invalidated on the next read or on nntp_free.
+**  Returns an nntp_status.
+*/
+enum nntp_status
+nntp_read_response(struct nntp *nntp, time_t timeout, enum nntp_code *code,
+                   char **rest)
+{
+    char *line;
+    enum nntp_status status;
+
+    status = nntp_read_line(nntp, timeout, &line);
+    if (status != NNTP_READ_OK)
+        return status;
+    *code = strtol(line, rest, 10);
+    if (*rest != line + 3)
+        *code = 0;
+    else if (CTYPE(isspace, *rest[0]))
+        (*rest)++;
+    return status;
+}
+
+
+/*
+**  Read a command from an NNTP connection with the given timeout.  Takes a
+**  cvector as the third argument and stores in it the command and arguments,
+**  split on whitespace.  The vectors will point into the nntp input buffer
+**  and will therefore be invalidated on the next read or on nntp_free, but
+**  not until then.  Returns an nntp_status.
+*/
+enum nntp_status
+nntp_read_command(struct nntp *nntp, time_t timeout, struct cvector *command)
+{
+    enum nntp_status status;
+    char *line;
+
+    status = nntp_read_line(nntp, timeout, &line);
+    if (status == NNTP_READ_OK)
+        cvector_split_space(nntp->in.data + nntp->in.used, command);
+    return status;
+}
+
+
+/*
+**  Read multiline data from an NNTP connection.  Takes the nntp struct and a
+**  timeout (in seconds) and sets the third argument to a pointer to the data
+**  (still in wire format) and the fourth argument to its length.  Returns an
+**  nntp_status code.
+*/
+enum nntp_status
+nntp_read_multiline(struct nntp *nntp, time_t timeout, char **data,
+                    size_t *length)
+{
+    struct buffer *in = &nntp->in;
+    enum nntp_status status = NNTP_READ_OK;
+    size_t offset;
+    size_t start = 0;
+
+    buffer_compact(in);
+    while (status == NNTP_READ_OK) {
+        if (buffer_find_string(in, "\r\n.\r\n", start, &offset)) {
+            offset += 5;
+            in->left -= offset;
+            *length = offset;
+            *data = in->data + in->used;
+            in->used += offset;
+            return NNTP_READ_OK;
+        }
+
+        /* Back up by up to four characters in case our reads split on the
+           boundary of the delimiter. */
+        start = (in->left < 4) ? 0 : in->left - 4;
+        status = nntp_read_data(nntp, timeout);
+    }
+    return status;
+}
+
+
+/*
+**  Flush the NNTP output channel and makes sure that it has no errors.
+**  Returns true on success and false on failure.
+*/
+bool
+nntp_flush(struct nntp *nntp)
+{
+    ssize_t status;
+
+    if (nntp->out.left == 0)
+        return true;
+    status = xwrite(nntp->out_fd, nntp->out.data, nntp->out.left);
+    if (status < 0)
+        return false;
+    nntp->out.left = 0;
+    nntp->out.used = 0;
+    return true;
+}
+
+
+/*
+**  Send a line of data to an NNTP stream, flushing after sending it.  Takes
+**  the nntp struct and printf-style arguments for the rest of the line.
+**  Returns true on success and false on an error.
+*/
+bool
+nntp_send_line(struct nntp *nntp, const char *format, ...)
+{
+    va_list args;
+    bool done;
+
+    va_start(args, format);
+    done = buffer_vsprintf(&nntp->out, true, format, args);
+    va_end(args);
+    if (!done) {
+        va_start(args, format);
+        buffer_vsprintf(&nntp->out, true, format, args);
+        va_end(args);
+    }
+    buffer_append(&nntp->out, "\r\n", 2);
+    return nntp_flush(nntp);
+}
+
+
+/*
+**  Send a response to an NNTP command, or the opening banner of a server.
+**  Takes the nntp struct, a response code, and then printf-style arguments
+**  for the rest of the line.  Format may be NULL, indicating nothing should
+**  be printed after the response code.  Returns true on success and false on
+**  an error.
+*/
+bool
+nntp_respond(struct nntp *nntp, enum nntp_code code, const char *format, ...)
+{
+    va_list args;
+    bool done;
+
+    if (format == NULL)
+        buffer_sprintf(&nntp->out, true, "%d\r\n", code);
+    else {
+        buffer_sprintf(&nntp->out, true, "%d ", code);
+        va_start(args, format);
+        done = buffer_vsprintf(&nntp->out, true, format, args);
+        va_end(args);
+        if (!done) {
+            va_start(args, format);
+            buffer_vsprintf(&nntp->out, true, format, args);
+            va_end(args);
+        }
+        buffer_append(&nntp->out, "\r\n", 2);
+    }
+    return nntp_flush(nntp);
+}
+
+
+/*
+**  The same as nntp_respond(), but don't flush after sending the response.
+**  Used for beginning multiline responses primarily.
+*/
+void
+nntp_respond_noflush(struct nntp *nntp, enum nntp_code code,
+                     const char *format, ...)
+{
+    va_list args;
+    bool done;
+
+    if (format == NULL)
+        buffer_sprintf(&nntp->out, true, "%d\r\n", code);
+    else {
+        buffer_sprintf(&nntp->out, true, "%d ", code);
+        va_start(args, format);
+        done = buffer_vsprintf(&nntp->out, true, format, args);
+        va_end(args);
+        if (!done) {
+            va_start(args, format);
+            buffer_vsprintf(&nntp->out, true, format, args);
+            va_end(args);
+        }
+        buffer_append(&nntp->out, "\r\n", 2);
+    }
+}
