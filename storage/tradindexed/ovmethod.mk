2d14a6c9e8888e60acc37a632912b90dc16ef196
Russ Allbery <eagle@eyrie.org>
2004-05-18 01:18:57 +0000

    Drop the idiosyncratic installation method that would fail to install programs if the installed version was newer than the built version in favor of a make install that works the way that people generally expect and always installs the new binaries.

--- storage/tradindexed/ovmethod.mk
+++ storage/tradindexed/ovmethod.mk
@@ -4,6 +4,3 @@ tradindexed/tdx-util.o: tradindexed/tdx-util.c
 tradindexed/tdx-util: tradindexed/tdx-util.o libstorage.$(EXTLIB) $(LIBHIST)
 	$(LIBLD) $(LDFLAGS) -o $@ tradindexed/tdx-util.o \
 	    $(LIBSTORAGE) $(LIBHIST) $(LIBINN) $(EXTSTORAGELIBS) $(LIBS)
-
-$(D)$(PATHBIN)/tdx-util: tradindexed/tdx-util
-	$(LI_XPRI) $? $@
