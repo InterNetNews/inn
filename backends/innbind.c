1533b6b196a6fa17f5303bd928710f7ed57662e9
Russ Allbery <eagle@eyrie.org>
2004-05-19 06:09:52 +0000

    Unfortunately, this has now gotten more complicated (thanks, Solaris).
    Add two additional modes.  The first, innbind -t <port>, will test using
    innbind to bind a socket to that port to see if it works correctly.  The
    inability to bind sockets in a setuid subprocess unfortunately can't be
    detected at configure time (because we need a setuid helper program), so
    this will have to be used at run-time.
    
    The second mode, innbind -e, will both create the sockets and bind them as
    specified on the command line, and then exec the program given on the
    command line, with the options given there, adding an additional option
    -p followed by a comma-separated list of the bound file descriptors.  This
    is equivalent to the old inndstart program, but more flexible and doesn't
    require ever parsing any configuration files while root.
    
    The only library code used while root is just buffer_*, vector_*, and the
    message functions.

--- backends/innbind.c
+++ backends/innbind.c
@@ -12,7 +12,10 @@
 #include <pwd.h>
 #include <syslog.h>
 
+#include "inn/buffer.h"
+#include "inn/innconf.h"
 #include "inn/messages.h"
+#include "inn/network.h"
 #include "inn/vector.h"
 #include "libinn.h"
 
@@ -31,6 +34,14 @@
 #ifndef INND_PORT
 # define INND_PORT 119
 #endif
+
+/* Holds the information about a network socket to bind. */
+struct binding {
+    int fd;
+    int family;
+    char *address;
+    unsigned short port;
+};
 
 
 /*
@@ -53,6 +64,46 @@ convert_string(const char *string, long *result)
         return false;
     return true;
 }
+
+
+/*
+**  Parse a command-line argument into a struct binding.  The command line
+**  argument is four comma-separated values:  the file descriptor, the family,
+**  the listening address, and the port number.  The caller is responsible for
+**  freeing the address attribute of the supplied binding struct, although if
+**  a binding struct is passed in for use and has a non-NULL address, it will
+**  be freed first.
+*/
+static void
+parse_argument(const char *string, struct binding *binding)
+{
+    struct vector *spec;
+    long value;
+
+    /* Do the initial parse and allocate our data structures. */
+    spec = vector_split(string, ',', NULL);
+    if (spec->count != 4)
+        die("invalid command-line argument %s", string);
+
+    /* Get the file descriptor, address family, and port. */
+    if (!convert_string(spec->strings[0], &value))
+        die("invalid file descriptor %s in %s", spec->strings[0], string);
+    binding->fd = value;
+    if (!convert_string(spec->strings[1], &value))
+        die("invalid protocol family %s in %s", spec->strings[1], string);
+    binding->family = value;
+    if (binding->address != NULL)
+        free(binding->address);
+    binding->address = xstrdup(spec->strings[2]);
+    if (!convert_string(spec->strings[3], &value))
+        die("invalid port number %s in %s", spec->strings[3], string);
+    if (value == 0)
+        die("port may not be zero in %s", string);
+    binding->port = value;
+
+    /* Done.  Clean up. */
+    vector_free(spec);
+}    
 
 
 /*
@@ -102,41 +153,20 @@ bind_ipv6(int fd, const char *address, unsigned short port, const char *spec)
 
 
 /*
-**  Given a command line argument, which consists of a comma-separated quad of
-**  file descriptor number, protocol family, address, and port (sufficient
-**  generality to handle both IPv4 and IPv6), and bind it.  Dies on any
-**  failure.
+**  Given a given a struct binding, bind that file descriptor.  Also takes the
+**  command-line argument for error reporting.  Dies on any failure.
 */
 static void
-bind_address(const char *string)
+bind_address(struct binding *binding, const char *spec)
 {
-    struct vector *spec;
-    int family, fd, type;
-    unsigned short port;
-    long value;
+    int fd = binding->fd;
+    unsigned short port = binding->port;
+    int type;
     socklen_t length;
-
-    /* Do the initial parse and allocate our data structures. */
-    spec = vector_split(string, ',', NULL);
-    if (spec->count != 4)
-        die("invalid command-line argument %s", string);
-
-    /* Get the file descriptor, address family, and port. */
-    if (!convert_string(spec->strings[0], &value))
-        die("invalid file descriptor %s in %s", spec->strings[0], string);
-    fd = value;
-    if (!convert_string(spec->strings[1], &value))
-        die("invalid protocol family %s in %s", spec->strings[1], string);
-    family = value;
-    if (!convert_string(spec->strings[3], &value))
-        die("invalid port number %s in %s", spec->strings[3], string);
-    if (value == 0)
-        die("port may not be zero in %s", string);
-    port = value;
 
     /* Make sure that we're allowed to bind to that port. */
     if (port < 1024 && port != 119 && port != 433 && port != INND_PORT)
-        die("cannot bind to restricted port %hu in %s", port, string);
+        die("cannot bind to restricted port %hu in %s", port, spec);
 
     /* Sanity check on the socket. */
     length = sizeof(type);
@@ -146,17 +176,100 @@ bind_address(const char *string)
         die("invalid file descriptor %d: not SOCK_STREAM", fd);
 
     /* Based on the address family, parse either an IPv4 or IPv6 address. */
-    if (family == AF_INET)
-        bind_ipv4(fd, spec->strings[2], port, string);
+    if (binding->family == AF_INET)
+        bind_ipv4(fd, binding->address, port, spec);
 #ifdef HAVE_INET6
-    else if (family == AF_INET6)
-        bind_ipv6(fd, spec->strings[2], port, string);
+    else if (binding->family == AF_INET6)
+        bind_ipv6(fd, binding->address, port, spec);
 #endif
     else
-        die("unknown protocol family %s in %s", spec->strings[1], string);
+        die("unknown protocol family %d in %s", binding->family, spec);
+}
 
-    /* Done.  Clean up. */
-    vector_free(spec);
+
+/*
+**  Given a struct binding, create a socket for it and fill in the file
+**  descriptor.  Also takes the command-line argument for error reporting.
+**  Dies on any failure.
+*/
+static void
+create_socket(struct binding *binding, const char *spec)
+{
+    int fd;
+#ifdef SO_REUSEADDR
+    int flag;
+#endif
+
+    /* Create the socket. */
+    if (binding->family == AF_INET)
+        fd = socket(PF_INET, SOCK_STREAM, IPPROTO_IP);
+#ifdef HAVE_INET6
+    else if (binding->family == AF_INET6)
+        fd = socket(PF_INET6, SOCK_STREAM, IPPROTO_IP);
+#endif
+    else
+        die("unknown protocol family %d in %s", binding->family, spec);
+    if (fd < -1)
+        die("cannot create socket for %s", spec);
+
+    /* Mark it reusable if possible. */
+#ifdef SO_REUSEADDR
+    flag = 1;
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag)) < 0)
+        die("cannot mark socket reusable for %s", spec);
+#endif
+
+    /* Fill in the struct. */
+    binding->fd = fd;
+}
+
+
+/*
+**  Given the argument vector for a program to run, drop permissions and
+**  execute the program.  Add in a new -p option with the value given in the
+**  third argument (which says what file descriptors have been bound and are
+**  ready to be listened on).
+*/
+static void
+exec_program(int argc, char *argv[], const char *ports)
+{
+    const char **command;
+    int i;
+
+    command = xmalloc((argc + 2 + 1) * sizeof(char *));
+    command[0] = argv[0];
+    command[1] = "-p";
+    command[2] = ports;
+    for (i = 3; i - 2 < argc; i++)
+        command[i] = argv[i - 2];
+    command[i] = NULL;
+    if (execv(command[0], (char **) command) < 0)
+        sysdie("exec of %s failed", command[0]);
+}
+
+
+/*
+**  Test to see if innbind will work as designed.  Takes the port number to
+**  try to bind to (as a string) and attempts to create a simple IPv4 socket
+**  and use innbind to bind it.  Returns true on success, false on failure.
+*/
+static bool
+innbind_test(const char *string)
+{
+    long value;
+    int fd;
+
+    if (!convert_string(string, &value))
+        die("invalid port number %s", string);
+    if (!innconf_read(NULL))
+        exit(1);
+    fd = network_bind_ipv4("127.0.0.1", value);
+    if (fd < 0)
+        return false;
+    else {
+        close(fd);
+        return true;
+    }
 }
 
 
@@ -166,6 +279,8 @@ main(int argc, char *argv[])
     struct passwd *pwd;
     uid_t real_uid;
     int i;
+    struct binding binding = { 0, 0, NULL, 0 };
+    struct buffer ports = { 0, 0, 0, NULL };
 
     /* Set up the error handlers.  Errors go to stderr and to syslog with a
        priority of LOG_CRIT.  This priority level is too high, but it's chosen
@@ -188,8 +303,43 @@ main(int argc, char *argv[])
                 (unsigned long) pwd->pw_uid, (unsigned long) real_uid);
     }
 
-    /* Process command-line options. */
-    for (i = 1; i < argc; i++)
-        bind_address(argv[i]);
+    /* If the first argument is -t, test whether innbind can work as
+       designed.  We do this by dropping privileges, reading the next argument
+       and interpreting it as the port number, and then trying to bind a
+       simple IPv4 socket to that port. */
+    if (argc > 2 && strcmp(argv[1], "-t") == 0) {
+        if (setuid(real_uid) < 0 || geteuid() != real_uid)
+            sysdie("unable to setuid to %d", real_uid);
+        exit(innbind_test(argv[2]) ? 0 : 1);
+    }
+
+    /* If the first argument is -e, we have to actually create the sockets and
+       then exec another process.  In this case, there will still be a list of
+       addresses to bind to, but the file descriptor will always be 0.  At the
+       end of the list will be the literal argument "--" and then the program
+       to exec with its arguments.  Otherwise, all we have to do is walk the
+       arguments and bind each address. */
+    if (argc > 1 && strcmp(argv[1], "-e") == 0) {
+        for (i = 2; i < argc; i++) {
+            if (strcmp(argv[i], "--") == 0)
+                break;
+            parse_argument(argv[i], &binding);
+            create_socket(&binding, argv[i]);
+            bind_address(&binding, argv[i]);
+            if (i > 2)
+                buffer_append(&ports, ",", 1);
+            buffer_sprintf(&ports, true, "%hu", binding.fd);
+        }
+        if (argc <= i + 1)
+            die("no program name given to exec");
+        buffer_append(&ports, "", 1);
+        if (setuid(real_uid) < 0 || geteuid() != real_uid)
+            sysdie("unable to setuid to %d", real_uid);
+        exec_program(argc - i - 1, argv + i + 1, ports.data);
+    } else 
+        for (i = 1; i < argc; i++) {
+            parse_argument(argv[i], &binding);
+            bind_address(&binding, argv[i]);
+        }
     exit(0);
 }
