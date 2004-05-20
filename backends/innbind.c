7a5901e06cc6ffb7291fd4fc90ca9f2fcc6c4ed2
Russ Allbery <eagle@eyrie.org>
2004-05-20 00:37:45 +0000

    Remove the code supporting -t for testing or -e for acting as a wrapper. Heath came up with a much better solution.
    Add support for creating a socket and passing it back on standard output
    to the parent process on systems that support STREAMS.  This should be
    exactly the systems where the standard bind approach doesn't work, so this
    should cover everyone.  innbind now prints ok\n if it can bind an address
    and prints no\n followed immediately by an ioctl to pass the file descriptor
    if the creation fails due to permission denied.

--- backends/innbind.c
+++ backends/innbind.c
@@ -10,12 +10,12 @@
 #include "portable/socket.h"
 #include <errno.h>
 #include <pwd.h>
+#ifdef HAVE_STREAMS_SENDFD
+# include <stropts.h>
+#endif
 #include <syslog.h>
 
-#include "inn/buffer.h"
-#include "inn/innconf.h"
 #include "inn/messages.h"
-#include "inn/network.h"
 #include "inn/vector.h"
 #include "libinn.h"
 
@@ -109,9 +109,10 @@ parse_argument(const char *string, struct binding *binding)
 /*
 **  Bind an IPv4 address, given the file descriptor, string giving the
 **  address, and the port.  The fourth argument is the full binding
-**  specification for error reporting.  Dies on any failure.
+**  specification for error reporting.  Returns true on success, false if
+**  binding failed due to permission denied.  Die on any other failure.
 */
-static void
+static bool
 bind_ipv4(int fd, const char *address, unsigned short port, const char *spec)
 {
     struct sockaddr_in server;
@@ -123,18 +124,24 @@ bind_ipv4(int fd, const char *address, unsigned short port, const char *spec)
         die("invalid IPv4 address %s in %s", address, spec);
     server.sin_addr = addr;
     sin_set_length(&server);
-    if (bind(fd, (struct sockaddr *) &server, sizeof(server)) < 0)
-        sysdie("cannot bind socket for %s", spec);
+    if (bind(fd, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        if (errno == EACCES)
+            return false;
+        else
+            sysdie("cannot bind socket for %s", spec);
+    }
+    return true;
 }
 
 
 /*
 **  Bind an IPv6 address, given the file descriptor, string giving the
 **  address, and the port.  The fourth argument is the full binding
-**  specification for error reporting.  Dies on any failure.
+**  specification for error reporting.  Returns true on success, false if
+**  binding failed due to permission denied.  Die on any other failure.
 */
 #ifdef HAVE_INET6
-static void
+static bool
 bind_ipv6(int fd, const char *address, unsigned short port, const char *spec)
 {
     struct sockaddr_in6 server;
@@ -146,17 +153,23 @@ bind_ipv6(int fd, const char *address, unsigned short port, const char *spec)
         die("invalid IPv6 address %s in %s", address, spec);
     server.sin6_addr = addr;
     sin6_set_length(&server);
-    if (bind(fd, (struct sockaddr *) &server, sizeof(server)) < 0)
-        sysdie("cannot bind socket for %s", spec);
+    if (bind(fd, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        if (errno == EACCES)
+            return false;
+        else
+            sysdie("cannot bind socket for %s", spec);
+    }
+    return true;
 }
 #endif /* HAVE_INET6 */
 
 
 /*
 **  Given a given a struct binding, bind that file descriptor.  Also takes the
-**  command-line argument for error reporting.  Dies on any failure.
+**  command-line argument for error reporting.  Returns true on success, false
+**  if binding failed due to permission denied.  Die on any other failure.
 */
-static void
+static bool
 bind_address(struct binding *binding, const char *spec)
 {
     int fd = binding->fd;
@@ -177,10 +190,10 @@ bind_address(struct binding *binding, const char *spec)
 
     /* Based on the address family, parse either an IPv4 or IPv6 address. */
     if (binding->family == AF_INET)
-        bind_ipv4(fd, binding->address, port, spec);
+        return bind_ipv4(fd, binding->address, port, spec);
 #ifdef HAVE_INET6
     else if (binding->family == AF_INET6)
-        bind_ipv6(fd, binding->address, port, spec);
+        return bind_ipv6(fd, binding->address, port, spec);
 #endif
     else
         die("unknown protocol family %d in %s", binding->family, spec);
@@ -225,59 +238,26 @@ create_socket(struct binding *binding, const char *spec)
 
 
 /*
-**  Given the argument vector for a program to run, drop permissions and
-**  execute the program.  Add in a new -p option with the value given in the
-**  third argument (which says what file descriptors have been bound and are
-**  ready to be listened on).
-**
-**  Use the environment variable IN_INNBIND as a canary; if it is set, refuse
-**  to exec a program again (to avoid exec loops).
+**  Given a file descriptor, attempt to pass it via stdout.  Die on any error.
+**  Currently, file descriptor passing is only implemented for systems that
+**  use STREAMS.
 */
+#ifdef HAVE_STREAMS_SENDFD
 static void
-exec_program(int argc, char *argv[], const char *ports)
+send_fd(int fd)
 {
-    const char **command;
-    int i;
-
-    if (getenv("IN_INNBIND") != NULL)
-        die("IN_INNBIND already set, apparent exec loop");
-    command = xmalloc((argc + 2 + 1) * sizeof(char *));
-    command[0] = argv[0];
-    command[1] = "-p";
-    command[2] = ports;
-    for (i = 3; i - 2 < argc; i++)
-        command[i] = argv[i - 2];
-    command[i] = NULL;
-    if (putenv((char *) "IN_INNBIND=1") < 0)
-        sysdie("cannot putenv IN_INNBIND");
-    if (execv(command[0], (char **) command) < 0)
-        sysdie("exec of %s failed", command[0]);
+    if (isastream(STDOUT_FILENO) != 1)
+        die("cannot pass file descriptor: stdout is not a stream");
+    if (ioctl(STDOUT_FILENO, I_SENDFD, fd) < 0)
+        sysdie("cannot pass file descriptor");
 }
-
-
-/*
-**  Test to see if innbind will work as designed.  Takes the port number to
-**  try to bind to (as a string) and attempts to create a simple IPv4 socket
-**  and use innbind to bind it.  Returns true on success, false on failure.
-*/
-static bool
-innbind_test(const char *string)
+#else /* !HAVE_STREAMS_SENDFD */
+static void
+send_fd(int fd UNUSED)
 {
-    long value;
-    int fd;
-
-    if (!convert_string(string, &value))
-        die("invalid port number %s", string);
-    if (!innconf_read(NULL))
-        exit(1);
-    fd = network_bind_ipv4("127.0.0.1", value);
-    if (fd < 0)
-        return false;
-    else {
-        close(fd);
-        return true;
-    }
+    die("cannot pass file descriptor: STREAMS not supported");
 }
+#endif /* !HAVE_STREAMS_SENDFD */
 
 
 int
@@ -286,8 +266,9 @@ main(int argc, char *argv[])
     struct passwd *pwd;
     uid_t real_uid;
     int i;
+    bool done;
     struct binding binding = { 0, 0, NULL, 0 };
-    struct buffer ports = { 0, 0, 0, NULL };
+    bool force_sendfd = false;
 
     /* Set up the error handlers.  Errors go to stderr and to syslog with a
        priority of LOG_CRIT.  This priority level is too high, but it's chosen
@@ -310,43 +291,35 @@ main(int argc, char *argv[])
                 (unsigned long) pwd->pw_uid, (unsigned long) real_uid);
     }
 
-    /* If the first argument is -t, test whether innbind can work as
-       designed.  We do this by dropping privileges, reading the next argument
-       and interpreting it as the port number, and then trying to bind a
-       simple IPv4 socket to that port. */
-    if (argc > 2 && strcmp(argv[1], "-t") == 0) {
-        if (setuid(real_uid) < 0 || geteuid() != real_uid)
-            sysdie("unable to setuid to %d", real_uid);
-        exit(innbind_test(argv[2]) ? 0 : 1);
+    /* If the first argument is -p, force creation of the socket and file
+       descriptor passing rather than even attempting to bind the socket
+       first. */
+    if (argc > 1 && strcmp(argv[1], "-p") == 0) {
+        force_sendfd = true;
+        argc--;
+        argv++;
     }
+    if (argc < 2)
+        die("no addresses specified");
 
-    /* If the first argument is -e, we have to actually create the sockets and
-       then exec another process.  In this case, there will still be a list of
-       addresses to bind to, but the file descriptor will always be 0.  At the
-       end of the list will be the literal argument "--" and then the program
-       to exec with its arguments.  Otherwise, all we have to do is walk the
-       arguments and bind each address. */
-    if (argc > 1 && strcmp(argv[1], "-e") == 0) {
-        for (i = 2; i < argc; i++) {
-            if (strcmp(argv[i], "--") == 0)
-                break;
-            parse_argument(argv[i], &binding);
+    /* Walk the argument list and try to bind each argument.  For each
+       successful bind, print "ok\n" to stdout.  For each bind that fails with
+       permission denied, print "no\n" to stdout and then immediately attempt
+       to create a new file descriptor and pass it back over stdout. */
+    for (i = 1; i < argc; i++) {
+        parse_argument(argv[i], &binding);
+        done = false;
+        if (!force_sendfd)
+            done = bind_address(&binding, argv[i]);
+        if (done)
+            write(STDOUT_FILENO, "ok\n", 3);
+        else {
+            write(STDOUT_FILENO, "no\n", 3);
             create_socket(&binding, argv[i]);
-            bind_address(&binding, argv[i]);
-            if (i > 2)
-                buffer_append(&ports, ",", 1);
-            buffer_sprintf(&ports, true, "%hu", binding.fd);
+            if (!bind_address(&binding, argv[i]))
+                sysdie("socket binding failed for %s", argv[i]);
+            send_fd(binding.fd);
         }
-        if (argc <= i + 1)
-            die("no program name given to exec");
-        buffer_append(&ports, "", 1);
-        if (setuid(real_uid) < 0 || geteuid() != real_uid)
-            sysdie("unable to setuid to %d", real_uid);
-        exec_program(argc - i - 1, argv + i + 1, ports.data);
-    } else 
-        for (i = 1; i < argc; i++) {
-            parse_argument(argv[i], &binding);
-            bind_address(&binding, argv[i]);
-        }
+    }
     exit(0);
 }
--- tests/util/innbind-t.c
+++ tests/util/innbind-t.c
@@ -4,37 +4,47 @@
 #include "config.h"
 #include "clibrary.h"
 #include "portable/socket.h"
+#include "portable/wait.h"
 #include <errno.h>
+#ifdef HAVE_STREAMS_SENDFD
+# include <stropts.h>
+#endif
 
 #include "inn/messages.h"
 #include "libtest.h"
 
+/* The path to the uninstalled innbind helper program. */
+static const char innbind[] = "../../backends/innbind";
+
 /* The server portion of the test.  Listens to a socket and accepts a
    connection, making sure what is printed on that connection matches what the
    client is supposed to print. */
-static void
+static int
 listener(int fd, int n)
 {
     int client;
     FILE *out;
     char buffer[512];
 
+    alarm(1);
     client = accept(fd, NULL, NULL);
     close(fd);
     if (client < 0) {
         syswarn("cannot accept connection from socket");
-        ok(n, false);
-        return;
+        ok(n++, false);
+        ok(n++, false);
+        return n;
     }
-    ok(n, true);
+    ok(n++, true);
     out = fdopen(client, "r");
     if (fgets(buffer, sizeof(buffer), out) == NULL) {
         syswarn("cannot read from socket");
-        ok(n + 1, false);
-        return;
+        ok(n++, false);
+        return n;
     }
-    ok_string(n + 1, "socket test\r\n", buffer);
+    ok_string(n++, "socket test\r\n", buffer);
     fclose(out);
+    return n;
 }
 
 /* Send a constant string to a socket, used to finish the client side of the
@@ -50,24 +60,6 @@ client_send(int fd)
     fputs("socket test\r\n", out);
     fclose(out);
     _exit(0);
-}
-
-/* The server portion of the exec test.  Accept a connection on the provided
-   file descriptor and then send one line and close the connection. */
-static void
-responder(int fd)
-{
-    int client;
-
-    if (getenv("IN_INNBIND") == NULL)
-        _exit(1);
-    if (listen(fd, 1) < 0)
-        _exit(1);
-    client = accept(fd, NULL, NULL);
-    close(fd);
-    if (client < 0)
-        _exit(1);
-    client_send(client);
 }
 
 /* Create a client IPv4 connection to the local server and then send a static
@@ -111,171 +103,218 @@ client_ipv6(void)
         _exit(1);
     server.sin6_addr = addr;
     if (connect(fd, (struct sockaddr *) &server, sizeof(server)) < 0)
-        _exit(1);
+        sysdie("connect failed");
     client_send(fd);
 }
 #endif
 
+/* Run innbind.  Takes the current test number, the file descriptor, the
+   address family, and the address to bind to.  Returns the new test
+   number. */
+static int
+run_innbind(int n, int fd, int family, const char *addr)
+{
+    int pipefds[2];
+    char buffer[128];
+    pid_t child, result;
+    int status;
+
+    if (pipe(pipefds) < 0)
+        sysdie("cannot create pipe");
+    child = fork();
+    if (child < 0)
+        sysdie("cannot fork");
+    else if (child == 0) {
+        close(1);
+        if (dup2(pipefds[1], 1) < 0)
+            _exit(1);
+        close(pipefds[0]);
+        snprintf(buffer, sizeof(buffer), "%d,%d,%s,11119", fd, family, addr);
+        if (execl(innbind, innbind, buffer, (char *) 0) < 0)
+            _exit(1);
+    } else {
+        close(pipefds[1]);
+        status = read(pipefds[0], buffer, 3);
+        if (status < 3) {
+            syswarn("read failed (return %d)", status);
+            ok(n++, false);
+        } else {
+            buffer[3] = '\0';
+            ok_string(n++, "ok\n", buffer);
+        }
+        result = waitpid(child, &status, 0);
+        if (result != child)
+            die("cannot wait for innbind");
+        ok(n++, WIFEXITED(status) && WEXITSTATUS(status) == 0);
+    }
+    return n;
+}
+
 /* Run a test of binding for IPv4.  Create a socket, use innbind to bind it to
    port 11119 on the loopback address, and then fork a child to connect to it.
    Make sure that connecting to it works correctly. */
-static void
+static int
 test_ipv4(int n)
 {
     int fd;
-    char command[128];
     pid_t child;
 
     fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
     if (fd < 0)
         sysdie("cannot create socket");
-    snprintf(command, sizeof(command),
-             "../../backends/innbind %d,%d,127.0.0.1,11119", fd,
-             AF_INET);
-    if (system(command) == 0)
-        ok(n, true);
-    else {
-        warn("cannot bind socket");
-        ok(n, false);
-        return;
+    n = run_innbind(n, fd, AF_INET, "127.0.0.1");
+    if (listen(fd, 1) < 0) {
+        ok(n++, false);
+        ok(n++, false);
+        ok(n++, false);
+    } else {
+        ok(n++, true);
+        child = fork();
+        if (child < 0)
+            sysdie("cannot fork");
+        else if (child == 0)
+            client_ipv4();
+        else
+            n = listener(fd, n);
     }
-    if (listen(fd, 1) < 0)
-        sysdie("cannot listen to socket");
-    child = fork();
-    if (child < 0)
-        sysdie("cannot fork");
-    else if (child == 0)
-        client_ipv4();
-    else
-        listener(fd, n + 1);
+    return n;
 }
 
 /* Run a test of binding for IPv6.  Create a socket, use innbind to bind it to
    port 11119 on the loopback address, and then fork a child to connect to it.
    Make sure that connecting to it works correctly. */
 #ifdef HAVE_INET6
-static void
+static int
 test_ipv6(int n)
 {
     int fd, i;
-    char command[128];
     pid_t child;
 
     fd = socket(AF_INET6, SOCK_STREAM, IPPROTO_IP);
     if (fd < 0) {
         if (errno == EAFNOSUPPORT || errno == EPROTONOSUPPORT) {
-            for (i = n; i < n + 3; i++)
+            for (i = n; i < n + 5; i++)
                 printf("ok %d # skip\n", i);
-            return;
+            return n + 5;
         } else
             sysdie("cannot create socket");
     }
-    snprintf(command, sizeof(command),
-             "../../backends/innbind %d,%d,::1,11119", fd, AF_INET);
-    if (system(command) == 0)
-        ok(n, true);
-    else {
-        warn("cannot bind socket");
-        ok(n, false);
-        return;
+    n = run_innbind(n, fd, AF_INET6, "::");
+    if (listen(fd, 1) < 0) {
+        ok(n++, false);
+        ok(n++, false);
+        ok(n++, false);
+    } else {
+        ok(n++, true);
+        child = fork();
+        if (child < 0)
+            sysdie("cannot fork");
+        else if (child == 0)
+            client_ipv6();
+        else
+            n = listener(fd, n);
     }
-    if (listen(fd, 1) < 0)
-        sysdie("cannot listen to socket");
-    child = fork();
-    if (child < 0)
-        sysdie("cannot fork");
-    else if (child == 0)
-        client_ipv6();
-    else
-        listener(fd, n + 1);
+    return n;
 }
-#else
+#else /* !HAVE_INET6 */
 static void
 test_ipv6(int n)
 {
     int i;
 
-    for (i = n; i < n + 3; i++)
+    for (i = n; i < n + 5; i++)
         printf("ok %d # skip\n", i);
+    return n + 5;
 }
-#endif
+#endif /* !HAVE_INET6 */
 
-/* Test the exec mode of innbind.  We're going to exec ourselves again, with
-   an IPv4 socket open on 11119.  That socket will just be a responder, so
-   we'll then connect to it and make sure that it returns what we want. */
-static void
-test_exec(int n)
+/* Test file descriptor passing on systems that support it.  Use innbind to
+   create a socket bound to port 11119, receive the socket, and then fork a
+   child to connect to it.  Make sure that connecting works correctly. */
+#ifdef HAVE_STREAMS_SENDFD
+static int
+test_sendfd(int n)
 {
-    char command[128], buffer[512];
-    pid_t child;
-    struct sockaddr_in server;
-    int fd;
-    FILE *out;
+    int pipefds[2], fd;
+    char buffer[128];
+    struct strrecvfd fdrec;
+    pid_t child, result;
+    int status;
 
-    /* Run the innbind as a child process. */
-    snprintf(command, sizeof(command),
-             "../../backends/innbind -e 0,%d,127.0.0.1,11119 -- ./innbind.t",
-             AF_INET);
+    if (pipe(pipefds) < 0)
+        sysdie("cannot create pipe");
     child = fork();
+    fd = -1;
     if (child < 0)
         sysdie("cannot fork");
     else if (child == 0) {
-        system(command);
-        _exit(0);
+        close(1);
+        if (dup2(pipefds[1], 1) < 0)
+            _exit(1);
+        close(pipefds[0]);
+        snprintf(buffer, sizeof(buffer), "0,%d,127.0.0.1,11119", AF_INET);
+        if (execl(innbind, innbind, "-p", buffer, (char *) 0) < 0)
+            _exit(1);
+    } else {
+        close(pipefds[1]);
+        status = read(pipefds[0], buffer, 3);
+        if (status < 3) {
+            syswarn("read failed (return %d)", status);
+            ok(n++, false);
+        } else {
+            buffer[3] = '\0';
+            ok_string(n++, "no\n", buffer);
+        }
+        if (ioctl(pipefds[0], I_RECVFD, &fdrec) < 0)
+            ok(n++, false);
+        else
+            ok(n++, true);
+        fd = fdrec.fd;
+        result = waitpid(child, &status, 0);
+        if (result != child)
+            die("cannot wait for innbind");
+        ok(n++, WIFEXITED(status) && WEXITSTATUS(status) == 0);
     }
-
-    /* We're in the parent.  Try to connect to the server that we should have
-       just created and make sure that it returns the appropriate string. */
-    fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-    if (fd < 0)
-        sysdie("cannot create socket");
-    memset(&server, 0, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(11119);
-    if (inet_aton("127.0.0.1", &server.sin_addr) == 0)
-        die("cannot parse 127.0.0.1");
-    sleep(1);
-    if (connect(fd, (struct sockaddr *) &server, sizeof(server)) < 0) {
-        syswarn("connect to exec client failed");
+    if (fd < 0 || listen(fd, 1) < 0) {
         ok(n++, false);
         ok(n++, false);
     } else {
-        ok(n++, true);
-        out = fdopen(fd, "r");
-        if (out == NULL)
-            sysdie("cannot fdopen %d", fd);
-        if (fgets(buffer, sizeof(buffer), out) == NULL) {
-            syswarn("cannot read from socket");
-            ok(n, false);
-        } else
-            ok_string(n, "socket test\r\n", buffer);
-        fclose(out);
+        child = fork();
+        if (child < 0)
+            sysdie("cannot fork");
+        else if (child == 0)
+            client_ipv4();
+        else
+            n = listener(fd, n);
     }
-    close(fd);
+    return n;
 }
+#else /* !HAVE_STREAMS_SENDFD */
+static int
+test_sendfd(int n)
+{
+    int i;
+
+    for (i = n; i < n + 5; i++)
+        printf("ok %d # skip\n", i);
+    return n;
+}
+#endif /* !HAVE_INET6 */
 
 int
-main(int argc, char *argv[])
+main(void)
 {
-    int fd;
-
-    /* If the first argument is -r, we're the server part of the exec test.
-       Figure out our open file descriptor and then run responder on it. */
-    if (argc == 3 && strcmp(argv[1], "-p") == 0) {
-        fd = strtol(argv[2], NULL, 10);
-        responder(fd);
-        _exit(0);
-    }
+    int n;
 
     if (access("innbind.t", F_OK) < 0)
         if (access("util/innbind.t", F_OK) == 0)
             chdir("util");
 
-    test_init(8);
+    test_init(15);
 
-    test_ipv4(1);
-    test_ipv6(4);
-    test_exec(7);
+    n = test_ipv4(1);           /* Tests  1-5.  */
+    n = test_ipv6(n);           /* Tests  6-10. */
+    n = test_sendfd(n);         /* Tests 11-15. */
 
     return 0;
 }
