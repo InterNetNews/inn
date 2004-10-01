b4d90b787afa753b647416bf65d3b51e824f3f77
Russ Allbery <eagle@eyrie.org>
2004-10-01 05:52:14 +0000

    The argument to SMsetup for SM_RDWR and SM_PREOPEN must be a bool *, not an int *.  This makes a difference with some versions of GCC on some platforms.

--- expire/makehistory.c
+++ expire/makehistory.c
@@ -761,7 +761,8 @@ main(int argc, char **argv)
 {
     ARTHANDLE *art = NULL;
     bool AppendMode;
-    int i, val;
+    int i;
+    bool val;
     char *HistoryDir;
     char *RebuiltflagPath;
     char *p;
