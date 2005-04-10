698fdf51e758516d8e3acd8c31bc2c17a2d69729
Russ Allbery <eagle@eyrie.org>
2005-04-10 18:27:15 +0000

    Reformat to match the INN formatting standard, add an include guard, and add C++ compilation protection.

--- authprogs/libauth.h
+++ authprogs/libauth.h
@@ -1,11 +1,15 @@
-/*
+/*  $Id$
 **
-**  Common headers for authenticators and resolvers.
-**
+**  Some utility functions for writing authenticators and resolvers.
 */
+
+#ifndef LIBAUTH_H
+#define LIBAUTH_H 1
 
 #include "config.h"
 #include "portable/socket.h"
+#include <stdio.h>
+#include <inn/defines.h>
 
 /* Holds the resolver information from nnrpd. */
 struct res_info {
@@ -20,20 +24,22 @@ struct auth_info {
     char *password;
 };
 
-/*
- * Reads connection information from a file descriptor (normally stdin, when
- * talking to nnrpd) and returns a new res_info or auth_info struct, or
- * returns NULL on failure.  Note that the fields will never be NULL; if the
- * corresponding information is missing, it is an error (which will be
- * logged and NULL will be returned).  The client is responsible for freeing
- * the struct and its fields; this can be done by calling the appropriate
- * destruction function below.
- */
+BEGIN_DECLS
 
+/* Reads connection information from a file descriptor (normally stdin, when
+   talking to nnrpd) and returns a new res_info or auth_info struct, or
+   returns NULL on failure.  Note that the fields will never be NULL; if the
+   corresponding information is missing, it is an error (which will be logged
+   and NULL will be returned).  The client is responsible for freeing the
+   struct and its fields; this can be done by calling the appropriate
+   destruction function below. */
 extern struct auth_info *get_auth_info(FILE *);
 extern struct res_info  *get_res_info (FILE *);
 
-extern void free_auth_info(struct auth_info*);
-extern void free_res_info (struct res_info*);
+/* Free a res_info or auth_info struct. */
+extern void free_auth_info(struct auth_info *);
+extern void free_res_info (struct res_info  *);
 
+END_DECLS
 
+#endif /* !LIBAUTH_H */
