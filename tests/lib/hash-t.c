23aebe690b77b757725359c9320fc4084be1a6f8
Russ Allbery <eagle@eyrie.org>
2005-04-10 06:24:12 +0000

    Add some HashToText and TextToHash tests.

--- tests/lib/hash-t.c
+++ tests/lib/hash-t.c
@@ -11,8 +11,9 @@ int
 main(void)
 {
     HASH h1, h2;
+    const char *text;
 
-    test_init(12);
+    test_init(14);
 
     h1 = HashMessageID("<lhs@test.invalid>");
     h2 = HashMessageID("<lhs@TEST.invalid>");
@@ -42,6 +43,12 @@ main(void)
     h1 = HashMessageID("<postmaster.test.invalid>");
     h2 = HashMessageID("<POSTMASTER.test.invalid>");
     ok(12, HashCompare(&h1, &h2) != 0);
+
+    h1 = HashMessageID("<87is2w1u6i.fsf@windlord.stanford.edu>");
+    text = HashToText(h1);
+    ok_string(13, "A0D432DC9718979BEFB4ACADA4BAD863", text);
+    h2 = TextToHash("A0D432DC9718979BEFB4ACADA4BAD863");
+    ok(14, HashCompare(&h1, &h2) == 0);
 
     return 0;
 }
