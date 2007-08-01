98cc14cfef8d449ed8045b64e9552259f01f2886
Julien ÉLIE <julien@trigofacile.com>
2007-08-01 20:23:12 +0000

    Remove two unsuitable tests for hstrerror.c.

--- tests/lib/hstrerror-t.c
+++ tests/lib/hstrerror-t.c
@@ -18,15 +18,13 @@ test_error(int n, const char *expected, int error)
 int
 main(void)
 {
-    test_init(7);
+    test_init(5);
 
     test_error(1, "Internal resolver error", -1);
     test_error(2, "No resolver error", 0);
     test_error(3, "No address associated with name", NO_ADDRESS);
     test_error(4, "Resolver error 777777", 777777);
     test_error(5, "Resolver error -99999", -99999);
-    test_error(6, "Resolver error 1000000", 1000000);
-    test_error(7, "Resolver error -100000", -100000);
 
     return 0;
 }
