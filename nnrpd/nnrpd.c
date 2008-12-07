c44a59acbf7f7e931d1151b4917864cc1249c5aa
Julien ÉLIE <julien@trigofacile.com>
2008-12-07 11:01:43 +0000

    Prettier answer when the command does not need any argument.

--- nnrpd/nnrpd.c
+++ nnrpd/nnrpd.c
@@ -1208,7 +1208,7 @@ main(int argc, char *argv[])
 	if ((cp->Minac != CMDany && ac < cp->Minac)
 	 || (cp->Maxac != CMDany && ac > cp->Maxac)) {
 	    Reply("%d Syntax is:  %s %s\r\n",
-		NNTP_ERR_SYNTAX, cp->Name, cp->Help ? cp->Help : "No argument allowed");
+		NNTP_ERR_SYNTAX, cp->Name, cp->Help ? cp->Help : "(no argument allowed)");
 	    continue;
 	}
 
