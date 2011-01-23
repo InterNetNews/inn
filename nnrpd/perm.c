9053042ba249cd19dabad525902c29d0099f8d74
Julien ÉLIE <julien@trigofacile.com>
2011-01-23 09:22:38 +0000

    Improve the logging of Perl and Python access hooks. The origin wasn't previously mentioned:
    "syntax error in (null)(2), Expected value."
    
    Now, nnrpd will log:
    
    "syntax error in perl_access-block(2), Expected value."

--- nnrpd/perm.c
+++ nnrpd/perm.c
@@ -1066,6 +1066,7 @@ PERMvectortoaccess(ACCESSGROUP *acc, const char *name,
     SetDefaultAccess(acc);
     str = xstrdup(name);
     acc->name = str;
+    file->filename = str;
 
     for (i = 0; i <= access_vec->count; i++) {
       tok = CONFgettoken(PERMtoks, file);
@@ -1074,6 +1075,9 @@ PERMvectortoaccess(ACCESSGROUP *acc, const char *name,
         accessdecl_parse(acc, file, tok);
       }
     }
+
+    /* No need to free str here; it is used by acc, and will be
+     * free'd when the access block is free'd. */
     free(file);
     return;
 }
@@ -1605,7 +1609,7 @@ PERMgetpermissions(void)
 
         access_realms[0] = xcalloc(1, sizeof(ACCESSGROUP));
 
-        PERMvectortoaccess(access_realms[0], "perl-dynamic", access_vec);
+        PERMvectortoaccess(access_realms[0], "perl_access-block", access_vec);
 
         vector_free(access_vec);
       } else {
@@ -1640,7 +1644,7 @@ PERMgetpermissions(void)
             access_realms[0] = xcalloc(1, sizeof(ACCESSGROUP));
             memset(access_realms[0], 0, sizeof(ACCESSGROUP));
 
-            PERMvectortoaccess(access_realms[0], "python-dynamic", access_vec);
+            PERMvectortoaccess(access_realms[0], "python_access-block", access_vec);
 
             vector_free(access_vec);
         } else {
