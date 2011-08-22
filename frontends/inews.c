25089e8161904e3395343de1213770bb5144d21a
Julien ÉLIE <julien@trigofacile.com>
2011-08-22 08:50:27 +0000

    Cast the argument to toupper()
    Following the previous commit dealing with tolower(), cast the argument
    to an unsigned char.

--- frontends/inews.c
+++ frontends/inews.c
@@ -238,7 +238,7 @@ StripOffHeaders(char *article)
 	}
 
 	/* See if it's a known header. */
-	c = islower((unsigned char) *p) ? toupper(*p) : *p;
+	c = islower((unsigned char) *p) ? toupper((unsigned char) *p) : *p;
 	for (hp = Table; hp < ARRAY_END(Table); hp++)
 	    if (c == hp->Name[0]
 	     && p[hp->Size] == ':'
