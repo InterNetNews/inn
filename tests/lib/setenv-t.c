2661fe2eb2ae1dc300a0dc3f67ebd9a6999f75d3
Julien ÉLIE <julien@trigofacile.com>
2013-01-16 21:57:16 +0000

    test suite:  remove a useless wrapped test for setenv
    Remove a test that makes the test suite fail on a few systems.
    The code it is testing is obvious whereas getting the amount of
    memory and the data size limits right is extremely difficult
    in all cases.  Besides, this test does not serve much purpose
    and is not present in rra-c-util 4.7.

--- tests/lib/setenv-t.c
+++ tests/lib/setenv-t.c
@@ -18,13 +18,10 @@ static const char test_value2[] = "Do not use Happy Fun Ball on concrete.";
 int
 main(void)
 {
-    char *value;
-    int status;
-
     if (getenv(test_var))
         die("%s already in the environment!", test_var);
 
-    test_init(12);
+    test_init(8);
 
     ok(1, test_setenv(test_var, test_value1, 0) == 0);
     ok_string(2, test_value1, getenv(test_var));
@@ -34,23 +31,6 @@ main(void)
     ok_string(6, test_value2, getenv(test_var));
     ok(7, test_setenv(test_var, "", 1) == 0);
     ok_string(8, "", getenv(test_var));
-
-    /* We're run by a shell script wrapper that sets resource limits such
-       that we can allocate one string of this size but not two.  Note that
-       Linux doesn't support data limits, so skip if we get an unexpected
-       success here. */
-    value = xmalloc(100 * 1024);
-    memset(value, 'A', 100 * 1024 - 1);
-    value[100 * 1024 - 1] = 0;
-    ok(9, test_setenv(test_var, value, 0) == 0);
-    ok_string(10, "", getenv(test_var));
-    status = test_setenv(test_var, value, 1);
-    if (status == 0) {
-        skip_block(11, 2, "no data limit support");
-    } else {
-        ok(11, (status == -1) && (errno == ENOMEM));
-        ok_string(12, "", getenv(test_var));
-    }
 
     return 0;
 }
