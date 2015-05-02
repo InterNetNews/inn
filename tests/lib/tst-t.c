f93afc3ddc097496d874e60fcb62b8fd87756dd7
Julien ÉLIE <julien@trigofacile.com>
2015-05-02 16:29:02 +0000

    tests/lib/tst-t.c:  Fix encoding (a non-ASCII char was used)

--- tests/lib/tst-t.c
+++ tests/lib/tst-t.c
@@ -31,7 +31,7 @@ main(void)
     char foo[] = "foo";
     char testing[] = "testing";
     char Strange[] = "Strange";
-    char change[] = "çhange";
+    char change[] = "\231hange";
 
     test_init(38);
 
@@ -47,13 +47,13 @@ main(void)
     ok(9, tst_insert(tst, SUC"testing", testing, 0, NULL) == TST_OK);
     ok(10, tst_insert(tst, SUC"t", t, 0, NULL) == TST_OK);
     ok(11, tst_insert(tst, SUC"Strange", Strange, 0, NULL) == TST_OK);
-    ok(12, tst_insert(tst, SUC"çhange", change, 0, NULL) == TST_OK);
+    ok(12, tst_insert(tst, SUC"\231hange", change, 0, NULL) == TST_OK);
     ok(13, tst_insert(tst, SUC"", foo, 0, NULL) == TST_NULL_KEY);
     ok(14, tst_insert(tst, NULL, foo, 0, NULL) == TST_NULL_KEY);
     ok_string(15, "testing", tst_search(tst, SUC"testing"));
     ok_string(16, "t", tst_search(tst, SUC"t"));
     ok_string(17, "Strange", tst_search(tst, SUC"Strange"));
-    ok_string(18, "çhange", tst_search(tst, SUC"çhange"));
+    ok_string(18, "\231hange", tst_search(tst, SUC"\231hange"));
     ok_string(19, "foo", tst_search(tst, SUC"test"));
     ok(20, tst_search(tst, SUC"") == NULL);
     ok(21, tst_search(tst, SUC"Peter") == NULL);
@@ -67,7 +67,7 @@ main(void)
     ok_string(29, "foo", tst_search(tst, SUC"test"));
     ok_string(30, "testing", tst_delete(tst, SUC"testing"));
     ok_string(31, "foo", tst_search(tst, SUC"test"));
-    ok_string(32, "çhange", tst_delete(tst, SUC"çhange"));
+    ok_string(32, "\231hange", tst_delete(tst, SUC"\231hange"));
     ok_string(33, "foo", tst_delete(tst, SUC"test"));
     ok(34, tst_search(tst, NULL) == NULL);
     ok(35, tst_delete(tst, NULL) == NULL);
