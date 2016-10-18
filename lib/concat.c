0ba9649c08c278586c58fa4052ba340c133ab08f
Julien ÉLIE <julien@trigofacile.com>
2016-10-18 20:19:56 +0000

    Update concat library to its latest version in rra-c-util

--- lib/concat.c
+++ lib/concat.c
@@ -1,40 +1,54 @@
-/*  $Id$
-**
-**  Concatenate strings with dynamic memory allocation.
-**
-**  Written by Russ Allbery <eagle@eyrie.org>
-**  This work is hereby placed in the public domain by its author.
-**
-**  Usage:
-** 
-**       string = concat(string1, string2, ..., (char *) 0);
-**       path = concatpath(base, name);
-**
-**  Dynamically allocates (using xmalloc) sufficient memory to hold all of
-**  the strings given and then concatenates them together into that
-**  allocated memory, returning a pointer to it.  Caller is responsible for
-**  freeing.  Assumes xmalloc is available.  The last argument must be a
-**  null pointer (to a char *, if you actually find a platform where it
-**  matters).
-**
-**  concatpath is similar, except that it only takes two arguments.  If the
-**  second argument begins with / or ./, a copy of it is returned;
-**  otherwise, the first argument, a slash, and the second argument are
-**  concatenated together and returned.  This is useful for building file
-**  names where names that aren't fully qualified are qualified with some
-**  particular directory.
-*/
+/* $Id$
+ *
+ * Concatenate strings with dynamic memory allocation.
+ *
+ * Usage:
+ *
+ *      string = concat(string1, string2, ..., (char *) 0);
+ *      path = concatpath(base, name);
+ *
+ * Dynamically allocates (using xmalloc) sufficient memory to hold all of the
+ * strings given and then concatenates them together into that allocated
+ * memory, returning a pointer to it.  Caller is responsible for freeing.
+ * Assumes xmalloc is available.  The last argument must be a null pointer (to
+ * a char *, if you actually find a platform where it matters).
+ *
+ * concatpath is similar, except that it only takes two arguments.  If the
+ * second argument begins with / or ./, a copy of it is returned; otherwise,
+ * the first argument, a slash, and the second argument are concatenated
+ * together and returned.  This is useful for building file names where names
+ * that aren't fully qualified are qualified with some particular directory.
+ *
+ * The canonical version of this file *used to be* maintained in the
+ * rra-c-util package, which can be found at
+ * <http://www.eyrie.org/~eagle/software/rra-c-util/>.
+ *
+ * Written by Russ Allbery <eagle@eyrie.org>
+ *
+ * The authors hereby relinquish any claim to any copyright that they may have
+ * in this work, whether granted under contract or by operation of law or
+ * international treaty, and hereby commit to the public, at large, that they
+ * shall not, at any time in the future, seek to enforce any copyright in this
+ * work against any person or entity, or prevent any person or entity from
+ * copying, publishing, distributing or creating derivative works of this
+ * work.
+ */
 
 #include "config.h"
-#include "inn/libinn.h"
+#include "clibrary.h"
 
-#include <stdarg.h>
-#include <string.h>
+#include "inn/concat.h"
+#include "inn/xmalloc.h"
 
 /* Abbreviation for cleaner code. */
-#define VA_NEXT(var, type)      ((var) = (type) va_arg(args, type))
+#define VA_NEXT(var, type) ((var) = (type) va_arg(args, type))
 
-/* ANSI C requires at least one named parameter. */
+
+/*
+ * Concatenate all of the arguments into a newly allocated string.  ANSI C
+ * requires at least one named parameter, but it's not treated any different
+ * than the rest.
+ */
 char *
 concat(const char *first, ...)
 {
@@ -50,10 +64,12 @@ concat(const char *first, ...)
     va_end(args);
     length++;
 
-    /* Create the string.  Doing the copy ourselves avoids useless string
-       traversals of result, if using strcat, or string, if using strlen to
-       increment a pointer into result, at the cost of losing the native
-       optimization of strcat if any. */
+    /*
+     * Create the string.  Doing the copy ourselves avoids useless string
+     * traversals of result, if using strcat, or string, if using strlen to
+     * increment a pointer into result, at the cost of losing the native
+     * optimization of strcat if any.
+     */
     result = xmalloc(length);
     p = result;
     va_start(args, first);
@@ -67,6 +83,10 @@ concat(const char *first, ...)
 }
 
 
+/*
+ * Concatenate name with base, unless name begins with / or ./.  Return the
+ * new string in newly allocated memory.
+ */
 char *
 concatpath(const char *base, const char *name)
 {
