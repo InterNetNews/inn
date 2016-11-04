c00f87883e78155f81ca19807a60cd98cbe98e49
Julien ÉLIE <julien@trigofacile.com>
2016-11-04 22:19:40 +0000

    Mark these files as now unmaintained in rra-c-util

--- tests/lib/strlcpy-t.c
+++ tests/lib/strlcpy-t.c
@@ -2,8 +2,9 @@
  *
  * strlcpy test suite.
  *
- * The canonical version of this file is maintained in the rra-c-util package,
- * which can be found at <http://www.eyrie.org/~eagle/software/rra-c-util/>.
+ * The canonical version of this file *used to be* maintained in the
+ * rra-c-util package, which can be found at
+ * <https://www.eyrie.org/~eagle/software/rra-c-util/>.
  *
  * Written by Russ Allbery <eagle@eyrie.org>
  *
