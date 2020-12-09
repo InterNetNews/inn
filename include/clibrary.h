e48ce10dc28e50d80d48297178821278b06fd161
Julien ÉLIE <julien@trigofacile.com>
2020-12-09 21:06:15 +0000

    Update to latest rra-c-util upstream version
    Specific fixes for INN builds on Alpine and BSD:
    * In the getnameinfo replacement, handle musl libc's gethostbyaddr,
    which returns the string conversion of the IP address if the host
    doesn't resolve.  This only affects the test suite, since musl libc
    supports IPv6 and thus doesn't need this replacement.
    
    * NetBSD prefers reallocarr to reallocarray and only prototypes the
    latter if _OPENBSD_SOURCE is defined, which is not one of the macros
    Autoconf defines by default.  Work around this by checking whether
    reallocarray is declared, not only whether it's available, and
    protyping it if it's not declared.  This uses the semi-hidden
    deprecated symbol on NetBSD.

--- include/clibrary.h
+++ include/clibrary.h
@@ -205,7 +205,7 @@ extern int vsnprintf(char *, size_t, const char *, va_list)
 #if !HAVE_MKSTEMP
 extern int mkstemp(char *);
 #endif
-#if !HAVE_REALLOCARRAY
+#if !HAVE_DECL_REALLOCARRAY
 extern void *reallocarray(void *, size_t, size_t);
 #endif
 #if !HAVE_SETENV
