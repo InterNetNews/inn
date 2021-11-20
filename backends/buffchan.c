5cc4e0d12ffb5bb2d5064534e1a13f8f34a8234c
Julien ÉLIE <Julien-Elie@users.noreply.github.com>
2021-11-20 11:32:34 +0100

    filechan: Remove it, use buffchan instead
    Remove filechan in favour of buffchan.
    Update innupgrade to take care of the change in newsfeeds.
    
    Move map.c into buffchan.c as it is the only program still using these
    functions.

--- backends/buffchan.c
+++ backends/buffchan.c
@@ -15,7 +15,6 @@
 #include "inn/messages.h"
 #include "inn/paths.h"
 #include "inn/qio.h"
-#include "map.h"
 
 /*
 **  Hash functions for hashing sitenames.
@@ -25,6 +24,16 @@
     j = (j << 5) + j + *p++
 #define SITE_SIZE      128
 #define SITE_BUCKET(j) &SITEtable[j & (SITE_SIZE - 1)]
+
+
+/*
+**  For the mapping of site names to file names.
+*/
+typedef struct _PAIR {
+    char First;
+    char *Key;
+    char *Value;
+} PAIR;
 
 
 /*
@@ -58,6 +67,8 @@ typedef struct _SITEHASH {
 /* Global variables. */
 static char *Format;
 static const char *Map;
+static PAIR *MAPdata;
+static PAIR *MAPend;
 static int BufferMode;
 static int CloseEvery;
 static int FlushEvery;
@@ -66,6 +77,82 @@ static int FlushSeconds;
 static sig_atomic_t GotInterrupt;
 static SITEHASH SITEtable[SITE_SIZE];
 static time_t Now;
+
+
+/*
+**  Free the map.
+*/
+static void
+MAPfree(void)
+{
+    PAIR *mp;
+
+    for (mp = MAPdata; mp < MAPend; mp++) {
+        free(mp->Key);
+        free(mp->Value);
+    }
+    free(MAPdata);
+    MAPdata = NULL;
+}
+
+
+/*
+**  Read the map file.
+*/
+static void
+MAPread(const char *name)
+{
+    FILE *F;
+    int i;
+    PAIR *mp;
+    char *p;
+    char buff[BUFSIZ];
+
+    if (MAPdata != NULL)
+        MAPfree();
+
+    /* Open file, count lines. */
+    if ((F = fopen(name, "r")) == NULL) {
+        fprintf(stderr, "Can't open %s, %s\n", name, strerror(errno));
+        exit(1);
+    }
+    for (i = 0; fgets(buff, sizeof buff, F) != NULL; i++)
+        continue;
+    mp = MAPdata = xmalloc((i + 1) * sizeof(PAIR));
+
+    /* Read each line; ignore blank and comment lines. */
+    fseeko(F, 0, SEEK_SET);
+    while (fgets(buff, sizeof buff, F) != NULL) {
+        if ((p = strchr(buff, '\n')) != NULL)
+            *p = '\0';
+        if (buff[0] == '\0' || buff[0] == '#'
+            || (p = strchr(buff, ':')) == NULL)
+            continue;
+        *p++ = '\0';
+        mp->First = buff[0];
+        mp->Key = xstrdup(buff);
+        mp->Value = xstrdup(p);
+        mp++;
+    }
+    fclose(F);
+    MAPend = mp;
+}
+
+
+/*
+**  Look up a name in the map, return original value if not found.
+*/
+static char *
+MAPname(char *p)
+{
+    PAIR *mp;
+    char c;
+
+    for (c = *p, mp = MAPdata; mp < MAPend; mp++)
+        if (c == mp->First && strcmp(p, mp->Key) == 0)
+            return mp->Value;
+    return p;
+}
 
 
 /*
--- backends/map.h
+++
@@ -1,3 +0,0 @@
-void MAPfree(void);             /* free the map */
-void MAPread(const char *name); /* read the map file */
-char *MAPname(char *p);         /* lookup in the map */
