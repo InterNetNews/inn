fe5902a1537ec3c784d367bbc59a1969192e9f25
Julien ÉLIE <Julien-Elie@users.noreply.github.com>
2022-02-08 21:54:11 +0100

    Convert libinn_dbz(3) documentation to POD
    Document the new API.  Several functions have changed (dbzfetch, dbzstore):
    arguments and return values are the same whether tagged hash format was
    enabled or not.
    
    Document .pag name, DBZSTORE_xxx values, the default values for
    DBZ_INTERNAL_HASH_SIZE, nonblock only if not mmap'ing.
    
    Fix the comment in the dbzfetch() function.

--- include/inn/dbz.h
+++ include/inn/dbz.h
@@ -7,13 +7,13 @@
 
 BEGIN_DECLS
 
-/* This is the number of bytes of the md5 to actually store in
- * the .pag file.  This number directly effects the collision
+/* This is the number of bytes of the MD5 to actually store in
+ * the .pag file.  This number directly affects the collision
  * rate and memory usage.  You can probably set this number as
- * low as 5 w/o problems and some sites may want to set it as
+ * low as 4 w/o problems and some sites may want to set it as
  * high as 8.  Anything higher than that is probably not useful.
- * Note at the internal hash size isn't the only factor that
- * effects collision rate.  The table index is used as an implicit
+ * Note that the internal hash size isn't the only factor that
+ * affects collision rate.  The table index is used as an implicit
  * part of the hash value stored also.
  */
 #ifdef DO_TAGGED_HASH
@@ -41,17 +41,17 @@ typedef struct {
        copy.  This will replace a single large write to disk when dbzsync is
        called. */
     bool writethrough;
-    /* Whether to do hash lookups from disk, memory or a mmap'ed file */
+    /* Whether to do hash lookups from disk, memory or a mmap'ed file. */
     dbz_incore_val pag_incore;
     dbz_incore_val exists_incore;
     /* Whether dbzstore should update the database async or sync.  This
-       is only applicable if you're not mmaping the database */
+       is only applicable if you're not mmaping the database. */
     bool nonblock;
 } dbzoptions;
 
 #if !defined(lint) && (defined(__SUNPRO_C) || defined(_nec_ews))
 #    pragma pack(1)
-#endif /* nor lint, nor __SUNPRO_C, nor sgi, nor _nec_ews */
+#endif
 /* Leave the __attribute__ ((__packed__)) on there because removing it
  * might change the layout of the data structure on disk on some platform,
  * thus invalidating old history files.
@@ -62,7 +62,7 @@ typedef struct {
 } __attribute__((__packed__)) erec;
 #if !defined(lint) && (defined(__SUNPRO_C) || defined(_nec_ews))
 #    pragma pack()
-#endif /* nor lint, nor__SUNPRO_C, nor _nec_ews */
+#endif
 
 /* standard dbm functions */
 extern bool dbzinit(const char *name);
