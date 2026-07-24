fe28158f409f71bac11fef38bb86891a6102f97a
Kevin Bowling <kevin.bowling@kev009.com>
2026-07-24 02:08:32 -0700

    Check innxmit batch prefixes before indexing
    Compare a batch pathname with patharticles before reading the separator
    at the configured prefix length.  Short nonempty batch lines no longer
    read beyond their terminator.
    
    see #371

--- backends/innxmit.c
+++ backends/innxmit.c
@@ -1130,6 +1130,7 @@ main(int ac, char *av[])
     volatile int port = NNTP_PORT;
     bool val;
     char *path;
+    size_t patharticleslen;
     volatile unsigned int ConnectTimeout;
     volatile unsigned int TotalTimeout;
 
@@ -1139,6 +1140,7 @@ main(int ac, char *av[])
     /* Set defaults. */
     if (!innconf_read(NULL))
         exit(1);
+    patharticleslen = strlen(innconf->patharticles);
 
     ConnectTimeout = 0;
     TotalTimeout = 0;
@@ -1401,11 +1403,10 @@ main(int ac, char *av[])
             continue;
 
         /* Split the line into possibly two fields. */
-        if (Article[0] == '/' && Article[strlen(innconf->patharticles)] == '/'
-            && strncmp(Article, innconf->patharticles,
-                       strlen(innconf->patharticles))
-                   == 0)
-            Article += strlen(innconf->patharticles) + 1;
+        if (Article[0] == '/'
+            && strncmp(Article, innconf->patharticles, patharticleslen) == 0
+            && Article[patharticleslen] == '/')
+            Article += patharticleslen + 1;
         if ((MessageID = strchr(Article, ' ')) != NULL) {
             *MessageID++ = '\0';
             if (*MessageID != '<' || (p = strrchr(MessageID, '>')) == NULL
